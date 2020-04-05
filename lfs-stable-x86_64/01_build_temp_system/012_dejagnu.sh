3d3018ba53d148a61b927cabb146bbf480d25f76
Vladimir <mrvladislavovich@gmail.com>
2020-04-05 23:24:13 +0400

    LFS: 01_build_temp_system/012_dejagnu.sh updated

--- lfs-stable-x86_64/01_build_temp_system/012_dejagnu.sh
+++ lfs-stable-x86_64/01_build_temp_system/012_dejagnu.sh
@@ -8,7 +8,7 @@ PRGNAME="dejagnu"
 # http://www.linuxfromscratch.org/lfs/view/9.0/chapter05/dejagnu.html
 
 # Home page: http://www.gnu.org/software/dejagnu/
-# Download: vhttp://ftp.gnu.org/gnu/dejagnu/dejagnu-1.6.2.tar.gz
+# Download:  http://ftp.gnu.org/gnu/dejagnu/dejagnu-1.6.2.tar.gz
 
 source "$(pwd)/check_environment.sh"                  || exit 1
 source "$(pwd)/unpack_source_archive.sh" "${PRGNAME}" || exit 1
