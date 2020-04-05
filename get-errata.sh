aa2aed78e5b103022488cb04748f41b35aa6f0a0
Vladimir <mrvladislavovich@gmail.com>
2020-04-06 02:35:47 +0400

    get-errata.sh added

---
+++ get-errata.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+VERSION="stable"
+BOOK_URL="http://www.linuxfromscratch.org"
+ERRATA_LFS_URL="${BOOK_URL}/lfs"
+ERRATA_BLFS_URL="${BOOK_URL}/blfs"
+LGREEN="\033[1;32m"
+BROWN="\033[0;33m"
+LBLUE="\033[1;34m"
+RESET="\033[0m"
+MOVE_CURS_TO_27_COL="\033[27G"
+
+show_packages() {
+    echo -e "${LGREEN}$1${RESET}"
+    for PKG in $2; do
+        PKGNAME="$(echo "${PKG}" | cut -d \> -f 2)"
+        URL="${ERRATA_LFS_URL}/$(echo "${PKG}" | cut -d / -f 3- | \
+            cut -d \" -f 1)"
+        echo -e "${BROWN}${PKGNAME}${MOVE_CURS_TO_27_COL}${LBLUE}${URL}${RESET}"
+    done
+}
+
+get_pkg_list() {
+    PKG_LIST="$(wget -q -O - "$1/errata/${VERSION}/" | \
+        grep '<a href="../../view/' | grep -v "development version of the" | \
+        cut -d \" -f 2- | cut -d \< -f 1)"
+}
+
+get_pkg_list "${ERRATA_LFS_URL}"
+show_packages LFS "${PKG_LIST}"
+echo ""
+get_pkg_list "${ERRATA_BLFS_URL}"
+show_packages BLFS "${PKG_LIST}"
