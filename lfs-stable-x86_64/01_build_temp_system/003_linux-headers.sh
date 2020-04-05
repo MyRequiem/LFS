40b519bc15d4541bb36566d4772967360927d256
Vladimir <mrvladislavovich@gmail.com>
2020-04-05 19:12:56 +0400

    LFS: 01_build_temp_system/003_linux-headers.sh updated

--- lfs-stable-x86_64/01_build_temp_system/003_linux-headers.sh
+++ lfs-stable-x86_64/01_build_temp_system/003_linux-headers.sh
@@ -4,28 +4,29 @@ PRGNAME="linux-headers"
 
 ### Linux Headers
 
-# http://www.linuxfromscratch.org/lfs/view/9.0/chapter05/linux-headers.html
+# http://www.linuxfromscratch.org/lfs/view/stable/chapter05/linux-headers.html
 
 # Home page:    https://www.kernel.org/
-# Download:     https://www.kernel.org/pub/linux/kernel/v5.x/linux-5.2.21.tar.xz
+# Download:     https://www.kernel.org/pub/linux/kernel/v5.x/linux-5.5.15.tar.xz
 # All versions: https://mirrors.edge.kernel.org/pub/linux/kernel/
 
-# Версия ядра linux для LFS-9.0 - 5.2.8
+# На 05.04.20 версия ядра linux для LFS-stable - 5.5.3
 # По рекомендации на странице
 # http://www.linuxfromscratch.org/lfs/view/stable/chapter03/packages.html
-# cледует использовать последнюю доступную версию ядра 5.2.x
-# На 20.01.20 последняя версия ядра ветки 5.2.x это 5.2.21
+# cледует использовать последнюю доступную версию ядра 5.5.x
+# На 05.04.20 последняя версия ядра ветки 5.5.x это 5.5.15
 
 ARCH_NAME="$(echo ${PRGNAME} | cut -d - -f 1)"
 source "$(pwd)/check_environment.sh"                    || exit 1
 source "$(pwd)/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
 
-# очищаем исходники ядра
+# очищаем дерево исходников ядра
 make mrproper
-# во время установки заголовков в указанную параметром INSTALL_HDR_PATH
-# директорию, эта директория сначала полностью очищается. Поэтому будет
-# правильнее установить заголовки во временную директорию, например
-# ./tmp-linux-headers, а уже потом скопировать содержимое директории
-# ./tmp-linux-headers/include/ в /tools/include
-make INSTALL_HDR_PATH=./tmp-"${PRGNAME}" headers_install
-cp -rv ./tmp-"${PRGNAME}"/include/* /tools/include
+
+# извлечем заголовки из исходного кода ядра. Рекомендованный
+#    make target "headers_install"
+# не может быть использован, поскольку он требует rsync, который пока не
+# установлен в LFS системе. Заголовки сначала помещаются в ./usr/include/, а
+# затем мы их копируем в /tools/include/
+make headers
+cp -vr usr/include/* /tools/include/
