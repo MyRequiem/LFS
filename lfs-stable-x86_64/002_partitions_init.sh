3688eac18cdd742a6dfabd8c1f6a514f9a08c5d7
Vladimir <mrvladislavovich@gmail.com>
2020-04-05 16:38:41 +0400

    LFS: 002_partitions_init.sh modified

--- lfs-stable-x86_64/002_partitions_init.sh
+++ lfs-stable-x86_64/002_partitions_init.sh
@@ -1,50 +1,46 @@
 #! /bin/bash
 
-# http://www.linuxfromscratch.org/lfs/view/9.0/chapter03/introduction.html
-# http://www.linuxfromscratch.org/lfs/view/9.0/chapter04/creatingtoolsdir.html
+# http://www.linuxfromscratch.org/lfs/view/stable/chapter03/introduction.html
+# http://www.linuxfromscratch.org/lfs/view/stable/chapter04/creatingtoolsdir.html
 
 LFS="/mnt/lfs"
 PART="/dev/sda10"
 
-# сначала нужно отформатировать раздел, на котором будем собирать систему LFS.
+# сначала нужно отмонтировать раздел на котором будем собирать LFS систему
+# (если он примонтирован) и отформатировать его в ext4
 echo "Attention!!!"
 echo "Partition on which the system will be build: ${PART}"
 echo -n "Do you want to format it to a ext4 file system? [yes/No]: "
+
 read -r JUNK
+
 if [[ "x${JUNK}" == "xyes" ]]; then
     umount "${PART}" &>/dev/null
     echo ""
     fdisk -l "${PART}"
     echo ""
     mkfs.ext4 -v "${PART}"
+else
+    echo "Canceled ..."
+    exit 0
 fi
 
 # создаем точку монтирования
-! [ -d "${LFS}" ] && mkdir -pv "${LFS}"
+mkdir -pv "${LFS}"
 
 # монтируем раздел ${PART} в /mnt/lfs
 # Если бы мы использовали несколько разделов для LFS, например отдельный раздел
 # для /home, то нужно будет создать директорию ${LFS}/home и туда смонтировать
 # другой раздел /dev/sdaXX, и т.д.
-if ! mount | /bin/grep -q "${PART}"; then
-    mount -v -t ext4 "${PART}" "${LFS}"
-fi
+mount -vt ext4 "${PART}" "${LFS}"
 
 # создаем нужные директории:
 # /mnt/lfs/sources
 # /mnt/lfs/tools
-# и ссылку на хосте /tools -> /mnt/lfs/tools
-if ! [ -d "${LFS}/sources" ]; then
-    mkdir -pv "${LFS}/sources"
-    # разрешена запись всем пользователям, но удалять может только владелец
-    # каталога
-    chmod -v a+wt "${LFS}/sources"
-fi
+mkdir -pv "${LFS}"/{sources,tools}
+# разрешена запись всем пользователям, но удалять может только владелец
+# каталога
+chmod -v a+wt "${LFS}/sources"
 
-if ! [ -d "${LFS}/tools" ]; then
-    mkdir -pv "${LFS}/tools"
-fi
-
-if ! [ -L /tools ]; then
-    ln -sv "${LFS}/tools" /
-fi
+# ссылка на хосте /tools -> /mnt/lfs/tools
+ln -sfv "${LFS}/tools" /
