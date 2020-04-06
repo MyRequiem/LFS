ba063c70c24d701a48bdc862658eadd66eb235ce
Vladimir <mrvladislavovich@gmail.com>
2020-04-06 14:50:49 +0400

    LFS: 01_build_temp_system/031_tar.sh updated

--- lfs-stable-x86_64/01_build_temp_system/031_tar.sh
+++ lfs-stable-x86_64/01_build_temp_system/031_tar.sh
@@ -5,7 +5,7 @@ PRGNAME="tar"
 ### Tar
 # Программа архивации файлов и каталогов
 
-# http://www.linuxfromscratch.org/lfs/view/9.0/chapter05/tar.html
+# http://www.linuxfromscratch.org/lfs/view/stable/chapter05/tar.html
 
 # Home page: http://www.gnu.org/software/tar/
 # Download:  http://ftp.gnu.org/gnu/tar/tar-1.32.tar.xz
