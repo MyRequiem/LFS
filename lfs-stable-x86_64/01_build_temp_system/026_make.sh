8369b1379ca0c65f2f1f984403930724a494a743
Vladimir <mrvladislavovich@gmail.com>
2020-04-06 13:23:57 +0400

    LFS: 01_build_temp_system/026_make.sh updated

--- lfs-stable-x86_64/01_build_temp_system/026_make.sh
+++ lfs-stable-x86_64/01_build_temp_system/026_make.sh
@@ -5,19 +5,16 @@ PRGNAME="make"
 ### Make
 # Программы для компиляции
 
-# http://www.linuxfromscratch.org/lfs/view/9.0/chapter05/make.html
+# http://www.linuxfromscratch.org/lfs/view/stable/chapter05/make.html
 
 # Home page: http://www.gnu.org/software/make/
-# Download:  http://ftp.gnu.org/gnu/make/make-4.2.1.tar.gz
+# Download:  http://ftp.gnu.org/gnu/make/make-4.3.tar.gz
 
 source "$(pwd)/check_environment.sh"                  || exit 1
 source "$(pwd)/unpack_source_archive.sh" "${PRGNAME}" || exit 1
 
-# обойдем ошибку, вызванную glibc-2.27 и более поздними версиями
-sed -i '211,217 d; 219,229 d; 232 d' glob/glob.c
-
 # Make не будет ссылаться на библиотеки Guile, которые могут присутствовать в
-# хост-системе, но пока не доступны в нашей временной среде
+# хост-системе, но не доступны в нашей временной среде
 #    --without-guile
 ./configure         \
     --prefix=/tools \
