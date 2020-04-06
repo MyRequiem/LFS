0313f657f516bfc3030af9b1beafbaceacbe40ea
Vladimir <mrvladislavovich@gmail.com>
2020-04-06 11:51:09 +0400

    LFS: 01_build_temp_system/check_environment.sh modified

--- lfs-stable-x86_64/01_build_temp_system/check_environment.sh
+++ lfs-stable-x86_64/01_build_temp_system/check_environment.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 
+PART="/dev/sda10"
+
 if [[ "$(whoami)" != "lfs" ]]; then
     echo "You need to login as lfs user"
+    echo "  # su - lfs"
     exit 1
 fi
 
@@ -9,6 +12,12 @@ if [ -z "${LFS}" ]; then
     echo "Environment variable LFS is empty. Why?"
     echo "Check your ~/.bashrc file. It must be set to a variable:"
     echo 'LFS="/mnt/lfs"'
+    exit 1
+fi
+
+if ! mount | /bin/grep -q "${LFS}"; then
+    echo "Mount point ${LFS} not mounted. You need to mount it:"
+    echo "  # mount -v ${PART} ${LFS}"
     exit 1
 fi
 
