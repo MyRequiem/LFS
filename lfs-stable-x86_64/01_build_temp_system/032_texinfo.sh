85014c1cbe4e998706580bacea852c5c63ea2166
Vladimir <mrvladislavovich@gmail.com>
2020-04-06 14:56:18 +0400

    LFS: 01_build_temp_system/032_texinfo.sh updated

--- lfs-stable-x86_64/01_build_temp_system/032_texinfo.sh
+++ lfs-stable-x86_64/01_build_temp_system/032_texinfo.sh
@@ -5,10 +5,10 @@ PRGNAME="texinfo"
 ### Texinfo
 # Программы для чтения, записи и конвертации страниц info
 
-# http://www.linuxfromscratch.org/lfs/view/9.0/chapter05/texinfo.html
+# http://www.linuxfromscratch.org/lfs/view/stable/chapter05/texinfo.html
 
 # Home page: http://www.gnu.org/software/texinfo/
-# Download: http://ftp.gnu.org/gnu/texinfo/texinfo-6.6.tar.xz
+# Download:  http://ftp.gnu.org/gnu/texinfo/texinfo-6.7.tar.xz
 
 source "$(pwd)/check_environment.sh"                  || exit 1
 source "$(pwd)/unpack_source_archive.sh" "${PRGNAME}" || exit 1
