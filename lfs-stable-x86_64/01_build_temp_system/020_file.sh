5f02ce04e0c86e671e8ff77cf802882e31bb9edb
Vladimir <mrvladislavovich@gmail.com>
2020-04-06 12:17:57 +0400

    LFS: 01_build_temp_system/020_file.sh updated

--- lfs-stable-x86_64/01_build_temp_system/020_file.sh
+++ lfs-stable-x86_64/01_build_temp_system/020_file.sh
@@ -5,10 +5,10 @@ PRGNAME="file"
 ### File
 # Утилита для определения типа файла
 
-# http://www.linuxfromscratch.org/lfs/view/9.0/chapter05/file.html
+# http://www.linuxfromscratch.org/lfs/view/stable/chapter05/file.html
 
 # Home page: https://www.darwinsys.com/file/
-# Download:  ftp://ftp.astron.com/pub/file/file-5.37.tar.gz
+# Download:  ftp://ftp.astron.com/pub/file/file-5.38.tar.gz
 
 source "$(pwd)/check_environment.sh"                  || exit 1
 source "$(pwd)/unpack_source_archive.sh" "${PRGNAME}" || exit 1
