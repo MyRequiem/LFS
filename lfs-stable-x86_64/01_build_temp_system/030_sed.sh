c581f957d6cbbf84930ac2967ae64964b73b0102
Vladimir <mrvladislavovich@gmail.com>
2020-04-06 14:16:59 +0400

    LFS: 01_build_temp_system/030_sed.sh updated

--- lfs-stable-x86_64/01_build_temp_system/030_sed.sh
+++ lfs-stable-x86_64/01_build_temp_system/030_sed.sh
@@ -5,10 +5,10 @@ PRGNAME="sed"
 ### Sed
 # Потоковый редактор
 
-# http://www.linuxfromscratch.org/lfs/view/9.0/chapter05/sed.html
+# http://www.linuxfromscratch.org/lfs/view/stable/chapter05/sed.html
 
 # Home page: http://www.gnu.org/software/sed/
-# Download:  http://ftp.gnu.org/gnu/sed/sed-4.7.tar.xz
+# Download:  http://ftp.gnu.org/gnu/sed/sed-4.8.tar.xz
 
 source "$(pwd)/check_environment.sh"                  || exit 1
 source "$(pwd)/unpack_source_archive.sh" "${PRGNAME}" || exit 1
