7940671e7758a3aec5752fcba6384d7402270b63
Vladimir <mrvladislavovich@gmail.com>
2020-04-06 15:37:19 +0400

    LFS: 01_build_temp_system/035_stripping.sh updated

--- lfs-stable-x86_64/01_build_temp_system/035_stripping.sh
+++ lfs-stable-x86_64/01_build_temp_system/035_stripping.sh
@@ -4,7 +4,9 @@ source "$(pwd)/check_environment.sh" || exit 1
 
 # исполняемые файлы и библиотеки, созданные до сих пор, содержат не нужную нам
 # отладочную информацию, поэтому можно ее удалить
+echo "strip --strip-debug /tools/lib/* ..."
 strip --strip-debug /tools/lib/*
+echo "/usr/bin/strip --strip-unneeded /tools/{,s}bin/* ..."
 /usr/bin/strip --strip-unneeded /tools/{,s}bin/*
 # в выводе этих команд будут  присутствовать сообщения о том, что не
 # распознается формат файлов. В основном это оносится к скриптам, а не бинарным
@@ -16,7 +18,9 @@ strip --strip-debug /tools/lib/*
 # уничтожены, и пакеты придется пересобирать заново.
 
 # удалим документацию
+echo "rm -rf /tools/{,share}/{info,man,doc} ..."
 rm -rf /tools/{,share}/{info,man,doc}
 
 # удалим не нужные *.la файлы (libtool-архивы), устанавливаемые с библиотеками
-find /tools/{lib,libexec} -name \*.la -delete
+echo "find /tools/{lib,libexec} -name "*.la" -delete ..."
+find /tools/{lib,libexec} -name "*.la" -delete
