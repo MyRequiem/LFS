217c4bc7264ab22be4bf814ad09c526e599dc0f9
Vladimir <mrvladislavovich@gmail.com>
2020-04-06 15:17:17 +0400

    LFS: 01_build_temp_system/034_util-linux.sh updated

--- lfs-stable-x86_64/01_build_temp_system/034_util-linux.sh
+++ lfs-stable-x86_64/01_build_temp_system/034_util-linux.sh
@@ -6,10 +6,10 @@ PRGNAME="util-linux"
 # Содержит различные утилиты для обработки файловых систем, консолей, разделов,
 # сообщений и т.д.
 
-# http://www.linuxfromscratch.org/lfs/view/9.0/chapter06/util-linux.html
+# http://www.linuxfromscratch.org/lfs/view/stable/chapter06/util-linux.html
 
 # Home page: http://freecode.com/projects/util-linux
-# Download:  https://www.kernel.org/pub/linux/utils/util-linux/v2.34/util-linux-2.34.tar.xz
+# Download:  https://www.kernel.org/pub/linux/utils/util-linux/v2.35/util-linux-2.35.1.tar.xz
 
 source "$(pwd)/check_environment.sh"                  || exit 1
 source "$(pwd)/unpack_source_archive.sh" "${PRGNAME}" || exit 1
