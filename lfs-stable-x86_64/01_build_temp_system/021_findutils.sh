0dc6b855eec5e24286c3c4c41be6c498940bc18a
Vladimir <mrvladislavovich@gmail.com>
2020-04-06 12:24:52 +0400

    LFS: 01_build_temp_system/021_findutils.sh updated

--- lfs-stable-x86_64/01_build_temp_system/021_findutils.sh
+++ lfs-stable-x86_64/01_build_temp_system/021_findutils.sh
@@ -5,18 +5,13 @@ PRGNAME="findutils"
 ### Findutils
 # Пакет содержит программы для поиска файлов
 
-# http://www.linuxfromscratch.org/lfs/view/9.0/chapter05/findutils.html
+# http://www.linuxfromscratch.org/lfs/view/stable/chapter05/findutils.html
 
 # Home page: http://www.gnu.org/software/findutils/
-# Download:  http://ftp.gnu.org/gnu/findutils/findutils-4.6.0.tar.gz
+# Download:  http://ftp.gnu.org/gnu/findutils/findutils-4.7.0.tar.xz
 
 source "$(pwd)/check_environment.sh"                  || exit 1
 source "$(pwd)/unpack_source_archive.sh" "${PRGNAME}" || exit 1
-
-# некоторые исправления для glibc-2.28
-sed -i 's/IO_ftrylockfile/IO_EOF_SEEN/' gl/lib/*.c
-sed -i '/unistd/a #include <sys/sysmacros.h>' gl/lib/mountlist.c
-echo "#define _IO_IN_BACKUP 0x100" >> gl/lib/stdio-impl.h
 
 ./configure \
     --prefix=/tools || exit 1
