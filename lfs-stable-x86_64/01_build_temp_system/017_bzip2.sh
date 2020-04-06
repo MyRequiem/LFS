ac110d8117f82fa3ae715d254a603cb45d785571
Vladimir <mrvladislavovich@gmail.com>
2020-04-06 11:54:07 +0400

    LFS: 01_build_temp_system/017_bzip2.sh updated

--- lfs-stable-x86_64/01_build_temp_system/017_bzip2.sh
+++ lfs-stable-x86_64/01_build_temp_system/017_bzip2.sh
@@ -5,7 +5,7 @@ PRGNAME="bzip2"
 ### Bzip2
 # Программы для сжатия и распаковки файлов
 
-# http://www.linuxfromscratch.org/lfs/view/9.0/chapter05/bzip2.html
+# http://www.linuxfromscratch.org/lfs/view/stable/chapter05/bzip2.html
 
 # Home page: https://sourceforge.net/projects/bzip2/
 # Download:  https://www.sourceware.org/pub/bzip2/bzip2-1.0.8.tar.gz
@@ -13,5 +13,17 @@ PRGNAME="bzip2"
 source "$(pwd)/check_environment.sh"                  || exit 1
 source "$(pwd)/unpack_source_archive.sh" "${PRGNAME}" || exit 1
 
-make || make -j1 || exit 1
+# пакет Bzip2 не содержит скрипта 'configure' и включает в себя два Makefile:
+#    Makefile-libbz2_so    - для сборки shared библиотеки
+#    Makefile              - для сборки static библиотеки
+#
+# Нам нужны обе, поэтому будем компилировать в два этапа:
+make -f Makefile-libbz2_so || exit 1
+make clean
+make || exit 1
+
 make PREFIX=/tools install
+
+cp -v  bzip2-shared  /tools/bin/bzip2
+cp -av libbz2.so*    /tools/lib
+ln -sv libbz2.so.1.0 /tools/lib/libbz2.so
