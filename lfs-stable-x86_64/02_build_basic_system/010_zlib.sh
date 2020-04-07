190a3aca72a27a7c67e0a21dd94ad14ab085df10
Vladimir <mrvladislavovich@gmail.com>
2020-04-07 17:04:42 +0400

    LFS: 02_build_basic_system/010_zlib.sh updated

--- lfs-stable-x86_64/02_build_basic_system/010_zlib.sh
+++ lfs-stable-x86_64/02_build_basic_system/010_zlib.sh
@@ -5,7 +5,7 @@ PRGNAME="zlib"
 ### Zlib
 # Универсальная многопоточная библиотека сжатия данных
 
-# http://www.linuxfromscratch.org/lfs/view/9.0/chapter06/zlib.html
+# http://www.linuxfromscratch.org/lfs/view/stable/chapter06/zlib.html
 
 # Home page: https://www.zlib.net/
 # Download:  https://zlib.net/zlib-1.2.11.tar.xz
@@ -14,16 +14,16 @@ ROOT="/"
 source "${ROOT}check_environment.sh"                  || exit 1
 source "${ROOT}unpack_source_archive.sh" "${PRGNAME}" || exit 1
 
+TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
+rm -rf "${TMP_DIR}"
+mkdir -pv "${TMP_DIR}/lib"
+
 ./configure \
     --prefix=/usr || exit 1
 
 make || exit 1
 make check
 make install
-
-TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
-rm -rf "${TMP_DIR}"
-mkdir -pv "${TMP_DIR}"
 make install DESTDIR="${TMP_DIR}"
 
 # библиотеку libz.so.${VERSION} необходимо переместить из /usr/lib в /lib, а
@@ -38,12 +38,13 @@ make install DESTDIR="${TMP_DIR}"
 #       libz.so -> ../../lib/libz.so.${VERSION}
 mv -v /usr/lib/libz.so.* /lib
 # /usr/lib/libz.so -> ../../lib/libz.so.${VERSION}
-ln -sfv ../../lib/"$(readlink /usr/lib/libz.so)" /usr/lib/libz.so
+ln -sfv "../../lib/$(readlink /usr/lib/libz.so)" /usr/lib/libz.so
 
-cd "${TMP_DIR}" || exit 1
-mkdir -pv lib
-mv -v usr/lib/libz.so.* lib/
-ln -sfv ../../lib/"$(readlink usr/lib/libz.so)" usr/lib/libz.so
+(
+    cd "${TMP_DIR}" || exit 1
+    mv -v usr/lib/libz.so.* lib/
+    ln -sfv "../../lib/$(readlink usr/lib/libz.so)" usr/lib/libz.so
+)
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Package: ${PRGNAME} (compression library)
