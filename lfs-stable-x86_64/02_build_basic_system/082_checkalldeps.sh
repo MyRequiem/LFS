81740a00d2742a00c23c1cedd07c5e747aa39109
Vladimir <mrvladislavovich@gmail.com>
2020-04-07 15:34:16 +0400

    LFS: 02_build_basic_system/082_checkalldeps.sh added

---
+++ lfs-stable-x86_64/02_build_basic_system/082_checkalldeps.sh
@@ -0,0 +1,79 @@
+#! /bin/sh
+
+# created by MyRequiem
+
+# install this script:
+# --------------------
+#    # cp 082_checkalldeps.sh /mnt/lfs/sbin/checkalldeps.sh
+#    # chown root:root /mnt/lfs/sbin/checkalldeps.sh
+#    # chmod 744 /mnt/lfs/sbin/checkalldeps.sh
+
+### BLACKLIST example:
+# BLACKLIST="\
+#     /usr/bin/opera \
+#     /opt/GuitarPro6/GuitarPro \
+# "
+
+BLACKLIST=""
+
+DIRS="\
+    /bin \
+    /sbin \
+    /lib \
+    /usr/bin \
+    /usr/sbin \
+    /usr/lib \
+    /usr/local/bin \
+    /usr/local/sbin \
+    /usr/local/lib \
+    /usr/libexec \
+    /opt \
+"
+
+SCRIPTNAME=$(basename "$0")
+LOG=/tmp/${SCRIPTNAME}.log
+TMP=/tmp/${SCRIPTNAME}.tmp
+ERRORS=/tmp/${SCRIPTNAME}.err
+/bin/false > "${LOG}"
+
+for DIR in ${DIRS}; do
+    if [ -d "${DIR}" ]; then
+        echo -e "\033[0;33m${DIR}/\033[0m"
+        find "${DIR}" -type f 2>/dev/null | while read -r FILE; do
+            if ! file "${FILE}" | grep -e "shared object" -e "executable" | \
+                    grep ELF 1>/dev/null 2>&1; then
+                continue
+            fi
+
+            for BL in ${BLACKLIST}; do
+                [ "${FILE}" = "${BL}" ] && continue 2
+            done
+
+            /bin/false > "${TMP}"
+            /usr/bin/ldd "${FILE}" 2>"${ERRORS}" | \
+                grep -E "not found|no version information" | \
+                sort -u > "${TMP}"
+
+            cat "${ERRORS}" >> "${TMP}"
+            if [ "$(stat -c%s "${TMP}")" != "0" ]; then
+                    echo -e "    \033[0;31m${FILE}\033[0m"
+                    {
+                        echo -e "${FILE}\n------------------"
+                        cat "${TMP}"
+                        echo
+                    } >> "${LOG}"
+            fi
+        done
+    fi
+done
+
+rm -f "${TMP}" "${ERRORS}"
+
+if [ "$(stat -c%s "${LOG}")" != "0" ]; then
+    echo -e "\n\033[0;31mldd found problems with dependencies"
+    echo -e "\033[1;34mlog file: ${LOG}\033[0m\n"
+else
+    rm -f "${LOG}"
+    echo -en "\n\033[1;32mCongratulations !!! ldd not found problems with "
+    echo -e "dependencies\033[0m\n"
+fi
