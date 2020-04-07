3e280303d694754e09d4d766264526c414c659c3
Vladimir <mrvladislavovich@gmail.com>
2020-04-07 17:54:37 +0400

    LFS: 02_build_basic_system/013_file.sh updated

--- lfs-stable-x86_64/02_build_basic_system/013_file.sh
+++ lfs-stable-x86_64/02_build_basic_system/013_file.sh
@@ -5,14 +5,18 @@ PRGNAME="file"
 ### File
 # Утилита для определения типа файла
 
-# http://www.linuxfromscratch.org/lfs/view/9.0/chapter06/file.html
+# http://www.linuxfromscratch.org/lfs/view/stable/chapter06/file.html
 
 # Home page: https://www.darwinsys.com/file/
-# Download:  ftp://ftp.astron.com/pub/file/file-5.37.tar.gz
+# Download:  ftp://ftp.astron.com/pub/file/file-5.38.tar.gz
 
 ROOT="/"
 source "${ROOT}check_environment.sh"                  || exit 1
 source "${ROOT}unpack_source_archive.sh" "${PRGNAME}" || exit 1
+
+TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
+rm -rf "${TMP_DIR}"
+mkdir -pv "${TMP_DIR}"
 
 ./configure \
     --prefix=/usr || exit 1
@@ -20,10 +24,6 @@ source "${ROOT}unpack_source_archive.sh" "${PRGNAME}" || exit 1
 make || exit 1
 make check
 make install
-
-TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
-rm -rf "${TMP_DIR}"
-mkdir -pv "${TMP_DIR}"
 make install DESTDIR="${TMP_DIR}"
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
