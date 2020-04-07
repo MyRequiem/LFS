4183da6412e438452e32b504cdfe2a4a41266041
Vladimir <mrvladislavovich@gmail.com>
2020-04-07 09:53:56 +0400

    LFS: 02_build_basic_system/007_man-pages.sh updated

--- lfs-stable-x86_64/02_build_basic_system/007_man-pages.sh
+++ lfs-stable-x86_64/02_build_basic_system/007_man-pages.sh
@@ -6,20 +6,20 @@ PRGNAME="man-pages"
 # Пакет Man-pages содержит более 2300 man-страниц.
 # Страницы устанавливаются в /usr/share/man/man{1,2,3,4,5,6,7,8}/*
 
-# http://www.linuxfromscratch.org/lfs/view/9.0/chapter06/man-pages.html
+# http://www.linuxfromscratch.org/lfs/view/stable/chapter06/man-pages.html
 
 # Home page: https://www.kernel.org/doc/man-pages/
-# Download:  https://www.kernel.org/pub/linux/docs/man-pages/man-pages-5.02.tar.xz
+# Download:  https://www.kernel.org/pub/linux/docs/man-pages/man-pages-5.05.tar.xz
 
 ROOT="/"
 source "${ROOT}check_environment.sh"                  || exit 1
 source "${ROOT}unpack_source_archive.sh" "${PRGNAME}" || exit 1
 
-make install
-
 TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
 rm -rf "${TMP_DIR}"
 mkdir -pv "${TMP_DIR}"
+
+make install
 make install DESTDIR="${TMP_DIR}"
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
