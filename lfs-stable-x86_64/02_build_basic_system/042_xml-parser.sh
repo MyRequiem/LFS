8eb00a2832fd66c10c240f32ceb6fd682ba119be
Vladimir <mrvladislavovich@gmail.com>
2020-04-08 15:26:34 +0400

    LFS: 02_build_basic_system/042_xml-parser.sh updated

--- lfs-stable-x86_64/02_build_basic_system/042_xml-parser.sh
+++ lfs-stable-x86_64/02_build_basic_system/042_xml-parser.sh
@@ -1,28 +1,28 @@
 #! /bin/bash
 
-PRGNAME="XML-Parser"
+PRGNAME="xml-parser"
 
 ### XML::Parser
 # Модуль XML::Parser является Perl-интерфейсом для синтаксического анализатора
 # XML документов Expat
 
-# http://www.linuxfromscratch.org/lfs/view/9.0/chapter06/xml-parser.html
+# http://www.linuxfromscratch.org/lfs/view/stable/chapter06/xml-parser.html
 
 # Home page: https://github.com/chorny/XML-Parser
-# Download:  https://cpan.metacpan.org/authors/id/T/TO/TODDR/XML-Parser-2.44.tar.gz
+# Download:  https://cpan.metacpan.org/authors/id/T/TO/TODDR/XML-Parser-2.46.tar.gz
 
 ROOT="/"
 source "${ROOT}check_environment.sh"                  || exit 1
-source "${ROOT}unpack_source_archive.sh" "${PRGNAME}" || exit 1
+source "${ROOT}unpack_source_archive.sh" "XML-Parser" || exit 1
+
+TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
+rm -rf "${TMP_DIR}"
+mkdir -pv "${TMP_DIR}"
 
 perl Makefile.PL
 make
 make test
 make install
-
-TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
-rm -rf "${TMP_DIR}"
-mkdir -pv "${TMP_DIR}"
 make install DESTDIR="${TMP_DIR}"
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
