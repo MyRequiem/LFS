277a65aa600ed18ffb9c3b0e06934b2636f69e19
Vladimir <mrvladislavovich@gmail.com>
2020-04-08 11:45:02 +0400

    LFS: 02_build_basic_system/027_libcap.sh updated

--- lfs-stable-x86_64/02_build_basic_system/027_libcap.sh
+++ lfs-stable-x86_64/02_build_basic_system/027_libcap.sh
@@ -7,47 +7,36 @@ PRGNAME="libcap"
 # возможностей, доступных в ядрах Linux. Эти возможности предоставляют
 # разделение корневых привилегий в набор различных привилегий.
 
-# http://www.linuxfromscratch.org/lfs/view/9.0/chapter06/libcap.html
+# http://www.linuxfromscratch.org/lfs/view/stable/chapter06/libcap.html
 
 # Home page: https://sites.google.com/site/fullycapable/
-# Download:  https://www.kernel.org/pub/linux/libs/security/linux-privs/libcap2/libcap-2.27.tar.xz
+# Download:  https://www.kernel.org/pub/linux/libs/security/linux-privs/libcap2/libcap-2.31.tar.xz
 
 ROOT="/"
 source "${ROOT}check_environment.sh"                  || exit 1
 source "${ROOT}unpack_source_archive.sh" "${PRGNAME}" || exit 1
 
-# запретим установку статической библиотеки
-sed -i '/install.*STALIBNAME/d' libcap/Makefile
+TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
+rm -rf "${TMP_DIR}"
+mkdir -pv "${TMP_DIR}/lib"
 
-make || exit 1
+# запретим установку двух статических библиотек
+sed -i '/install.*STA...LIBNAME/d' libcap/Makefile || exit 1
 
-# в этот пакет не входит набор тестов, поэтому сразу устанавливаем
-
+# собирать библиотеки с префиксом /lib, а не /lib64
+#    lib=lib
+make lib=lib || exit 1
+make test
 # избегаем ошибок установки, если ядро или файловая система не поддерживают
 # расширенные возможности
 #    RAISE_SETFCAP=no
 # установить библиотеку в $prefix/lib, а не в $prefix/lib64
 #    lib=lib
-make RAISE_SETFCAP=no lib=lib prefix=/usr install
-chmod -v 755 "/usr/lib/libcap.so.${VERSION}"
+make lib=lib install
+make lib=lib install DESTDIR="${TMP_DIR}"
 
-TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
-rm -rf "${TMP_DIR}"
-mkdir -pv "${TMP_DIR}/lib"
-make RAISE_SETFCAP=no lib=lib prefix="${TMP_DIR}/usr" install
-chmod -v 755 "${TMP_DIR}/usr/lib/libcap.so.${VERSION}"
-
-# библиотеку необходимо переместить из /usr/lib в /lib
-mv -v /usr/lib/libcap.so.* /lib
-mv -v "${TMP_DIR}/usr/lib"/libcap.so.* "${TMP_DIR}/lib"
-
-# воссоздадим ссылку libcap.so в /usr/lib
-# libcap.so -> ../../lib/libcap.so.2
-ln -sfv "../../lib/$(readlink /usr/lib/libcap.so)" /usr/lib/libcap.so
-(
-    cd "${TMP_DIR}/usr/lib" || exit 1
-    ln -sfv "../../lib/$(readlink libcap.so)" libcap.so
-)
+chmod -v 755 "/lib/libcap.so.${VERSION}"
+chmod -v 755 "${TMP_DIR}/lib/libcap.so.${VERSION}"
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Package: ${PRGNAME} (get/set POSIX capabilities)
