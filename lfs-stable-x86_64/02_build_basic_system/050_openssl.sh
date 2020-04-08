2ae93a1bd4a9a20cf6535eee569261e44e1c6efa
Vladimir <mrvladislavovich@gmail.com>
2020-04-08 20:48:45 +0400

    LFS: 02_build_basic_system/050_openssl.sh updated

--- lfs-stable-x86_64/02_build_basic_system/050_openssl.sh
+++ lfs-stable-x86_64/02_build_basic_system/050_openssl.sh
@@ -7,23 +7,20 @@ PRGNAME="openssl"
 # предоставления криптографических функций другим пакетам, таким как OpenSSH,
 # почтовым приложениям, веб-браузерам (для доступа по протоколу HTTPS) и т.д.
 
-# http://www.linuxfromscratch.org/lfs/view/9.0/chapter06/openssl.html
+# http://www.linuxfromscratch.org/lfs/view/development/chapter06/openssl.html
 
 # Home page: https://www.openssl.org/
-# Download:  https://www.openssl.org/source/openssl-1.1.1d.tar.gz
-
-# В стабильной версии LFS-9.0 на 05.02.20 указаны обновления для этого пакета
-# до версии 1.1.1d
-#    http://www.linuxfromscratch.org/lfs/errata/9.0/
-#
-#    ... Upgrade to OpenSSL-1.1.1d using the instructions in
-#    http://www.linuxfromscratch.org/lfs/view/development/chapter06/openssl.html
+# Download:  https://www.openssl.org/source/openssl-1.1.1f.tar.gz
 
 ROOT="/"
 source "${ROOT}check_environment.sh"                  || exit 1
 source "${ROOT}unpack_source_archive.sh" "${PRGNAME}" || exit 1
 
-./config \
+TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
+rm -rf "${TMP_DIR}"
+mkdir -pv "${TMP_DIR}"
+
+./config                  \
     --prefix=/usr         \
     --openssldir=/etc/ssl \
     --libdir=lib          \
@@ -37,20 +34,15 @@ make test
 # устанавливаем пакет
 sed -i '/INSTALL_LIBS/s/libcrypto.a libssl.a//' Makefile
 make MANSUFFIX=ssl install
-
-TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
-rm -rf "${TMP_DIR}"
-mkdir -pv "${TMP_DIR}"
-make install DESTDIR="${TMP_DIR}"
+make MANSUFFIX=ssl install DESTDIR="${TMP_DIR}"
 
 # устанавливаем документацию
-mv -v /usr/share/doc/${PRGNAME} \
-    "/usr/share/doc/${PRGNAME}-${VERSION}"
-cp -vfr doc/* "/usr/share/doc/${PRGNAME}-${VERSION}"
+DOCS="/usr/share/doc/${PRGNAME}-${VERSION}"
+mv -v /usr/share/doc/${PRGNAME} "${DOCS}"
+cp -vfr doc/* "${DOCS}"
 
-mv -v "${TMP_DIR}/usr/share/doc/${PRGNAME}" \
-    "${TMP_DIR}/usr/share/doc/${PRGNAME}-${VERSION}"
-cp -vfr doc/* "${TMP_DIR}/usr/share/doc/${PRGNAME}-${VERSION}"
+mv -v "${TMP_DIR}/usr/share/doc/${PRGNAME}" "${TMP_DIR}${DOCS}"
+cp -vfr doc/* "${TMP_DIR}${DOCS}"
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Package: ${PRGNAME} (Secure Sockets Layer toolkit)
