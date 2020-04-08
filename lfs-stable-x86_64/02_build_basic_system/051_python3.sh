b514fa17863bad215d3438ad409d38190429730c
Vladimir <mrvladislavovich@gmail.com>
2020-04-08 21:19:36 +0400

    LFS: 02_build_basic_system/051_python3.sh updated

--- lfs-stable-x86_64/02_build_basic_system/051_python3.sh
+++ lfs-stable-x86_64/02_build_basic_system/051_python3.sh
@@ -5,14 +5,19 @@ PRGNAME="python3"
 ### python3
 # Язык программирования Python 3
 
-# http://www.linuxfromscratch.org/lfs/view/9.0/chapter06/Python.html
+# http://www.linuxfromscratch.org/lfs/view/stable/chapter06/Python.html
 
 # Home page: https://www.python.org/
-# Download:  https://www.python.org/ftp/python/3.7.4/Python-3.7.4.tar.xz
+# Download:  https://www.python.org/ftp/python/3.8.1/Python-3.8.1.tar.xz
+# Docs:      https://www.python.org/ftp/python/doc/3.8.1/python-3.8.1-docs-html.tar.bz2
 
 ROOT="/"
 source "${ROOT}check_environment.sh"              || exit 1
 source "${ROOT}unpack_source_archive.sh" "Python" || exit 1
+
+TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
+rm -rf "${TMP_DIR}"
+mkdir -pv "${TMP_DIR}"
 
 # связываться с уже установленной системной версией Expat
 #    --with-system-expat
@@ -29,32 +34,27 @@ source "${ROOT}unpack_source_archive.sh" "Python" || exit 1
 
 make || exit 1
 
-# сначала устанавливаем во временную директорию
-TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
-rm -rf "${TMP_DIR}"
-mkdir -pv "${TMP_DIR}"
-make install DESTDIR="${TMP_DIR}"
-
 # тесты на данном этапе не запускаем, т.к. они требуют установленных TK и
 # X Window System, поэтому сразу устанавливаем в корневую систему
+
+# Важно: сначала устанавливаем во временную директорию и только потом в корень
+# системы
+make install DESTDIR="${TMP_DIR}"
 make install
 
 MAJ_VERSION="$(echo "${VERSION}" | cut -d . -f 1,2)"
-chmod -v 755 "/usr/lib/libpython${MAJ_VERSION}m.so"
+chmod -v 755 "/usr/lib/libpython${MAJ_VERSION}.so"
 chmod -v 755 /usr/lib/libpython3.so
-chmod -v 755 "${TMP_DIR}/usr/lib/libpython${MAJ_VERSION}m.so"
+chmod -v 755 "${TMP_DIR}/usr/lib/libpython${MAJ_VERSION}.so"
 chmod -v 755 "${TMP_DIR}/usr/lib/libpython3.so"
 
 # ссылка в /usr/bin
 # pip3 -> pip${MAJ_VERSION}
-ln -sfv "pip${MAJ_VERSION}" /usr/bin/pip3
+ln -svf "pip${MAJ_VERSION}" /usr/bin/pip3
 (
     cd "${TMP_DIR}/usr/bin" || exit 1
     ln -sfv "pip${MAJ_VERSION}" pip3
 )
-
-cp -R "/usr/lib/python${MAJ_VERSION}/site-packages"/* \
-    "${TMP_DIR}/usr/lib/python${MAJ_VERSION}/site-packages/"
 
 # устанавливаем документацию
 PYTHON_DOC_ARCH_NAME="python-${VERSION}-docs-html"
@@ -62,12 +62,12 @@ tar xvf "/sources/${PYTHON_DOC_ARCH_NAME}".tar.?z* || exit 1
 cd "${PYTHON_DOC_ARCH_NAME}" || exit 1
 chown -R root:root ./*
 
-DOC_DIR=/usr/share/doc/python-${VERSION}/html
-install -v -dm755 "${DOC_DIR}"
-cp -R ./* "${DOC_DIR}"
+DOCS="/usr/share/doc/python-${VERSION}/html"
+install -v -dm755 "${DOCS}"
+cp -R ./*         "${DOCS}"
 
-install -v -dm755 "${TMP_DIR}/${DOC_DIR}"
-cp -R ./* "${TMP_DIR}/${DOC_DIR}"
+install -v -dm755 "${TMP_DIR}/${DOCS}"
+cp -R ./*         "${TMP_DIR}/${DOCS}"
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Package: ${PRGNAME} (object-oriented interpreted programming language)
