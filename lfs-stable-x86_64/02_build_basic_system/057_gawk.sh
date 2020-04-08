0c77a1f795148a838d823a25b5f92980e04b108d
Vladimir <mrvladislavovich@gmail.com>
2020-04-09 00:22:06 +0400

    LFS: 02_build_basic_system/057_gawk.sh updated

--- lfs-stable-x86_64/02_build_basic_system/057_gawk.sh
+++ lfs-stable-x86_64/02_build_basic_system/057_gawk.sh
@@ -5,7 +5,7 @@ PRGNAME="gawk"
 ### Gawk
 # Программы для работы с текстовыми файлами
 
-# http://www.linuxfromscratch.org/lfs/view/9.0/chapter06/gawk.html
+# http://www.linuxfromscratch.org/lfs/view/stable/chapter06/gawk.html
 
 # Home page: http://www.gnu.org/software/gawk/
 # Download:  http://ftp.gnu.org/gnu/gawk/gawk-5.0.1.tar.xz
@@ -13,6 +13,11 @@ PRGNAME="gawk"
 ROOT="/"
 source "${ROOT}check_environment.sh"                  || exit 1
 source "${ROOT}unpack_source_archive.sh" "${PRGNAME}" || exit 1
+
+TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
+DOCDIR="/usr/share/doc/${PRGNAME}-${VERSION}"
+rm -rf "${TMP_DIR}"
+mkdir -pv "${TMP_DIR}${DOCDIR}"
 
 # не будем устанавливать некоторые ненужные файлы из группы extras
 sed -i 's/extras//' Makefile.in
@@ -23,11 +28,6 @@ sed -i 's/extras//' Makefile.in
 make || exit 1
 make check
 make install
-
-TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
-DOCDIR="/usr/share/doc/${PRGNAME}-${VERSION}"
-rm -rf "${TMP_DIR}"
-mkdir -pv "${TMP_DIR}${DOCDIR}"
 make install DESTDIR="${TMP_DIR}"
 
 # установим документацию
