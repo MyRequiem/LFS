e86da099a9e2b490589ee43ad7fbf19c1921523c
Vladimir <mrvladislavovich@gmail.com>
2020-04-09 10:57:19 +0400

    LFS: 02_build_basic_system/066_libpipeline.sh updated

--- lfs-stable-x86_64/02_build_basic_system/066_libpipeline.sh
+++ lfs-stable-x86_64/02_build_basic_system/066_libpipeline.sh
@@ -6,14 +6,18 @@ PRGNAME="libpipeline"
 # Пакет содержит библиотеку для манипулирования конвейерами подпроцессов гибким
 # и удобным способом
 
-# http://www.linuxfromscratch.org/lfs/view/9.0/chapter06/libpipeline.html
+# http://www.linuxfromscratch.org/lfs/view/stable/chapter06/libpipeline.html
 
 # Home page: http://libpipeline.nongnu.org/
-# Download:  http://download.savannah.gnu.org/releases/libpipeline/libpipeline-1.5.1.tar.gz
+# Download:  http://download.savannah.gnu.org/releases/libpipeline/libpipeline-1.5.2.tar.gz
 
 ROOT="/"
 source "${ROOT}check_environment.sh"                  || exit 1
 source "${ROOT}unpack_source_archive.sh" "${PRGNAME}" || exit 1
+
+TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
+rm -rf "${TMP_DIR}"
+mkdir -pv "${TMP_DIR}"
 
 ./configure \
     --prefix=/usr || exit 1
@@ -21,10 +25,6 @@ source "${ROOT}unpack_source_archive.sh" "${PRGNAME}" || exit 1
 make || exit 1
 make check
 make install
-
-TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
-rm -rf "${TMP_DIR}"
-mkdir -pv "${TMP_DIR}"
 make install DESTDIR="${TMP_DIR}"
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
