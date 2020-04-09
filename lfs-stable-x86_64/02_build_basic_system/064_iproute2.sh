88d258cf84ec8094cced7e1870ab7962ba837c2c
Vladimir <mrvladislavovich@gmail.com>
2020-04-09 10:38:04 +0400

    LFS: 02_build_basic_system/064_iproute2.sh updated

--- lfs-stable-x86_64/02_build_basic_system/064_iproute2.sh
+++ lfs-stable-x86_64/02_build_basic_system/064_iproute2.sh
@@ -6,33 +6,33 @@ PRGNAME="iproute2"
 # Инструменты, используемые для администрирования многих расширенных функций
 # IPv4-маршрутизации ядра linux
 
-# http://www.linuxfromscratch.org/lfs/view/9.0/chapter06/iproute2.html
+# http://www.linuxfromscratch.org/lfs/view/stable/chapter06/iproute2.html
 
 # Home page: https://www.kernel.org/pub/linux/utils/net/iproute2/
-# Download:  https://www.kernel.org/pub/linux/utils/net/iproute2/iproute2-5.2.0.tar.xz
+# Download:  https://www.kernel.org/pub/linux/utils/net/iproute2/iproute2-5.5.0.tar.xz
 
 ROOT="/"
 source "${ROOT}check_environment.sh"                  || exit 1
 source "${ROOT}unpack_source_archive.sh" "${PRGNAME}" || exit 1
 
+TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
+rm -rf "${TMP_DIR}"
+mkdir -pv "${TMP_DIR}"
+
 # программу arpd создавать не будем, так как она зависит от Berkeley DB,
 # которая у нас в LFS системе не установлена
-sed -i /ARPD/d Makefile
+sed -i /ARPD/d Makefile || exit 1
 # тем не менее man-страницы для arpd все равно будут установлены, удалим их
 rm -fv man/man8/arpd.8
 
 # также необходимо отключить сборку двух модулей, для которых требуется
 # iptables
-sed -i 's/.m_ipt.o//' tc/Makefile
+sed -i 's/.m_ipt.o//' tc/Makefile || exit 1
 
 make
-# пакет не содержит наборы тестов, поэтому сразу устанавливаем
+# пакет не содержит набора тестов, поэтому сразу устанавливаем
 DOC_DIR="/usr/share/doc/${PRGNAME}-${VERSION}"
 make DOCDIR="${DOC_DIR}" install
-
-TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
-rm -rf "${TMP_DIR}"
-mkdir -pv "${TMP_DIR}"
 make DOCDIR="${DOC_DIR}" install DESTDIR="${TMP_DIR}"
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
