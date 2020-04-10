2b063aa1af3dfdbc678678cc6c68597c44b22838
Vladimir <mrvladislavovich@gmail.com>
2020-04-10 13:30:46 +0400

    LFS: 04_making_LFS_system_bootable/002_etc-lfs-release.sh updated

--- lfs-stable-x86_64/04_making_LFS_system_bootable/002_etc-lfs-release.sh
+++ lfs-stable-x86_64/04_making_LFS_system_bootable/002_etc-lfs-release.sh
@@ -1,19 +1,21 @@
 #! /bin/bash
 
-PRGNAME="lfs-release"
-VERSION="9.0"
+PRGNAME="etc-lfs-release"
+VERSION="stable"
 
-### lfs-release
+### /etc/lfs-release (system info)
 # /etc/lfs-release - содержит версию LFS системы
 # /etc/lsb-release - информация о системе
+# /etc/os-release  - информация о системе, которая используется некоторыми
+#                       графическими средами рабочего стола
 
-# http://www.linuxfromscratch.org/lfs/view/9.0/chapter09/theend.html
+# http://www.linuxfromscratch.org/lfs/view/stable/chapter09/theend.html
 
 ROOT="/"
 source "${ROOT}check_environment.sh"      || exit 1
 source "${ROOT}config_file_processing.sh" || exit 1
 
-TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
+TMP_DIR="/tmp/pkg-${PRGNAME}"
 rm -rf "${TMP_DIR}"
 mkdir -pv "${TMP_DIR}/etc"
 
@@ -43,13 +45,30 @@ EOF
 cp "${LSB_RELEASE}" "${TMP_DIR}/etc/"
 config_file_processing "${LSB_RELEASE}"
 
-cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+OS_RELEASE="/etc/os-release"
+if [ -f "${OS_RELEASE}" ]; then
+    mv "${OS_RELEASE}" "${OS_RELEASE}.old"
+fi
+
+cat << EOF > "${OS_RELEASE}"
+NAME="Linux From Scratch"
+VERSION="${VERSION}"
+ID=lfs
+PRETTY_NAME="Linux From Scratch ${VERSION}"
+VERSION_CODENAME="MyRequiem-LFS"
+EOF
+
+cp "${OS_RELEASE}" "${TMP_DIR}/etc/"
+config_file_processing "${OS_RELEASE}"
+
+cat << EOF > "/var/log/packages/${PRGNAME}"
 # Package: ${PRGNAME} (system info)
 #
 # /etc/lfs-release
 # /etc/lsb-release
+# /etc/os-release
 #
 EOF
 
 source "${ROOT}/write_to_var_log_packages.sh" \
-    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
+    "${TMP_DIR}" "${PRGNAME}"
