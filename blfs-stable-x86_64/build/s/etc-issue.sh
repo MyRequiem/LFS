aff73a1f4a9ee77a8cc96baf3ab28abd6bb4bb84
Vladimir <mrvladislavovich@gmail.com>
2020-04-10 23:47:15 +0400

    BLFS: build/s/etc-issue.sh updated

--- blfs-stable-x86_64/build/s/etc-issue.sh
+++ blfs-stable-x86_64/build/s/etc-issue.sh
@@ -1,20 +1,19 @@
 #! /bin/bash
 
-PRGNAME="etc_issue"
-VERSION="9.0"
+PRGNAME="etc-issue"
 
-### /etc/issue
+### /etc/issue (pre-login message)
 # Файл /etc/issue содержит сообщения, которые выводятся до приглашения на вход
 # в систему. Он может содержать различные последовательности @char и \char,
 # которые читает утилита agetty
 
-# http://www.linuxfromscratch.org/blfs/view/9.0/postlfs/logon.html
+# http://www.linuxfromscratch.org/blfs/view/stable/postlfs/logon.html
 
-ROOT="/"
-source "${ROOT}check_environment.sh"      || exit 1
-source "${ROOT}config_file_processing.sh" || exit 1
+ROOT="/root"
+source "${ROOT}/check_environment.sh"      || exit 1
+source "${ROOT}/config_file_processing.sh" || exit 1
 
-TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
+TMP_DIR="/tmp/build-${PRGNAME}/package-${PRGNAME}"
 rm -rf "${TMP_DIR}"
 mkdir -pv "${TMP_DIR}/etc"
 
@@ -38,21 +37,21 @@ fi
 # \m   architecture identifier of the machine, e.g., x86_64
 # \n   nodename of the machine, also known as the hostname, e.g. lfs
 # \o   domainname of the machine
-# \r   release number of the kernel, e.g., 5.2.21
+# \r   release number of the kernel, e.g., 5.5.15
 # \t   current time
 # \u   number of current users logged in
 # \U   string "N user" where N is the number of current users logged in
 # \v   version of the OS, e.g., the build-date etc, e.g.
 #       #2 SMP Fri Jun 24 13:38:27 CDT 2016
 
-# Linux 5.2.21 x86_64
-# Wed Mar 18 16:14:39
+# Linux 5.5.15 x86_64
+# Fri Apr 10 [23:23:26]
 printf " \\\s \\\r \\\m\\n \\\d [\\\t]\\n\\n" > "${ISSUE}"
 
 cp "${ISSUE}" "${TMP_DIR}/etc/"
 config_file_processing "${ISSUE}"
 
-cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+cat << EOF > "/var/log/packages/${PRGNAME}"
 # Package: ${PRGNAME} (pre-login message)
 #
 # /etc/issue is a text file which contains a message or system identification
@@ -63,4 +62,4 @@ cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 EOF
 
 source "${ROOT}/write_to_var_log_packages.sh" \
-    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
+    "${TMP_DIR}" "${PRGNAME}"
