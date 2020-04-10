4709c5e6c758aa66ad5849728c988be6c6b19984
Vladimir <mrvladislavovich@gmail.com>
2020-04-10 13:05:29 +0400

    LFS: 04_making_LFS_system_bootable/001_etc-fstab.sh updated

--- lfs-stable-x86_64/04_making_LFS_system_bootable/001_etc-fstab.sh
+++ lfs-stable-x86_64/04_making_LFS_system_bootable/001_etc-fstab.sh
@@ -1,19 +1,20 @@
 #! /bin/bash
 
-PRGNAME="etc_fstab"
-VERSION="9.0"
+PRGNAME="etc-fstab"
+SWAP_PART="/dev/sda6"
+ROOT_PART="/dev/sda10"
 
-### etc_fstab
+### /etc/fstab (partition mount settings)
 # /etc/fstab - файл в котором хранятся настройки монтирования различных
 # разделов, включая корень и swap
 
-# http://www.linuxfromscratch.org/lfs/view/9.0/chapter08/fstab.html
+# http://www.linuxfromscratch.org/lfs/view/stable/chapter08/fstab.html
 
 ROOT="/"
 source "${ROOT}check_environment.sh"      || exit 1
 source "${ROOT}config_file_processing.sh" || exit 1
 
-TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
+TMP_DIR="/tmp/pkg-${PRGNAME}"
 rm -rf "${TMP_DIR}"
 mkdir -pv "${TMP_DIR}/etc"
 
@@ -27,8 +28,8 @@ cat << EOF > "${FSTAB}"
 
 # File System    mount-point    type        options             dump fsck order
 # ------------------------------------------------------------------------------
-/dev/sda6        swap           swap        defaults            0       0
-/dev/sda10       /              ext4        defaults            1       1
+${SWAP_PART}        swap           swap        defaults            0       0
+${ROOT_PART}       /              ext4        defaults            1       1
 proc             /proc          proc        nosuid,noexec,nodev 0       0
 sysfs            /sys           sysfs       nosuid,noexec,nodev 0       0
 devpts           /dev/pts       devpts      gid=5,mode=620      0       0
@@ -41,7 +42,7 @@ EOF
 cp "${FSTAB}" "${TMP_DIR}/etc/"
 config_file_processing "${FSTAB}"
 
-cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+cat << EOF > "/var/log/packages/${PRGNAME}"
 # Package: ${PRGNAME} (partition mount settings)
 #
 # /etc/fstab
@@ -49,4 +50,4 @@ cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 EOF
 
 source "${ROOT}/write_to_var_log_packages.sh" \
-    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
+    "${TMP_DIR}" "${PRGNAME}"
