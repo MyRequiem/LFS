8d7e2da3119e9fd7721c1dca3cc70f064af2cf27
Vladimir <mrvladislavovich@gmail.com>
2020-04-13 19:43:51 +0400

    BLFS: build/a/unzip.sh updated

--- blfs-stable-x86_64/build/a/unzip.sh
+++ blfs-stable-x86_64/build/a/unzip.sh
@@ -6,7 +6,7 @@ VERSION="6.0"
 ### UnZip
 # Утилиты для распаковки ZIP-архивов
 
-# http://www.linuxfromscratch.org/blfs/view/svn/general/unzip.html
+# http://www.linuxfromscratch.org/blfs/view/stable/general/unzip.html
 
 # Home page: https://sourceforge.net/projects/infozip/
 # Download:  https://downloads.sourceforge.net/infozip/unzip60.tar.gz
@@ -15,25 +15,24 @@ VERSION="6.0"
 # Required: no
 # Optional: no
 
-ROOT="/"
-source "${ROOT}check_environment.sh" || exit 1
+ROOT="/root"
+source "${ROOT}/check_environment.sh" || exit 1
 
-TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
-rm -rf "${TMP_DIR}"
-mkdir -pv "${TMP_DIR}"
-
-SOURCES="/sources"
-BUILD_DIR="${SOURCES}/build"
-mkdir -p "${BUILD_DIR}"
+SOURCES="/root/src"
+BUILD_DIR="/tmp/build-${PRGNAME}-${VERSION}"
+rm -rf "${BUILD_DIR}"
+mkdir -pv "${BUILD_DIR}"
 cd "${BUILD_DIR}" || exit 1
-rm -rf unzip60
 
 tar xvf "${SOURCES}/unzip60".tar.?z* || exit 1
 cd unzip60 || exit 1
 
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
 # применим патч
 patch --verbose -Np1 -i \
-    "/sources/${PRGNAME}-${VERSION}-consolidated_fixes-1.patch" || exit 1
+    "${SOURCES}/${PRGNAME}-${VERSION}-consolidated_fixes-1.patch" || exit 1
 
 # собираем пакет
 make -f unix/Makefile generic || exit 1
