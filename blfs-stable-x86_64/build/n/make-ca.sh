35a096f5f7e156c26f07a134b7edee046d902d95
Vladimir <mrvladislavovich@gmail.com>
2020-04-14 11:21:08 +0400

    BLFS: build/n/make-ca.sh updated

--- blfs-stable-x86_64/build/n/make-ca.sh
+++ blfs-stable-x86_64/build/n/make-ca.sh
@@ -6,79 +6,65 @@ PRGNAME="make-ca"
 # Инфраструктура открытых ключей (PKI) и методы проверки их подлинности в
 # ненадежных сетях.
 
-# http://www.linuxfromscratch.org/blfs/view/9.0/postlfs/make-ca.html
+# http://www.linuxfromscratch.org/blfs/view/stable/postlfs/make-ca.html
 
 # Home page: https://github.com/djlucas/make-ca/
-# Download:  https://github.com/djlucas/make-ca/releases/download/v1.4/make-ca-1.4.tar.xz
+# Download:  https://github.com/djlucas/make-ca/releases/download/v1.5/make-ca-1.5.tar.xz
+#            http://www.cacert.org/certs/root.crt
+#            http://www.cacert.org/certs/class3.crt
 
 # Required: p11-kit (требуется во время выполнения для создания хранилищ
 #               сертификатов из якорей доверия)
 # Optional: java или openjdk (для создания java PKCS#12 хранилища)
 #           nss (для создания общей NSSDB)
 
-ROOT="/"
-source "${ROOT}check_environment.sh"                  || exit 1
-source "${ROOT}unpack_source_archive.sh" "${PRGNAME}" || exit 1
+ROOT="/root"
+source "${ROOT}/check_environment.sh"                  || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
 
-TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
-rm -rf "${TMP_DIR}"
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
 mkdir -pv "${TMP_DIR}"
 
 make install
 make install DESTDIR="${TMP_DIR}"
+
 install -vdm755 /etc/ssl/local
 install -vdm755 "${TMP_DIR}/etc/ssl/local"
 
 /usr/sbin/make-ca -g
-
-ln -svf /etc/pki/tls/certs/ca-bundle.crt /etc/ssl/ca-bundle.crt
-(
-    cd "${TMP_DIR}/etc/ssl" || exit 1
-    ln -svf ../pki/tls/certs/ca-bundle.crt ca-bundle.crt
-)
 
 install -vdm755 /etc/cron.weekly
 install -vdm755 "${TMP_DIR}/etc/cron.weekly"
 
 cat > /etc/cron.weekly/update-pki.sh << "EOF"
 #!/bin/bash
+
 /usr/sbin/make-ca -g
 EOF
 chmod 754 /etc/cron.weekly/update-pki.sh
 
 cat > "${TMP_DIR}/etc/cron.weekly/update-pki.sh" << "EOF"
 #!/bin/bash
+
 /usr/sbin/make-ca -g
 EOF
 chmod 754 "${TMP_DIR}/etc/cron.weekly/update-pki.sh"
 
-wget http://www.cacert.org/certs/root.crt
-wget http://www.cacert.org/certs/class3.crt
+CACERT_CLASS_1_ROOT="/etc/ssl/local/CAcert_Class_1_root.pem"
+openssl x509 -in "${SOURCES}/root.crt" -text -fingerprint -setalias \
+    "CAcert Class 1 root" -addtrust serverAuth -addtrust emailProtection \
+    -addtrust codeSigning > "${CACERT_CLASS_1_ROOT}"
+touch "${TMP_DIR}${CACERT_CLASS_1_ROOT}"
 
-openssl x509 -in root.crt -text -fingerprint -setalias "CAcert Class 1 root" \
-    -addtrust serverAuth -addtrust emailProtection -addtrust codeSigning > \
-    /etc/ssl/local/CAcert_Class_1_root.pem
-touch "${TMP_DIR}/etc/ssl/local/CAcert_Class_1_root.pem"
-
-openssl x509 -in class3.crt -text -fingerprint -setalias "CAcert Class 3 root" \
-    -addtrust serverAuth -addtrust emailProtection -addtrust codeSigning > \
-    /etc/ssl/local/CAcert_Class_3_root.pem
-touch "${TMP_DIR}/etc/ssl/local/CAcert_Class_3_root.pem"
-
-/usr/sbin/make-ca -r -f
-
-openssl x509 -in /etc/ssl/certs/Makebelieve_CA_Root.pem \
-    -text \
-    -fingerprint \
-    -setalias "Disabled Makebelieve CA Root" \
-    -addreject serverAuth \
-    -addreject emailProtection \
-    -addreject codeSigning > /etc/ssl/local/Disabled_Makebelieve_CA_Root.pem
-touch "${TMP_DIR}/etc/ssl/local/Disabled_Makebelieve_CA_Root.pem"
+CACERT_CLASS_3_ROOT="/etc/ssl/local/CAcert_Class_3_root.pem"
+openssl x509 -in "${SOURCES}/class3.crt" -text -fingerprint -setalias \
+    "CAcert Class 3 root" -addtrust serverAuth -addtrust emailProtection \
+    -addtrust codeSigning > "${CACERT_CLASS_3_ROOT}"
+touch "${TMP_DIR}${CACERT_CLASS_3_ROOT}"
 
 /usr/sbin/make-ca -r -f
 
-cp -R /etc/pki "${TMP_DIR}/etc"
+cp -vR /etc/pki "${TMP_DIR}/etc"
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Package: ${PRGNAME} (deliver and manage a complete PKI configuration)
