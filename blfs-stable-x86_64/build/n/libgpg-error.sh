084e2e03bb18c902cfd0c36bf69a096d9f7424a8
Vladimir <mrvladislavovich@gmail.com>
2020-04-16 00:19:50 +0400

    BLFS: build/n/libgpg-error.sh updated

--- blfs-stable-x86_64/build/n/libgpg-error.sh
+++ blfs-stable-x86_64/build/n/libgpg-error.sh
@@ -7,25 +7,20 @@ PRGNAME="libgpg-error"
 # GnuPG. Среди них GPG, GPGSM, GPGME, GPG-Agent, libgcrypt, Libksba, DirMngr,
 # Pinentry, SmartCard Daemon и многое другое.
 
-# http://www.linuxfromscratch.org/blfs/view/9.0/general/libgpg-error.html
+# http://www.linuxfromscratch.org/blfs/view/stable/general/libgpg-error.html
 
 # Home page: https://www.gnupg.org/related_software/libgpg-error/index.en.html
-# Download:  https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.36.tar.bz2
+# Download:  https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.37.tar.bz2
 
 # Required: no
 # Optional: no
 
-ROOT="/"
-source "${ROOT}check_environment.sh"                  || exit 1
-source "${ROOT}unpack_source_archive.sh" "${PRGNAME}" || exit 1
+ROOT="/root"
+source "${ROOT}/check_environment.sh"                  || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
 
-DOCS="/usr/share/doc/${PRGNAME}-${VERSION}"
-TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
-rm -rf "${TMP_DIR}"
-mkdir -pv "${TMP_DIR}${DOCS}"
-
-# разрешим сборку с gawk-5.0
-sed -i 's/namespace/pkg_&/' src/Makefile.{am,in} src/mkstrtable.awk
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
 
 ./configure \
     --prefix=/usr || exit 1
@@ -35,6 +30,8 @@ make || exit 1
 make install
 make install DESTDIR="${TMP_DIR}"
 
+# документация
+DOCS="/usr/share/doc/${PRGNAME}-${VERSION}"
 install -v -m644 -D README "${DOCS}/README"
 install -v -m644 -D README "${TMP_DIR}${DOCS}/README"
 
@@ -45,8 +42,8 @@ cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Among these are GPG, GPGSM, GPGME, GPG-Agent, libgcrypt, Libksba, DirMngr,
 # Pinentry, SmartCard Daemon, and more.
 #
-# Home page: https://www.gnupg.org/related_software/libgpg-error/index.en.html
-# Download:  https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.36.tar.bz2
+# Home page: https://www.gnupg.org/related_software/${PRGNAME}/index.en.html
+# Download:  https://www.gnupg.org/ftp/gcrypt/${PRGNAME}/${PRGNAME}-${VERSION}.tar.bz2
 #
 EOF
 
