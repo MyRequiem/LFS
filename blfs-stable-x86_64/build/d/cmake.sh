2da3504b0fcc1c5a5fc2cf1200962631801f6717
Vladimir <mrvladislavovich@gmail.com>
2020-04-15 11:38:28 +0400

    BLFS: build/d/cmake.sh updated

--- blfs-stable-x86_64/build/d/cmake.sh
+++ blfs-stable-x86_64/build/d/cmake.sh
@@ -2,13 +2,13 @@
 
 PRGNAME="cmake"
 
-### CMake
+### CMake (cross-platform, open-source make system)
 # Современный набор инструментов, используемый для генерации Makefile
 
-# http://www.linuxfromscratch.org/blfs/view/9.0/general/cmake.html
+# http://www.linuxfromscratch.org/blfs/view/stable/general/cmake.html
 
 # Home page: https://cmake.org/
-# Download:  https://cmake.org/files/v3.15/cmake-3.15.2.tar.gz
+# Download:  https://cmake.org/files/v3.16/cmake-3.16.4.tar.gz
 
 # Required:    libuv
 # Recommended: zlib
@@ -16,16 +16,15 @@ PRGNAME="cmake"
 #              curl
 #              expat
 #              libarchive
-# Optional:    qt5        (для Qt-based GUI)
+# Optional:    qt5        (для Qt-based GUI, см. опцию конфигурации ниже)
 #              subversion (для тестов)
-#              sphinx     (для сборки документации)
+#              sphinx     (для сборки документации) https://pypi.org/project/Sphinx/
 
-ROOT="/"
-source "${ROOT}check_environment.sh"                  || exit 1
-source "${ROOT}unpack_source_archive.sh" "${PRGNAME}" || exit 1
+ROOT="/root"
+source "${ROOT}/check_environment.sh"                  || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
 
-TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
-rm -rf "${TMP_DIR}"
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
 mkdir -pv "${TMP_DIR}"
 
 # запрещаем приложениям, использующим cmake при сборке устанавливать файлы в
@@ -35,30 +34,35 @@ sed -i '/"lib64"/s/64//' Modules/GNUInstallDirs.cmake || exit 1
 # заставляет CMake связываться с Zlib, Bzip2, cURL, Expat и libarchive которые
 # уже установлены в системе
 #    --system-libs
-# не собирать GUI на основе Qt
-#    --no-qt-gui
 # использовать внутреннюю версию библиотеки JSON-C++ вместо системной
 #    --no-system-jsoncpp
 ./bootstrap              \
     --prefix=/usr        \
     --system-libs        \
-    --no-qt-gui          \
     --mandir=/share/man  \
     --no-system-jsoncpp  \
     --no-system-librhash \
     --docdir="/share/doc/${PRGNAME}-${VERSION}" || exit 1
 
+###
+# если установлен пакет qt5, то добавляем
+#    --qt-gui
+# для создания графического интерфейса CMake
+###
+
 make || exit 1
 
 # тесты
-# известно что тест RunCMake.CommandLineTar не проходит
+# известно что тест RunCMake.CommandLineTar завершается ошибкой, если пакет
+# zstd (из lfs) не установлен
+# unset LANG
 # NUMJOBS="$(($(nproc) + 1))"
 # bin/ctest -j"${NUMJOBS}" -O "${PRGNAME}-${VERSION}-test.log"
 
 make install
 make install DESTDIR="${TMP_DIR}"
 
-VER="$(echo "${VERSION}" | cut -d . -f 1,2)"
+MAJ_VERSION="$(echo "${VERSION}" | cut -d . -f 1,2)"
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Package: ${PRGNAME} (cross-platform, open-source make system)
 #
@@ -68,7 +72,7 @@ cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # workspaces that can be used in the compiler environment of your choice.
 #
 # Home page: https://cmake.org/
-# Download:  https://cmake.org/files/v${VER}/${PRGNAME}-${VERSION}.tar.gz
+# Download:  https://cmake.org/files/v${MAJ_VERSION}/${PRGNAME}-${VERSION}.tar.gz
 #
 EOF
 
