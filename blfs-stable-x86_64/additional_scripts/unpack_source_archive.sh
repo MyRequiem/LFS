57c9ca24b9f899c8cfa999a3732d4664f9e6a1ed
Vladimir <mrvladislavovich@gmail.com>
2020-04-16 12:40:53 +0400

    BLFS: additional_scripts/unpack_source_archive.sh updated

--- blfs-stable-x86_64/additional_scripts/unpack_source_archive.sh
+++ blfs-stable-x86_64/additional_scripts/unpack_source_archive.sh
@@ -10,6 +10,11 @@ if [ -z "${VERSION}" ]; then
 fi
 
 if [ -z "${VERSION}" ]; then
+    VERSION="$(find ${SOURCES} -type f -name "${SRC_ARCH_NAME}-*.t?z" \
+        2>/dev/null | head -n 1 | rev | cut -d . -f 2- | cut -d - -f 1 | rev)"
+fi
+
+if [ -z "${VERSION}" ]; then
     echo "Can not determine the package version of ${SRC_ARCH_NAME}"
     exit 1
 fi
@@ -19,5 +24,6 @@ rm -rf "${BUILD_DIR}"
 mkdir -pv "${BUILD_DIR}"
 cd "${BUILD_DIR}" || exit 1
 
-tar xvf "${SOURCES}/${SRC_ARCH_NAME}-${VERSION}"*.tar.?z* || exit 1
+tar xvf "${SOURCES}/${SRC_ARCH_NAME}-${VERSION}"*.tar.?z* || \
+    tar xvf "${SOURCES}/${SRC_ARCH_NAME}-${VERSION}"*.t?z || exit 1
 cd "${SRC_ARCH_NAME}-${VERSION}" || exit 1
