26cd16eb7f16e681bbaddbf222a0ee2d814ba8ef
Vladimir <mrvladislavovich@gmail.com>
2020-04-16 20:23:26 +0400

    BLFS: build/l/glib.sh updated

--- blfs-stable-x86_64/build/l/glib.sh
+++ blfs-stable-x86_64/build/l/glib.sh
@@ -2,40 +2,43 @@
 
 PRGNAME="glib"
 
-### GLib
+### GLib (library of C routines)
 # Библиотеки низкого уровня, которые включают подпрограммы поддержки C, такие
 # как списки, деревья, хэши, распределение памяти и многое другое.
 
-# http://www.linuxfromscratch.org/blfs/view/svn/general/glib2.html
+# http://www.linuxfromscratch.org/blfs/view/stable/general/glib2.html
 
 # Home page: https://www.gtk.org/
-# Download:  http://ftp.gnome.org/pub/gnome/sources/glib/2.64/glib-2.64.0.tar.xz
-# Patch:     http://www.linuxfromscratch.org/patches/blfs/svn/glib-2.64.0-skip_warnings-1.patch
+# Download:  http://ftp.gnome.org/pub/gnome/sources/glib/2.62/glib-2.62.4.tar.xz
+# Patches:   http://www.linuxfromscratch.org/patches/blfs/9.1/glib-2.62.4-cve_2020_6750_fix-1.patch
+#            http://www.linuxfromscratch.org/patches/blfs/9.1/glib-2.62.4-skip_warnings-1.patch
 
 # Required:    no
 # Recommended: libxslt
 #              pcre
-# Optional:    dbus
-#              bindfs
+# Optional:    dbus   (для некоторых тестов)
+#              bindfs (для некоторых тестов) https://bindfs.org/
 #              gdb
 #              docbook-xml
 #              docbook-xsl
-#              gtk-doc
-# Additional Runtime Dependencies: gobject-introspection
+#              gtk-doc (для сборки API документации, см. конфигурацию ниже)
+#              gobject-introspection
+#              desktop-file-utils (для тестов)
+#              shared-mime-info   (для тестов)
 
-ROOT="/"
-source "${ROOT}check_environment.sh"                  || exit 1
-source "${ROOT}unpack_source_archive.sh" "${PRGNAME}" || exit 1
+ROOT="/root"
+source "${ROOT}/check_environment.sh"                  || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
 
-TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
-rm -rf "${TMP_DIR}"
-mkdir -pv "${TMP_DIR}"
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+DOCS="/usr/share/doc/${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}${DOCS}"
 
 # Во многих случаях приложения, которые используют эту библиотеку, прямо или
 # косвенно через другие библиотеки, такие как GTK+-3, выводят многочисленные
-# предупреждения при запуске из командной строки. Применение патча позволяет
-# использовать переменную среды GLIB_LOG_LEVEL, которая подавляет нежелательные
-# сообщения.
+# предупреждения при запуске из командной строки. Применение следующего патча
+# позволяет использовать переменную среды GLIB_LOG_LEVEL, которая подавляет
+# нежелательные сообщения.
 #    1 - Alert
 #    2 - Critical
 #    3 - Error
@@ -43,39 +46,43 @@ mkdir -pv "${TMP_DIR}"
 #    5 - Notice
 # Например, не выводить сообщения Warning и Notice
 #    export GLIB_LOG_LEVEL=4
-patch -Np1 -i /sources/glib-2.64.0-skip_warnings-1.patch || exit 1
+patch --verbose -Np1 -i \
+    "${SOURCES}/${PRGNAME}-${VERSION}-skip_warnings-1.patch" || exit 1
+
+# исправление уязвимости обхода прокси
+patch --verbose -Np1 -i \
+    "${SOURCES}/${PRGNAME}-${VERSION}-cve_2020_6750_fix-1.patch" || exit 1
 
 mkdir build
 cd build || exit 1
 
-# устанавливать документацию
+# устанавливать man-страницы
 #    -Dman=true
 # отключить поддержку selinux
 #    -Dselinux=disabled
-# устанавливать API документацию
-#    -Ddoc=true
 meson                  \
     --prefix=/usr      \
     -Dman=true         \
     -Dselinux=disabled \
     .. || exit 1
 
+# если установлен пакет gtk-doc, то можно собрать и установить API документацию
+# добавив опцию конфигурации
+#    -Ddoc=true
+
 ninja || exit 1
-# в конце сборки в консоль будут выводится ошибки типа:
-# Error: no ID for constraint linkend: "G-VARIANT-TYPE-BOOLEAN:CAPS"
-# Такие ошибки допустимы.
+# в конце сборки в консоль могут выводится ошибки типа:
+#    Error: no ID for constraint linkend: ...
+# Такие ошибки допустимы и безвредны.
 
 ninja install
 DESTDIR=${TMP_DIR} ninja install
 
-DOCS="/usr/share/doc/${PRGNAME}-${VERSION}"
 mkdir -pv "${DOCS}"
-mkdir -pv "${TMP_DIR}${DOCS}"
-
 cp -vR ../docs/reference/{NEWS,gio,glib,gobject} "${DOCS}"
 cp -vR ../docs/reference/{NEWS,gio,glib,gobject} "${TMP_DIR}${DOCS}"
 
-# для запуска тестов требуются 2 пакета, которые пока не установлены
+# для запуска тестов требуются 2 установленных пакета:
 #    desktop-file-utils
 #    shared-mime-info
 #
