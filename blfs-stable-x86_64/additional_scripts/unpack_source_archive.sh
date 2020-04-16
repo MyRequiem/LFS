c59b5fccea1c866eb189e3a966b889b2077f49e9
Vladimir <mrvladislavovich@gmail.com>
2020-04-16 13:46:46 +0400

    BLFS: additional_scripts/unpack_source_archive.sh updated

--- blfs-stable-x86_64/additional_scripts/unpack_source_archive.sh
+++ blfs-stable-x86_64/additional_scripts/unpack_source_archive.sh
@@ -5,12 +5,20 @@ VERSION="$2"
 SOURCES="/root/src"
 
 if [ -z "${VERSION}" ]; then
+    ARCH_TYPE=".tar.?z"
     VERSION="$(find ${SOURCES} -type f -name "${SRC_ARCH_NAME}-*.tar.?z*" \
         2>/dev/null | head -n 1 | rev | cut -d . -f 3- | cut -d - -f 1 | rev)"
 fi
 
 if [ -z "${VERSION}" ]; then
+    ARCH_TYPE=".t?z"
     VERSION="$(find ${SOURCES} -type f -name "${SRC_ARCH_NAME}-*.t?z" \
+        2>/dev/null | head -n 1 | rev | cut -d . -f 2- | cut -d - -f 1 | rev)"
+fi
+
+if [ -z "${VERSION}" ]; then
+    ARCH_TYPE=".zip"
+    VERSION="$(find ${SOURCES} -type f -name "${SRC_ARCH_NAME}-*.zip" \
         2>/dev/null | head -n 1 | rev | cut -d . -f 2- | cut -d - -f 1 | rev)"
 fi
 
@@ -24,6 +32,13 @@ rm -rf "${BUILD_DIR}"
 mkdir -pv "${BUILD_DIR}"
 cd "${BUILD_DIR}" || exit 1
 
-tar xvf "${SOURCES}/${SRC_ARCH_NAME}-${VERSION}"*.tar.?z* || \
+if [[ "${ARCH_TYPE}" == ".tar.?z" ]]; then
+    tar xvf "${SOURCES}/${SRC_ARCH_NAME}-${VERSION}"*.tar.?z* || exit 1
+elif [[ "${ARCH_TYPE}" == ".t?z" ]]; then
     tar xvf "${SOURCES}/${SRC_ARCH_NAME}-${VERSION}"*.t?z || exit 1
+elif [[ "${ARCH_TYPE}" == ".zip" ]]; then
+    unzip -d "${SRC_ARCH_NAME}-${VERSION}" \
+        "${SOURCES}/${SRC_ARCH_NAME}-${VERSION}"*.zip || exit 1
+fi
+
 cd "${SRC_ARCH_NAME}-${VERSION}" || exit 1
