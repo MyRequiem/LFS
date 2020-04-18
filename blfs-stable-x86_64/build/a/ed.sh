2fa9aab30a4671acb780e5a9fb6e86e6c1ad8e41
Vladimir <mrvladislavovich@gmail.com>
2020-04-18 15:55:15 +0400

    BLFS: build/a/ed.sh updated

--- blfs-stable-x86_64/build/a/ed.sh
+++ blfs-stable-x86_64/build/a/ed.sh
@@ -2,13 +2,13 @@
 
 PRGNAME="ed"
 
-### GNU Ed
+### GNU Ed (The GNU ed line editor)
 # Реализация стандартного строкового редактора Unix. Используется для создания,
 # отображения, изменения тектовых файлов как в интерактивном режиме, так и с
 # помощью сценариев оболочки. Так же используется утилитой patch, если файл
 # *.patch создан с помощью Ed.
 
-# http://www.linuxfromscratch.org/blfs/view/9.0/postlfs/ed.html
+# http://www.linuxfromscratch.org/blfs/view/stable/postlfs/ed.html
 
 # Home page: http://www.gnu.org/software/ed/
 # Download:  https://ftp.gnu.org/gnu/ed/ed-1.15.tar.lz
@@ -17,12 +17,11 @@ PRGNAME="ed"
 #           lzip (для распаковки архива с исходниками в формате .tar.lz)
 # Optional: no
 
-ROOT="/"
-source "${ROOT}check_environment.sh"                  || exit 1
-source "${ROOT}unpack_source_archive.sh" "${PRGNAME}" || exit 1
+ROOT="/root"
+source "${ROOT}/check_environment.sh"                  || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
 
-TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
-rm -rf "${TMP_DIR}"
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
 mkdir -pv "${TMP_DIR}"
 
 ./configure       \
@@ -35,7 +34,7 @@ make install
 make install DESTDIR="${TMP_DIR}"
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
-# Package: ${PRGNAME} (text editor)
+# Package: ${PRGNAME} (The GNU ed line editor)
 #
 # GNU Ed is an 8-bit clean, more or less POSIX-compliant implementation of the
 # standard Unix line-oriented text editor. It is used to create, display,
