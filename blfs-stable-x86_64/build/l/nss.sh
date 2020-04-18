bc35dec92b02d54c703b95c7842a5995dc6eee07
Vladimir <mrvladislavovich@gmail.com>
2020-04-18 14:17:01 +0400

    BLFS: build/l/nss.sh updated

--- blfs-stable-x86_64/build/l/nss.sh
+++ blfs-stable-x86_64/build/l/nss.sh
@@ -8,27 +8,26 @@ PRGNAME="nss"
 # PKCS#7, PKCS#11, PKCS#12, сертификатов S/MIME, X.509 v3 и других стандартов
 # безопасности.
 
-# http://www.linuxfromscratch.org/blfs/view/svn/postlfs/nss.html
+# http://www.linuxfromscratch.org/blfs/view/stable/postlfs/nss.html
 
 # Home page: https://developer.mozilla.org/ru/docs/NSS
-# Download:  https://archive.mozilla.org/pub/security/nss/releases/NSS_3_51_RTM/src/nss-3.51.tar.gz
-# Patch:     http://www.linuxfromscratch.org/patches/blfs/svn/nss-3.51-standalone-1.patch
+# Download:  https://archive.mozilla.org/pub/security/nss/releases/NSS_3_50_RTM/src/nss-3.50.tar.gz
+# Patch:     http://www.linuxfromscratch.org/patches/blfs/9.1/nss-3.50-standalone-1.patch
 
 # Required:    nspr
 # Recommended: sqlite
 #              p11-kit
 # Optional:    no
 
-ROOT="/"
-source "${ROOT}check_environment.sh"                  || exit 1
-source "${ROOT}unpack_source_archive.sh" "${PRGNAME}" || exit 1
+ROOT="/root"
+source "${ROOT}/check_environment.sh"                  || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
 
-TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
-rm -rf "${TMP_DIR}"
-mkdir -pv "${TMP_DIR}"
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}/usr"/{bin,"include/${PRGNAME}",lib/pkgconfig}
 
 patch -Np1 --verbose -i \
-    "/sources/${PRGNAME}-${VERSION}-standalone-1.patch" || exit 1
+    "${SOURCES}/${PRGNAME}-${VERSION}-standalone-1.patch" || exit 1
 
 cd "${PRGNAME}" || exit 1
 
@@ -55,9 +54,6 @@ make -j1                    \
 
 # тесты уже были запущены во время сборки
 
-install -v -m755 -d "/usr/include/${PRGNAME}"
-install -v -m755 -d "${TMP_DIR}/usr"/{bin,"include/${PRGNAME}",lib/pkgconfig}
-
 cd ../dist || exit 1
 
 install -v -m755 Linux*/lib/*.so              /usr/lib
@@ -65,13 +61,13 @@ install -v -m755 Linux*/lib/*.so              "${TMP_DIR}/usr/lib"
 install -v -m644 Linux*/lib/{*.chk,libcrmf.a} /usr/lib
 install -v -m644 Linux*/lib/{*.chk,libcrmf.a} "${TMP_DIR}/usr/lib"
 
-cp -vRL "{public,private}/${PRGNAME}"/* \
-    "/usr/include/${PRGNAME}"
-cp -vRL "{public,private}/${PRGNAME}"/* \
-    "${TMP_DIR}/usr/include/${PRGNAME}"
+NSS_INCLUDE_DIR="/usr/include/${PRGNAME}"
+install -v -m755 -d "${NSS_INCLUDE_DIR}"
 
-chmod -v 644 "/usr/include/${PRGNAME}"/*
-chmod -v 644 "${TMP_DIR}/usr/include/${PRGNAME}"/*
+cp -vRL "{public,private}/${PRGNAME}"/* "${NSS_INCLUDE_DIR}"
+cp -vRL "{public,private}/${PRGNAME}"/* "${TMP_DIR}${NSS_INCLUDE_DIR}"
+chmod -v 644 "${NSS_INCLUDE_DIR}"/*
+chmod -v 644 "${TMP_DIR}${NSS_INCLUDE_DIR}"/*
 
 install -v -m755 Linux*/bin/{certutil,nss-config,pk12util} /usr/bin
 install -v -m755 Linux*/bin/{certutil,nss-config,pk12util} "${TMP_DIR}/usr/bin"
