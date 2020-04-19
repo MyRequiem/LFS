ddf24487671b76e5bee6e519379a96f44895a887
Vladimir <mrvladislavovich@gmail.com>
2020-04-19 09:58:47 +0400

    BLFS: build/a/libgudev.sh updated

--- blfs-stable-x86_64/build/a/libgudev.sh
+++ blfs-stable-x86_64/build/a/libgudev.sh
@@ -7,27 +7,31 @@ PRGNAME="libgudev"
 # частью udev-extras, затем udev, затем systemd, и потом была выделена в
 # отдельный проект.
 
-# http://www.linuxfromscratch.org/blfs/view/9.0/general/libgudev.html
+# http://www.linuxfromscratch.org/blfs/view/stable/general/libgudev.html
 
 # Home page: http://wiki.gnome.org/Projects/libgudev
 # Download:  http://ftp.gnome.org/pub/gnome/sources/libgudev/233/libgudev-233.tar.xz
 
 # Required: glib
-# Optional: gobject-introspection (for gir-data, needed for GNOME)
-#           gtk-doc
-#           umockdev
+# Optional: gobject-introspection
+#           gtk-doc  (для сборки API документации, см. параметры конфигурации ниже)
+#           umockdev (см. параметры конфигурации ниже) https://github.com/martinpitt/umockdev
 
-ROOT="/"
-source "${ROOT}check_environment.sh"                  || exit 1
-source "${ROOT}unpack_source_archive.sh" "${PRGNAME}" || exit 1
+ROOT="/root"
+source "${ROOT}/check_environment.sh"                  || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
 
-TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
-rm -rf "${TMP_DIR}"
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
 mkdir -pv "${TMP_DIR}"
 
 ./configure       \
     --prefix=/usr \
     --disable-umockdev || exit 1
+
+# если установлен пакет gtk-doc, то добавляем параметр
+#    --enable-gtk-doc
+# если установлен пакет umockdev, то убираем параметр
+#    --disable-umockdev
 
 make || exit 1
 # пакет не имеет набора тестов
