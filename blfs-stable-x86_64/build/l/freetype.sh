dbe0d26db981c63b5313bf61e7eef0d5780a2f16
Vladimir <mrvladislavovich@gmail.com>
2020-04-22 13:27:56 +0400

    BLFS: build/l/freetype.sh updated

--- blfs-stable-x86_64/build/l/freetype.sh
+++ blfs-stable-x86_64/build/l/freetype.sh
@@ -6,30 +6,30 @@ PRGNAME="freetype"
 # Библиотека (портативный движок рендеринга), которая позволяет приложениям
 # рендерить шрифты TrueType, OpenType и Type 1
 
-# http://www.linuxfromscratch.org/blfs/view/9.0/general/freetype2.html
+# http://www.linuxfromscratch.org/blfs/view/stable/general/freetype2.html
 
 # Home page: http://www.freetype.org
-# Download:  https://download.savannah.gnu.org/releases/freetype/freetype-2.10.1.tar.xz
-# Docs:      https://download.savannah.gnu.org/releases/freetype/freetype-doc-2.10.1.tar.xz
+# Download:  https://downloads.sourceforge.net/freetype/freetype-2.10.1.tar.xz
+# Docs:      https://downloads.sourceforge.net/freetype/freetype-doc-2.10.1.tar.xz
 
 # Required:    no
-# Optional:    no
-# Recommended: harfbuzz (сначала устанавливаем без этого пакета, а после его
-#                        установки нужно будет переустановить freetype)
+# Recommended: harfbuzz
 #              libpng
 #              which
+# Optional:    docwriter (для сборки документации) https://pypi.org/project/docwriter/
 
-ROOT="/"
-source "${ROOT}check_environment.sh"                  || exit 1
-source "${ROOT}unpack_source_archive.sh" "${PRGNAME}" || exit 1
+ROOT="/root"
+source "${ROOT}/check_environment.sh"                  || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
 
-TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
-rm -rf "${TMP_DIR}"
-mkdir -pv "${TMP_DIR}/usr/bin"
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+DOCS="/usr/share/doc/${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}${DOCS}"
 
-# распакуем документацию в директорию docs (уже существует в дереве исходников
-# freetype)
-tar -xf /sources/freetype-doc-2.10.1.tar.xz --strip-components=2 -C docs
+# распакуем документацию в директорию docs
+tar -xvf "${SOURCES}/${PRGNAME}-doc-${VERSION}.tar.xz" \
+    --strip-components=2 \
+    -C docs
 
 # включим проверку таблиц GX/AAT и OpenType
 #    # AUX_MODULES += gxvalid --> AUX_MODULES +=
@@ -53,16 +53,13 @@ make || exit 1
 make install
 make install DESTDIR="${TMP_DIR}"
 
-# вручную разместим утилиту конфигурации freetype в /usr/bin, необходимую для
-# других программ, которые используют библиотеку FreeType
+# вручную разместим утилиту freetype-config в /usr/bin, необходимую для других
+# программ, которые используют библиотеку FreeType
 cp builds/unix/freetype-config /usr/bin
 cp builds/unix/freetype-config "${TMP_DIR}/usr/bin"
 
 # установим документацию
-DOCS="/usr/share/doc/${PRGNAME}-${VERSION}"
 install -v -m755 -d "${DOCS}"
-install -v -m755 -d "${TMP_DIR}${DOCS}"
-
 cp -vR docs/* "${DOCS}"
 cp -vR docs/* "${TMP_DIR}${DOCS}"
 
