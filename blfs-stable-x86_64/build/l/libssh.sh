e5ababfc6223063fb359dcfd1dbd4dae0969cd09
Vladimir <mrvladislavovich@gmail.com>
2020-04-22 12:54:51 +0400

    BLFS: build/l/libssh.sh updated

--- blfs-stable-x86_64/build/l/libssh.sh
+++ blfs-stable-x86_64/build/l/libssh.sh
@@ -2,30 +2,31 @@
 
 PRGNAME="libssh"
 
-### libssh
+### libssh (library implementing ssh protocols)
 # Многоплатформенная библиотека C, реализующая протоколы SSHv1 и SSHv2 на
 # стороне клиента и сервера. С libssh можно удаленно выполнять программы,
 # передавать файлы и использовать безопасный и прозрачный туннель
 
-# нет в LFS и BLFS (только libssh2)
+# нет в BLFS
 
 # Home page: http://www.libssh.org/
-# Download:  https://www.libssh.org/files/0.9/libssh-0.9.3.tar.xz
+# Download:  https://www.libssh.org/files/0.9/libssh-0.9.4.tar.xz
 
 # Required: cmake
 #           openssl
 # Optional: no
 
-ROOT="/"
-source "${ROOT}check_environment.sh"                  || exit 1
-source "${ROOT}unpack_source_archive.sh" "${PRGNAME}" || exit 1
+ROOT="/root"
+source "${ROOT}/check_environment.sh"                  || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
 
-TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
-rm -rf "${TMP_DIR}"
-mkdir -pv "${TMP_DIR}"
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+DOCS="/usr/share/doc/${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}${DOCS}"
 
 mkdir build
 cd build || exit 1
+
 cmake                           \
     -DCMAKE_INSTALL_PREFIX=/usr \
     -DWITH_GCRYPT=1             \
@@ -41,13 +42,9 @@ make install
 make install DESTDIR="${TMP_DIR}"
 
 # документация
-DOCS="/usr/share/doc/${PRGNAME}-${VERSION}"
 install -v -m755 -d "${DOCS}"
-install -v -m755 -d "${TMP_DIR}${DOCS}"
-
-cd .. || exit 1
-cp -a  AUTHORS BSD COPYING ChangeLog INSTALL README "${DOCS}"
-cp -a  AUTHORS BSD COPYING ChangeLog INSTALL README "${TMP_DIR}${DOCS}"
+cp -av  ../{AUTHORS,BSD,COPYING,ChangeLog,INSTALL,README} "${DOCS}"
+cp -av  ../{AUTHORS,BSD,COPYING,ChangeLog,INSTALL,README} "${TMP_DIR}${DOCS}"
 
 MAJ_VERSION="$(echo "${VERSION}" | cut -d . -f 1,2)"
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
