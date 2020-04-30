844fc985924bfa28d8d73e66f9ad74dab1169a51
Vladimir <mrvladislavovich@gmail.com>
2020-04-30 17:08:46 +0400

    BLFS: build/template.sh updated

--- blfs-stable-x86_64/build/template.sh
+++ blfs-stable-x86_64/build/template.sh
@@ -16,17 +16,9 @@ PRGNAME=""
 ROOT="/root"
 source "${ROOT}/check_environment.sh"                  || exit 1
 source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
-source "${ROOT}/config_file_processing.sh"             || exit 1
 
 TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
 mkdir -pv "${TMP_DIR}"
-
-CONFIG="...."
-if [ -f "${CONFIG}" ]; then
-    mv "${CONFIG}" "${CONFIG}.old"
-fi
-
-config_file_processing "${CONFIG}"
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Package: ${PRGNAME} ()
@@ -43,4 +35,11 @@ source "${ROOT}/write_to_var_log_packages.sh" \
 # echo -e "\n---------------\nRemoving *.la files..."
 # remove-la-files.sh
 # echo "---------------"
+
+# source "${ROOT}/config_file_processing.sh"             || exit 1
+# CONFIG="...."
+# if [ -f "${CONFIG}" ]; then
+#     mv "${CONFIG}" "${CONFIG}.old"
+# fi
+# config_file_processing "${CONFIG}"
 
