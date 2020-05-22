7c9262112dd860fe597ac8df382f138ee8ba814a
Vladimir <mrvladislavovich@gmail.com>
2020-05-22 11:41:49 +0400

    BLFS: build/s/docbook-xml.sh updated

--- blfs-stable-x86_64/build/s/docbook-xml.sh
+++ blfs-stable-x86_64/build/s/docbook-xml.sh
@@ -101,6 +101,31 @@ xmlcatalog --noout --add "delegateURI" \
     "file:///etc/xml/docbook" \
     /etc/xml/catalog
 
+# будем использовать docbook-xml-${VERSION} при запросе любой версии 4.x
+# (4.1.2, 4.2, 4.3, 4.4)
+for DTDVERSION in 4.1.2 4.2 4.3 4.4; do
+    xmlcatalog --noout --add "public" \
+        "-//OASIS//DTD DocBook XML V${DTDVERSION}//EN" \
+        "http://www.oasis-open.org/docbook/xml/${DTDVERSION}/docbookx.dtd" \
+        /etc/xml/docbook
+    xmlcatalog --noout --add "rewriteSystem" \
+        "http://www.oasis-open.org/docbook/xml/${DTDVERSION}" \
+        "file:///usr/share/xml/docbook/xml-dtd-${VERSION}" \
+        /etc/xml/docbook
+    xmlcatalog --noout --add "rewriteURI" \
+        "http://www.oasis-open.org/docbook/xml/${DTDVERSION}" \
+        "file:///usr/share/xml/docbook/xml-dtd-${VERSION}" \
+        /etc/xml/docbook
+    xmlcatalog --noout --add "delegateSystem" \
+        "http://www.oasis-open.org/docbook/xml/${DTDVERSION}/" \
+        "file:///etc/xml/docbook" \
+        /etc/xml/catalog
+    xmlcatalog --noout --add "delegateURI" \
+        "http://www.oasis-open.org/docbook/xml/$DTDVERSION/" \
+        "file:///etc/xml/docbook" \
+        /etc/xml/catalog
+done
+
 cp -vR /etc/xml/* "${TMP_DIR}/etc/xml/"
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
