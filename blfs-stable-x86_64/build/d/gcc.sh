2cc42cb5725476b78d7496dc1f6323f08a3a63fb
Vladimir <mrvladislavovich@gmail.com>
2020-09-13 12:56:11 +0400

    BLFS: build/d/gcc.sh updated from 9.2.0 to 10.2.0

--- blfs-stable-x86_64/build/d/gcc.sh
+++ blfs-stable-x86_64/build/d/gcc.sh
@@ -9,7 +9,7 @@ PRGNAME="gcc"
 # http://www.linuxfromscratch.org/blfs/view/stable/general/gcc.html
 
 # Home page: https://gcc.gnu.org/
-# Download:  https://ftp.gnu.org/gnu/gcc/gcc-9.2.0/gcc-9.2.0.tar.xz
+# Download:  https://ftp.gnu.org/gnu/gcc/gcc-10.2.0/gcc-10.2.0.tar.xz
 
 # Required:    no
 # Recommended: dejagnu  (для тестов)
@@ -18,7 +18,7 @@ PRGNAME="gcc"
 #              isl      (для включения оптимизации graphite) https://repo.or.cz/isl.git
 
 ### NOTE
-# Для сборки версии 9.2.0 требуется минимум 12G свободного места + 930MB для
+# Для сборки версии 10.2.0 требуется минимум 12G свободного места + 1.1G для
 # тестов
 ###
 
@@ -39,10 +39,6 @@ mkdir -pv "${TMP_DIR}"{"${GDB}",/lib,/usr/lib/bfd-plugins}
 # установим имя каталога для 64-битных библиотек по умолчанию как 'lib'
 sed -e '/m64=/s/lib64/lib/' -i.orig gcc/config/i386/t-linux64
 
-# исправим проблему с Glibc-2.31
-sed -e '1161 s|^|//|' \
-    -i libsanitizer/sanitizer_common/sanitizer_platform_limits_posix.cc
-
 mkdir build
 cd build || exit 1
 
@@ -60,7 +56,9 @@ cd build || exit 1
     --enable-bootstrap \
     --enable-languages=c,c++,d,fortran,go,objc,obj-c++ || exit 1
 
-make || exit 1
+# собираем в количество потоков равных количествую ядер
+NUMJOBS="$(nproc)"
+make -j"${NUMJOBS}" || exit 1
 
 ###
 # Важно !!!
@@ -131,186 +129,6 @@ install -v -dm755 "${TMP_DIR}/usr/lib/bfd-plugins"
         liblto_plugin.so
 )
 
-# LC_MESSAGES=en_US.UTF-8
-# LC_CTYPE=en_US.UTF-8
-# LANG=en_US.UTF-8
-# export LANG LC_CTYPE LC_MESSAGES
-#
-# # после установки необходимо убедиться, что основные функции (компиляция и
-# # компоновка) установленной цепочки инструментов работают должным образом. Для
-# # проверки работоспособности создадим простейший фиктивный C-файл и
-# # скомпилируем его собранным нами gcc (после компиляции генерируется объектный
-# # файл a.out):
-# echo ""
-# echo "--------"
-# echo "Step: 1"
-# echo "--------"
-# echo "# creating simple C-file"
-# echo "echo 'int main(){}' > dummy.c"
-#
-# echo 'int main(){}' > dummy.c
-# echo ""
-#
-# echo -n "Press any key... "
-# read -r JUNK
-# echo "${JUNK}" > /dev/null
-# echo ""
-#
-# echo "--------"
-# echo "Step: 2"
-# echo "--------"
-# echo "# compiling source file dummy.c using /usr/bin/cc (link to /usr/bin/gcc)"
-# echo "# (as a result of compilation, an object file a.out is generated)"
-# echo "cc dummy.c -v -Wl,--verbose &> dummy.log"
-#
-# cc dummy.c -v -Wl,--verbose &> dummy.log
-# echo ""
-#
-# echo -n "Press any key... "
-# read -r JUNK
-# echo "${JUNK}" > /dev/null
-# echo ""
-#
-# echo "--------"
-# echo "Step: 3"
-# echo "--------"
-# echo "# show dynamic linker name"
-# echo "readelf -l a.out | grep ': /lib'"
-#
-# readelf -l a.out | grep ': /lib'
-# echo ""
-#
-# echo "# The output should be something like this:"
-# echo "#     [Requesting program interpreter: /lib64/ld-linux-x86-64.so.2]"
-# echo ""
-# echo -n "Press any key... "
-# read -r JUNK
-# echo "${JUNK}" > /dev/null
-# echo ""
-# # если вывод не показан, как указано выше, или вывод не был получен вообще,
-# # значит что-то не так
-#
-# # проверим настройки для стартовых файлов
-# # /usr/lib/crt1.o
-# # /usr/lib/crti.o
-# # /usr/lib/crtn.o
-# echo "--------"
-# echo "Step: 4"
-# echo "--------"
-# echo "# make sure that we're setup to use the correct start files"
-# echo "grep -o '/usr/lib.*/crt[1in].*succeeded' dummy.log"
-#
-# grep -o '/usr/lib.*/crt[1in].*succeeded' dummy.log
-# echo ""
-#
-# echo "# The output should be something like this:"
-# echo "/usr/lib/gcc/x86_64-pc-linux-gnu/${VERSION}/../../../../lib/crt1.o succeeded"
-# echo "/usr/lib/gcc/x86_64-pc-linux-gnu/${VERSION}/../../../../lib/crti.o succeeded"
-# echo "/usr/lib/gcc/x86_64-pc-linux-gnu/${VERSION}/../../../../lib/crtn.o succeeded"
-# echo ""
-# echo -n "Press any key... "
-# read -r JUNK
-# echo "${JUNK}" > /dev/null
-# echo ""
-# # В зависимости от архитектуры машины вывод может незначительно отличатся,
-# # обычно это имя каталога после /usr/lib/gcc/. Здесь важно обратить внимание на
-# # то, что gcc нашел все три файла crt*.o в каталоге /usr/lib/
-#
-# # убедимся, что компилятор ищет правильные заголовочные файлы
-# echo "--------"
-# echo "Step: 5"
-# echo "--------"
-# echo "# verify that the compiler is searching for the correct header files"
-# echo "grep -B4 '^ /usr/include' dummy.log"
-#
-# grep -B4 '^ /usr/include' dummy.log
-# echo ""
-#
-# echo "# The output should be something like this:"
-# echo "#include <...> search starts here:"
-# echo " /usr/lib/gcc/x86_64-pc-linux-gnu/${VERSION}/include"
-# echo " /usr/local/include"
-# echo " /usr/lib/gcc/x86_64-pc-linux-gnu/${VERSION}/include-fixed"
-# echo " /usr/include"
-#
-# echo ""
-# echo -n "Press any key... "
-# read -r JUNK
-# echo "${JUNK}" > /dev/null
-# echo ""
-#
-# # убедимся, что новый компоновщик использует правильные пути для поиска
-# echo "--------"
-# echo "Step: 6"
-# echo "--------"
-# echo "verify that the new linker is being used with the correct search paths"
-# printf "grep 'SEARCH.*/usr/lib' dummy.log | sed 's|; |\\\n|g'\n"
-#
-# grep 'SEARCH.*/usr/lib' dummy.log | sed 's|; |\n|g'
-# echo ""
-#
-# # ссылки на пути, в которых есть компоненты с -linux-gnu, должны
-# # игнорироваться, но весь остальной вывод должен быть такой
-# echo "# The output should be something like this:"
-# echo 'SEARCH_DIR("/usr/x86_64-pc-linux-gnu/lib64")'
-# echo 'SEARCH_DIR("/usr/local/lib64")'
-# echo 'SEARCH_DIR("/lib64")'
-# echo 'SEARCH_DIR("/usr/lib64")'
-# echo 'SEARCH_DIR("/usr/x86_64-pc-linux-gnu/lib")'
-# echo 'SEARCH_DIR("/usr/local/lib")'
-# echo 'SEARCH_DIR("/lib")'
-# echo 'SEARCH_DIR("/usr/lib")'
-#
-# echo ""
-# echo -n "Press any key... "
-# read -r JUNK
-# echo "${JUNK}" > /dev/null
-# echo ""
-#
-# # убедимся, что мы используем правильный libc
-# echo "--------"
-# echo "Step: 7"
-# echo "--------"
-# echo "# make sure that we're using the correct libc"
-# echo 'grep "/lib.*/libc.so.6 " dummy.log'
-#
-# grep "/lib.*/libc.so.6 " dummy.log
-# echo ""
-#
-# echo "# The output should be something like this:"
-# echo "attempt to open /lib/libc.so.6 succeeded"
-# echo ""
-# echo -n "Press any key... "
-# read -r JUNK
-# echo "${JUNK}" > /dev/null
-# echo ""
-#
-# # наконец, убедимся, что GCC использует правильный динамический компоновщик
-# echo "--------"
-# echo "Step: 8"
-# echo "--------"
-# echo "# make sure GCC is using the correct dynamic linker"
-# echo "grep found dummy.log"
-#
-# grep found dummy.log
-# echo ""
-#
-# echo "# The output should be something like this:"
-# echo "found ld-linux-x86-64.so.2 at /lib/ld-linux-x86-64.so.2"
-# echo ""
-# echo -n "Press any key... "
-# read -r JUNK
-# echo "${JUNK}" > /dev/null
-# echo ""
-# # если выходные данные не отображаются, как показано выше, или не получены
-# # вообще, значит, что-то серьезно не так. Любые проблемы должны быть решены,
-# # прежде чем продолжить процесс сборки.
-#
-# # очистим созданные нами тестовые файлы
-# echo "Cleaning:"
-# rm -v dummy.c a.out dummy.log
-# echo ""
-
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Package: ${PRGNAME} (GNU compiler collection)
 #
@@ -324,6 +142,185 @@ EOF
 
 source "${ROOT}/write_to_var_log_packages.sh" \
     "${TMP_DIR}" "${PRGNAME}-${VERSION}"
+
+# После установки необходимо убедиться, что основные функции (компиляция и
+# компоновка) работают должным образом. Вывод сообщений всех последующих команд
+# должен быть на английском языке
+LC_MESSAGES=en_US.UTF-8
+LC_CTYPE=en_US.UTF-8
+LANG=en_US.UTF-8
+export LANG LC_CTYPE LC_MESSAGES
+
+# для проверки работоспособности создадим простейший фиктивный C-файл и
+# скомпилируем его собранным нами gcc для генерации объектного файла a.out:
+echo ""
+echo "--------"
+echo "Step: 1"
+echo "--------"
+echo "# creating simple C-file"
+echo "echo 'int main(){}' > dummy.c"
+
+echo 'int main(){}' > dummy.c
+echo ""
+
+echo -n "Press any key... "
+read -r JUNK
+echo "${JUNK}" > /dev/null
+echo ""
+
+echo "--------"
+echo "Step: 2"
+echo "--------"
+echo "# compiling source file dummy.c using /usr/bin/cc (link to /usr/bin/gcc)"
+echo "# (as a result of compilation, an object file a.out is generated)"
+echo "cc dummy.c -v -Wl,--verbose &> dummy.log"
+
+cc dummy.c -v -Wl,--verbose &> dummy.log
+echo ""
+
+echo -n "Press any key... "
+read -r JUNK
+echo "${JUNK}" > /dev/null
+echo ""
+
+echo "--------"
+echo "Step: 3"
+echo "--------"
+echo "# show dynamic linker name"
+echo "readelf -l a.out | grep ': /lib'"
+
+readelf -l a.out | grep ': /lib'
+echo ""
+
+echo "# The output should be something like this:"
+echo "#     [Requesting program interpreter: /lib64/ld-linux-x86-64.so.2]"
+echo ""
+echo -n "Press any key... "
+read -r JUNK
+echo "${JUNK}" > /dev/null
+echo ""
+# если вывод не показан, как указано выше, или вывод не был получен вообще,
+# значит что-то не так
+
+# проверим настройки для стартовых файлов
+# /usr/lib/crt1.o
+# /usr/lib/crti.o
+# /usr/lib/crtn.o
+echo "--------"
+echo "Step: 4"
+echo "--------"
+echo "# make sure that we're setup to use the correct start files"
+echo "grep -o '/usr/lib.*/crt[1in].*succeeded' dummy.log"
+
+grep -o '/usr/lib.*/crt[1in].*succeeded' dummy.log
+echo ""
+
+echo "# The output should be something like this:"
+echo "/usr/lib/gcc/x86_64-pc-linux-gnu/${VERSION}/../../../../lib/crt1.o succeeded"
+echo "/usr/lib/gcc/x86_64-pc-linux-gnu/${VERSION}/../../../../lib/crti.o succeeded"
+echo "/usr/lib/gcc/x86_64-pc-linux-gnu/${VERSION}/../../../../lib/crtn.o succeeded"
+echo ""
+echo -n "Press any key... "
+read -r JUNK
+echo "${JUNK}" > /dev/null
+echo ""
+# В зависимости от архитектуры машины вывод может незначительно отличатся,
+# обычно это имя каталога после /usr/lib/gcc/. Здесь важно обратить внимание на
+# то, что gcc нашел все три файла crt*.o в каталоге /usr/lib/
+
+# убедимся, что компилятор ищет правильные заголовочные файлы
+echo "--------"
+echo "Step: 5"
+echo "--------"
+echo "# verify that the compiler is searching for the correct header files"
+echo "grep -B4 '^ /usr/include' dummy.log"
+
+grep -B4 '^ /usr/include' dummy.log
+echo ""
+
+echo "# The output should be something like this:"
+echo "#include <...> search starts here:"
+echo " /usr/lib/gcc/x86_64-pc-linux-gnu/${VERSION}/include"
+echo " /usr/local/include"
+echo " /usr/lib/gcc/x86_64-pc-linux-gnu/${VERSION}/include-fixed"
+echo " /usr/include"
+
+echo ""
+echo -n "Press any key... "
+read -r JUNK
+echo "${JUNK}" > /dev/null
+echo ""
+
+# убедимся, что новый компоновщик использует правильные пути для поиска
+echo "--------"
+echo "Step: 6"
+echo "--------"
+echo "verify that the new linker is being used with the correct search paths"
+printf "grep 'SEARCH.*/usr/lib' dummy.log | sed 's|; |\\\n|g'\n"
+
+grep 'SEARCH.*/usr/lib' dummy.log | sed 's|; |\n|g'
+echo ""
+
+# ссылки на пути, в которых есть компоненты с -linux-gnu, должны
+# игнорироваться, но весь остальной вывод должен быть такой
+echo "# The output should be something like this:"
+echo 'SEARCH_DIR("/usr/x86_64-pc-linux-gnu/lib64")'
+echo 'SEARCH_DIR("/usr/local/lib64")'
+echo 'SEARCH_DIR("/lib64")'
+echo 'SEARCH_DIR("/usr/lib64")'
+echo 'SEARCH_DIR("/usr/x86_64-pc-linux-gnu/lib")'
+echo 'SEARCH_DIR("/usr/local/lib")'
+echo 'SEARCH_DIR("/lib")'
+echo 'SEARCH_DIR("/usr/lib")'
+
+echo ""
+echo -n "Press any key... "
+read -r JUNK
+echo "${JUNK}" > /dev/null
+echo ""
+
+# убедимся, что мы используем правильный libc
+echo "--------"
+echo "Step: 7"
+echo "--------"
+echo "# make sure that we're using the correct libc"
+echo 'grep "/lib.*/libc.so.6 " dummy.log'
+
+grep "/lib.*/libc.so.6 " dummy.log
+echo ""
+
+echo "# The output should be something like this:"
+echo "attempt to open /lib/libc.so.6 succeeded"
+echo ""
+echo -n "Press any key... "
+read -r JUNK
+echo "${JUNK}" > /dev/null
+echo ""
+
+# наконец, убедимся, что GCC использует правильный динамический компоновщик
+echo "--------"
+echo "Step: 8"
+echo "--------"
+echo "# make sure GCC is using the correct dynamic linker"
+echo "grep found dummy.log"
+
+grep found dummy.log
+echo ""
+
+echo "# The output should be something like this:"
+echo "found ld-linux-x86-64.so.2 at /lib/ld-linux-x86-64.so.2"
+echo ""
+echo -n "Press any key... "
+read -r JUNK
+echo "${JUNK}" > /dev/null
+echo ""
+# если выходные данные не отображаются, как показано выше, или не получены
+# вообще, значит, что-то серьезно не так. Любые проблемы должны быть решены,
+# прежде чем продолжить процесс сборки.
+
+# очистим созданные нами тестовые файлы
+echo "Cleaning:"
+rm -v dummy.c a.out dummy.log
 
 echo -e "\n---------------\nRemoving *.la files..."
 remove-la-files.sh
