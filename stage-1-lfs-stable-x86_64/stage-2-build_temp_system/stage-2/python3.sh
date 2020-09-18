7884c6ac868298b2b2f15cc9c3fd34edc549512c
Vladimir <mrvladislavovich@gmail.com>
2020-09-18 18:53:51 +0400

    LFS:  stage-2-build_temp_system/stage-2/python3.sh updated

---
+++ stage-1-lfs-stable-x86_64/stage-2-build_temp_system/stage-2/python3.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+PRGNAME="python3"
+echo "Building ${PRGNAME}"
+ARCH_NAME="Python"
+
+### Python
+# Язык программирования Python v3
+
+# http://www.linuxfromscratch.org/lfs/view/stable/chapter07/Python.html
+
+# Home page: https://www.python.org/
+
+ROOT="/"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+# не устанавливаем статические библиотеки
+#    --enable-shared
+# утилита pip - 'Python package installer' на данном этапе нам не нужна
+#    --without-ensurepip
+./configure         \
+    --prefix=/usr   \
+    --enable-shared \
+    --without-ensurepip || exit 1
+
+make || make -j1 || exit 1
+make install
