246b8a8016e7fad6aee910185a08023b42f79326
Vladimir <mrvladislavovich@gmail.com>
2020-09-18 14:16:49 +0400

    LFS:  01_build_temp_system/stage-2/{rev.sh,libstdc++-pass-2.sh} updated

---
+++ 01_lfs-stable-x86_64/01_build_temp_system/stage-2/rev.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+PRGNAME="rev"
+echo "Building ${PRGNAME}"
+ARCH_NAME="util-linux"
+
+### rev
+# утилита из пакета util-linux, которая  посимвольно "переворачивает" строки
+
+# http://www.linuxfromscratch.org/lfs/view/stable/chapter07/util-linux.html
+
+# Home page: http://freecode.com/projects/util-linux
+
+ROOT="/"
+source "${ROOT}/check_environment.sh" || exit 1
+
+SOURCES="/sources"
+BUILD_DIR="${SOURCES}/build"
+
+mkdir -p "${BUILD_DIR}"
+cd "${BUILD_DIR}" || exit 1
+rm -rf "${ARCH_NAME}"*
+
+tar xvf "${SOURCES}/${ARCH_NAME}-"*.tar.?z* || exit 1
+cd "${ARCH_NAME}"* || exit 1
+
+./configure \
+    --prefix=/usr || exit 1
+
+make rev
+cp rev /usr/bin
