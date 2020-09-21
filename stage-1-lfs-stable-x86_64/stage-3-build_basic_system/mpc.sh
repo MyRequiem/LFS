962b665d1d748eb0c16221f3494fe89266bdeb87
Vladimir <mrvladislavovich@gmail.com>
2020-09-21 16:24:18 +0400

    LFS:  stage-3-build_basic_system/mpc.sh updated

--- stage-1-lfs-stable-x86_64/stage-3-build_basic_system/mpc.sh
+++ stage-1-lfs-stable-x86_64/stage-3-build_basic_system/mpc.sh
@@ -6,10 +6,9 @@ PRGNAME="mpc"
 # Пакет содержит библиотеку для арифметики комплексных чисел с произвольно
 # высокой точностью и правильным округлением результата.
 
-# http://www.linuxfromscratch.org/lfs/view/stable/chapter06/mpc.html
+# http://www.linuxfromscratch.org/lfs/view/stable/chapter08/mpc.html
 
 # Home page: http://www.multiprecision.org/
-# Download:  https://ftp.gnu.org/gnu/mpc/mpc-1.1.0.tar.gz
 
 ROOT="/"
 source "${ROOT}check_environment.sh"                  || exit 1
@@ -24,16 +23,16 @@ mkdir -pv "${TMP_DIR}"
     --disable-static \
     --docdir="/usr/share/doc/${PRGNAME}-${VERSION}" || exit 1
 
-# сборка
-make || exit 1
+make || make -j1 || exit 1
+# сборка html-документации
 make html || exit 1
 
-make check
+# make check
 
-make install
-make install-html
 make install DESTDIR="${TMP_DIR}"
 make install-html DESTDIR="${TMP_DIR}"
+
+/bin/cp -vR "${TMP_DIR}"/* /
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Package: ${PRGNAME} (Multiple Precision Complex Library)
@@ -48,5 +47,5 @@ cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 #
 EOF
 
-source "${ROOT}/write_to_var_log_packages.sh" \
+source "${ROOT}write_to_var_log_packages.sh" \
     "${TMP_DIR}" "${PRGNAME}-${VERSION}"
