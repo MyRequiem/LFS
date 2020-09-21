033e51dcfd34d12f7153e0369ca580c76da7299f
Vladimir <mrvladislavovich@gmail.com>
2020-09-21 12:12:29 +0400

    LFS:  stage-3-build_basic_system/flex.sh updated

--- stage-1-lfs-stable-x86_64/stage-3-build_basic_system/flex.sh
+++ stage-1-lfs-stable-x86_64/stage-3-build_basic_system/flex.sh
@@ -6,10 +6,9 @@ PRGNAME="flex"
 # Пакет содержит утилиту для генерации программ, которые распознают шаблоны в
 # тексте
 
-# http://www.linuxfromscratch.org/lfs/view/stable/chapter06/flex.html
+# http://www.linuxfromscratch.org/lfs/view/stable/chapter08/flex.html
 
 # Home page: https://github.com/westes/flex
-# Download:  https://github.com/westes/flex/releases/download/v2.6.4/flex-2.6.4.tar.gz
 
 ROOT="/"
 source "${ROOT}check_environment.sh"                  || exit 1
@@ -19,31 +18,23 @@ TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
 rm -rf "${TMP_DIR}"
 mkdir -pv "${TMP_DIR}"
 
-# исправим проблему для glibc-2.26
-sed -i "/math.h/a #include <malloc.h>" src/flexdef.h || exit 1
-
-# процедура сборки предполагает, что утилита help2man доступна в системе для
-# создания man-страниц из исполняемого файла, но на данный момент она еще не
-# установлена, поэтому мы используем переменную окружения HELP2MAN, чтобы
-# пропустить процесс создания man-страниц
-HELP2MAN=/tools/bin/true \
-./configure              \
-    --prefix=/usr        \
+./configure       \
+    --prefix=/usr \
     --docdir="/usr/share/doc/${PRGNAME}-${VERSION}" || exit 1
 
-make || exit 1
-make check
-make install
+make || make -j1 || exit 1
+# make check
 make install DESTDIR="${TMP_DIR}"
 
 # некоторые программы еще не знают о flex и пытаются запустить его
 # предшественника lex. Для поддержки таких программ создадим ссылку в /usr/bin
 # lex -> flex
-ln -sv flex /usr/bin/lex
 (
     cd "${TMP_DIR}/usr/bin" || exit 1
     ln -sv flex lex
 )
+
+/bin/cp -vR "${TMP_DIR}"/* /
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Package: ${PRGNAME} (fast lexical analyzer generator)
@@ -57,5 +48,5 @@ cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 #
 EOF
 
-source "${ROOT}/write_to_var_log_packages.sh" \
+source "${ROOT}write_to_var_log_packages.sh" \
     "${TMP_DIR}" "${PRGNAME}-${VERSION}"
