c7126c25ae7f6220ebe9843043dfb583d6a44932
Vladimir <mrvladislavovich@gmail.com>
2020-09-21 16:43:49 +0400

    LFS:  stage-3-build_basic_system/attr.sh updated

--- stage-1-lfs-stable-x86_64/stage-3-build_basic_system/attr.sh
+++ stage-1-lfs-stable-x86_64/stage-3-build_basic_system/attr.sh
@@ -5,10 +5,9 @@ PRGNAME="attr"
 ### Attr (tools for using extended attributes on filesystems)
 # Утилиты для управления расширенными атрибутами объектов файловой системы
 
-# http://www.linuxfromscratch.org/lfs/view/stable/chapter06/attr.html
+# http://www.linuxfromscratch.org/lfs/view/stable/chapter08/attr.html
 
 # Home page: https://savannah.nongnu.org/projects/attr
-# Download:  http://download.savannah.gnu.org/releases/attr/attr-2.4.48.tar.gz
 
 ROOT="/"
 source "${ROOT}check_environment.sh"                  || exit 1
@@ -26,10 +25,24 @@ mkdir -pv "${TMP_DIR}/lib"
     --sysconfdir=/etc \
     --docdir="/usr/share/doc/${PRGNAME}-${VERSION}" || exit 1
 
-make || exit 1
+make || make -j1 || exit 1
+
 # тесты должны выполняться в файловой системе, которая поддерживает расширенные
 # атрибуты, такие как ext2, ext3 или ext4
-make check
+# make check
+
+# устанавливаем пакет
+make install DESTDIR="${TMP_DIR}"
+
+# библиотеку необходимо переместить из /usr/lib в /lib
+mv -v "${TMP_DIR}/usr/lib/libattr.so."* "${TMP_DIR}/lib/"
+
+# воссоздадим ссылку libattr.so в /usr/lib
+#    libattr.so -> ../../lib/libattr.so.x.x.xxxx
+(
+    cd "${TMP_DIR}/usr/lib" || exit 1
+    ln -sfv "../../lib/$(readlink libattr.so)" libattr.so
+)
 
 # бэкапим конфиг /etc/xattr.conf перед установкой пакета, если он существует
 XATTR_CONFIG="/etc/xattr.conf"
@@ -37,23 +50,9 @@ if [ -f "${XATTR_CONFIG}" ]; then
     mv "${XATTR_CONFIG}" "${XATTR_CONFIG}.old"
 fi
 
-# устанавливаем пакет
-make install
-make install DESTDIR="${TMP_DIR}"
+/bin/cp -vR "${TMP_DIR}"/* /
 
 config_file_processing "${XATTR_CONFIG}"
-
-# расшаренную библиотеку необходимо переместить из /usr/lib в /lib
-mv -v /usr/lib/libattr.so.* /lib
-mv -v "${TMP_DIR}/usr/lib"/libattr.so.* "${TMP_DIR}/lib/"
-
-# воссоздадим ссылку libattr.so в /usr/lib
-# libattr.so -> ../../lib/libattr.so.x.x.xxxx
-ln -sfv "../../lib/$(readlink /usr/lib/libattr.so)" /usr/lib/libattr.so
-(
-    cd "${TMP_DIR}/usr/lib" || exit 1
-    ln -sfv "../../lib/$(readlink libattr.so)" libattr.so
-)
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Package: ${PRGNAME} (tools for using extended attributes on filesystems)
@@ -70,5 +69,5 @@ cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 #
 EOF
 
-source "${ROOT}/write_to_var_log_packages.sh" \
+source "${ROOT}write_to_var_log_packages.sh" \
     "${TMP_DIR}" "${PRGNAME}-${VERSION}"
