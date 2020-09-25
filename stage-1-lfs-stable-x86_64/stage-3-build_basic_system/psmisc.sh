0f109244e823810153bec4737779224d7373fb93
Vladimir <mrvladislavovich@gmail.com>
2020-09-25 17:16:36 +0400

    LFS:  stage-3-build_basic_system/psmisc.sh updated

--- stage-1-lfs-stable-x86_64/stage-3-build_basic_system/psmisc.sh
+++ stage-1-lfs-stable-x86_64/stage-3-build_basic_system/psmisc.sh
@@ -4,12 +4,12 @@ PRGNAME="psmisc"
 
 ### Psmisc (displaying information about running processes)
 # Программы для отображения информации о защенных процессах, а так же для
-# управления такими процессами
+# управления этими процессами: fuser, killall, peekfd, prtstat, pslog, pstree,
+# pstree.x11
 
-# http://www.linuxfromscratch.org/lfs/view/stable/chapter06/psmisc.html
+# http://www.linuxfromscratch.org/lfs/view/stable/chapter08/psmisc.html
 
 # Home page: http://psmisc.sourceforge.net/
-# Download:  https://sourceforge.net/projects/psmisc/files/psmisc/psmisc-23.2.tar.xz
 
 ROOT="/"
 source "${ROOT}check_environment.sh"                  || exit 1
@@ -22,27 +22,28 @@ mkdir -pv "${TMP_DIR}/bin"
 ./configure \
     --prefix=/usr || exit 1
 
-make || exit 1
-# пакет не содержит набора тестов, поэтому сразу устанавливаем
-make install
+make || make -j1 || exit 1
+
+# пакет не содержит набора тестов
+
 make install DESTDIR="${TMP_DIR}"
 
 # переместим программы 'killall' и 'fuser' из /usr/bin в /bin
-mv -v /usr/bin/fuser   /bin
-mv -v /usr/bin/killall /bin
 mv -v "${TMP_DIR}/usr/bin/fuser"   "${TMP_DIR}/bin"
 mv -v "${TMP_DIR}/usr/bin/killall" "${TMP_DIR}/bin"
+
+/bin/cp -vR "${TMP_DIR}"/* /
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Package: ${PRGNAME} (displaying information about running processes)
 #
 # The Psmisc package contains programs for displaying information about running
-# processes.
+# processes: fuser, killall, peekfd, prtstat, pslog, pstree, pstree.x11
 #
 # Home page: http://psmisc.sourceforge.net/
 # Download:  https://sourceforge.net/projects/${PRGNAME}/files/${PRGNAME}/${PRGNAME}-${VERSION}.tar.xz
 #
 EOF
 
-source "${ROOT}/write_to_var_log_packages.sh" \
+source "${ROOT}write_to_var_log_packages.sh" \
     "${TMP_DIR}" "${PRGNAME}-${VERSION}"
