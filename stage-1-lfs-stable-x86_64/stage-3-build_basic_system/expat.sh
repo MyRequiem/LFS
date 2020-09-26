89e49bf81444903e4516ebee7ad8cfe6f92ac7c3
Vladimir <mrvladislavovich@gmail.com>
2020-09-26 14:33:24 +0400

    LFS:  stage-3-build_basic_system/expat.sh updated

--- stage-1-lfs-stable-x86_64/stage-3-build_basic_system/expat.sh
+++ stage-1-lfs-stable-x86_64/stage-3-build_basic_system/expat.sh
@@ -5,10 +5,9 @@ PRGNAME="expat"
 ### Expat (C library for parsing XML)
 # Пакет содержит потоково-ориентированную библиотеку C для анализа XML
 
-# http://www.linuxfromscratch.org/lfs/view/stable/chapter06/expat.html
+# http://www.linuxfromscratch.org/lfs/view/stable/chapter08/expat.html
 
 # Home page: https://libexpat.github.io/
-# Download:  https://prdownloads.sourceforge.net/expat/expat-2.2.9.tar.xz
 
 ROOT="/"
 source "${ROOT}check_environment.sh"                  || exit 1
@@ -16,26 +15,22 @@ source "${ROOT}unpack_source_archive.sh" "${PRGNAME}" || exit 1
 
 TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
 rm -rf "${TMP_DIR}"
-mkdir -pv "${TMP_DIR}"
-
-# исправим проблему с регрессионными тестами в среде LFS
-sed -i 's|usr/bin/env |bin/|' run.sh.in || exit 1
-
 DOCS="/usr/share/doc/${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}${DOCS}"
+
 ./configure          \
     --prefix=/usr    \
     --disable-static \
     --docdir="${DOCS}" || exit 1
 
-make || exit 1
-make check
-make install
+make || make -j1 || exit 1
+# make check
 make install DESTDIR="${TMP_DIR}"
 
 # установим документацию
-install -v -m644 doc/*.{html,png,css} "${DOCS}"
-mkdir -pv "${TMP_DIR}${DOCS}"
 install -v -m644 doc/*.{html,png,css} "${TMP_DIR}${DOCS}"
+
+/bin/cp -vR "${TMP_DIR}"/* /
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Package: ${PRGNAME} (C library for parsing XML)
@@ -48,5 +43,5 @@ cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 #
 EOF
 
-source "${ROOT}/write_to_var_log_packages.sh" \
+source "${ROOT}write_to_var_log_packages.sh" \
     "${TMP_DIR}" "${PRGNAME}-${VERSION}"
