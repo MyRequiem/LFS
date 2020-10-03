714d20c3f1615f9ecbc7c5cf4c6a47eb17df7cf8
Vladimir <mrvladislavovich@gmail.com>
2020-10-03 15:55:42 +0400

    LFS:  stage-3-build_basic_system/python3.sh updated

--- stage-1-lfs-stable-x86_64/stage-3-build_basic_system/python3.sh
+++ stage-1-lfs-stable-x86_64/stage-3-build_basic_system/python3.sh
@@ -1,19 +1,14 @@
 #! /bin/bash
 
 PRGNAME="python3"
+ARCH_NAME="Python"
 
 ### python3 (object-oriented interpreted programming language)
 # Язык программирования Python 3
 
-# http://www.linuxfromscratch.org/lfs/view/stable/chapter06/Python.html
-
-# Home page: https://www.python.org/
-# Download:  https://www.python.org/ftp/python/3.8.3/Python-3.8.3.tar.xz
-# Docs:      https://www.python.org/ftp/python/doc/3.8.3/python-3.8.3-docs-html.tar.bz2
-
 ROOT="/"
-source "${ROOT}check_environment.sh"              || exit 1
-source "${ROOT}unpack_source_archive.sh" "Python" || exit 1
+source "${ROOT}check_environment.sh"                    || exit 1
+source "${ROOT}unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
 
 TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
 rm -rf "${TMP_DIR}"
@@ -32,10 +27,11 @@ mkdir -pv "${TMP_DIR}"
     --with-system-ffi   \
     --with-ensurepip=yes || exit 1
 
-make || exit 1
+make || make -j1 || exit 1
 
-# тесты на данном этапе не запускаем, т.к. они требуют установленных TK и
-# X Window System, поэтому сразу устанавливаем
+# тесты на данном этапе не запускаем, т.к. они требуют сконфигурированного
+# сетевого подключения и установленных TK и X Window System
+
 make install DESTDIR="${TMP_DIR}"
 
 MAJ_VERSION="$(echo "${VERSION}" | cut -d . -f 1,2)"
@@ -45,11 +41,8 @@ chmod -v 755 "${TMP_DIR}/usr/lib/libpython3.so"
 # ссылки в /usr/bin
 # pip3          -> pip${MAJ_VERSION}
 # easy_install3 -> easy_install-${MAJ_VERSION}
-(
-    cd "${TMP_DIR}/usr/bin" || exit 1
-    ln -sfv "pip${MAJ_VERSION}" pip3
-    ln -sfv "easy_install-${MAJ_VERSION}" easy_install3
-)
+ln -sfv "pip${MAJ_VERSION}"           "${TMP_DIR}/usr/bin/pip3"
+ln -sfv "easy_install-${MAJ_VERSION}" "${TMP_DIR}/usr/bin/easy_install3"
 
 # устанавливаем документацию
 DOCS="${TMP_DIR}/usr/share/doc/${PRGNAME}-${VERSION}/html"
@@ -62,7 +55,7 @@ tar                       \
     -xvf "/sources/python-${VERSION}-docs-html.tar.bz2"
 
 # устанавливаем пакет в корень файловой системы
-cp -vR "${TMP_DIR}"/* /
+/bin/cp -vR "${TMP_DIR}"/* /
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Package: ${PRGNAME} (object-oriented interpreted programming language)
@@ -73,9 +66,9 @@ cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # adaptable as an extension language for existing applications.
 #
 # Home page: https://www.python.org/
-# Download:  https://www.python.org/ftp/python/${VERSION}/Python-${VERSION}.tar.xz
+# Download:  https://www.python.org/ftp/python/${VERSION}/${ARCH_NAME}-${VERSION}.tar.xz
 #
 EOF
 
-source "${ROOT}/write_to_var_log_packages.sh" \
+source "${ROOT}write_to_var_log_packages.sh" \
     "${TMP_DIR}" "${PRGNAME}-${VERSION}"
