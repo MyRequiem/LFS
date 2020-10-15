374613b66626271657be33b171239552c24777ad
Vladimir <mrvladislavovich@gmail.com>
2020-10-15 14:46:43 +0400

    LFS:  stage-3-build_basic_system/diffutils.sh updated

--- stage-1-lfs-stable-x86_64/stage-3-build_basic_system/diffutils.sh
+++ stage-1-lfs-stable-x86_64/stage-3-build_basic_system/diffutils.sh
@@ -5,11 +5,6 @@ PRGNAME="diffutils"
 ### Diffutils (finds differences between files)
 # Утилиты, которые показывают различия между файлами или каталогами
 
-# http://www.linuxfromscratch.org/lfs/view/stable/chapter06/diffutils.html
-
-# Home page: http://www.gnu.org/software/diffutils/
-# Download:  http://ftp.gnu.org/gnu/diffutils/diffutils-3.7.tar.xz
-
 ROOT="/"
 source "${ROOT}check_environment.sh"                  || exit 1
 source "${ROOT}unpack_source_archive.sh" "${PRGNAME}" || exit 1
@@ -21,10 +16,11 @@ mkdir -pv "${TMP_DIR}"
 ./configure \
     --prefix=/usr || exit 1
 
-make || exit 1
-make check
-make install
+make || make -j1 || exit 1
+# make check
 make install DESTDIR="${TMP_DIR}"
+
+/bin/cp -vR "${TMP_DIR}"/* /
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Package: ${PRGNAME} (finds differences between files)
@@ -37,5 +33,5 @@ cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 #
 EOF
 
-source "${ROOT}/write_to_var_log_packages.sh" \
+source "${ROOT}write_to_var_log_packages.sh" \
     "${TMP_DIR}" "${PRGNAME}-${VERSION}"
