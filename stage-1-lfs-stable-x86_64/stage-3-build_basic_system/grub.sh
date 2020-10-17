c1264ab16696147a4d5acff7030d1acc9a63bdaf
Vladimir <mrvladislavovich@gmail.com>
2020-10-17 16:33:17 +0400

    LFS:  stage-3-build_basic_system/grub.sh updated

--- stage-1-lfs-stable-x86_64/stage-3-build_basic_system/grub.sh
+++ stage-1-lfs-stable-x86_64/stage-3-build_basic_system/grub.sh
@@ -5,11 +5,6 @@ PRGNAME="grub"
 ### GRUB (the GRand Unified Bootloader)
 # Загрузчик GRand Unified Bootloader
 
-# http://www.linuxfromscratch.org/lfs/view/stable/chapter06/grub.html
-
-# Home page: http://www.gnu.org/software/grub/
-# Download:  https://ftp.gnu.org/gnu/grub/grub-2.04.tar.xz
-
 ROOT="/"
 source "${ROOT}check_environment.sh"                  || exit 1
 source "${ROOT}unpack_source_archive.sh" "${PRGNAME}" || exit 1
@@ -32,14 +27,13 @@ mkdir -pv "${TMP_DIR}${BASH_COMPLETION}"
     --disable-efiemu  \
     --disable-werror || exit 1
 
-make || exit 1
-# пакет не содержит набора тестов, поэтому сразу устанавливаем
-make install
+make || make -j1 || exit 1
+# пакет не содержит набора тестов
 make install DESTDIR="${TMP_DIR}"
 
-BASH_COMPLETION_D="/etc/bash_completion.d"
-mv -v   "${BASH_COMPLETION_D}/grub" "${BASH_COMPLETION}"
-mv -v   "${TMP_DIR}${BASH_COMPLETION_D}/grub" "${TMP_DIR}${BASH_COMPLETION}"
+mv -v "${TMP_DIR}/etc/bash_completion.d/grub" "${TMP_DIR}${BASH_COMPLETION}"
+
+/bin/cp -vR "${TMP_DIR}"/* /
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Package: ${PRGNAME} (the GRand Unified Bootloader)
@@ -51,5 +45,5 @@ cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 #
 EOF
 
-source "${ROOT}/write_to_var_log_packages.sh" \
+source "${ROOT}write_to_var_log_packages.sh" \
     "${TMP_DIR}" "${PRGNAME}-${VERSION}"
