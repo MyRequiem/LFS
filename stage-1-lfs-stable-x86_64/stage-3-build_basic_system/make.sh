f7a2dc3b938975803ddaeeaf47ad5cf23fdd7750
Vladimir <mrvladislavovich@gmail.com>
2020-10-31 17:14:43 +0400

    LFS:  stage-3-build_basic_system/make.sh updated

--- stage-1-lfs-stable-x86_64/stage-3-build_basic_system/make.sh
+++ stage-1-lfs-stable-x86_64/stage-3-build_basic_system/make.sh
@@ -5,11 +5,6 @@ PRGNAME="make"
 ### Make (GNU make utility to maintain groups of programs)
 # Программы для компиляции
 
-# http://www.linuxfromscratch.org/lfs/view/stable/chapter06/make.html
-
-# Home page: http://www.gnu.org/software/make/
-# Download:  http://ftp.gnu.org/gnu/make/make-4.3.tar.gz
-
 ROOT="/"
 source "${ROOT}check_environment.sh"                  || exit 1
 source "${ROOT}unpack_source_archive.sh" "${PRGNAME}" || exit 1
@@ -21,12 +16,11 @@ mkdir -pv "${TMP_DIR}"
 ./configure \
     --prefix=/usr || exit 1
 
-make || exit 1
-# набор тестов должен знать, где находятся служебные файлы Perl в дереве
-# исходников. Для этого мы используем переменную окружения PERL5LIB
-make PERL5LIB="${PWD}/tests/" check
-make install
+make || make -j1 || exit 1
+# make check
 make install DESTDIR="${TMP_DIR}"
+
+/bin/cp -vR "${TMP_DIR}"/* /
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Package: ${PRGNAME} (GNU make utility to maintain groups of programs)
@@ -41,5 +35,5 @@ cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 #
 EOF
 
-source "${ROOT}/write_to_var_log_packages.sh" \
+source "${ROOT}write_to_var_log_packages.sh" \
     "${TMP_DIR}" "${PRGNAME}-${VERSION}"
