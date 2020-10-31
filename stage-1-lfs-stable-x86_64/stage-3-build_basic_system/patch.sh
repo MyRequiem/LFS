25c6481323a2be648fab8d707ccb3f64243c1be5
Vladimir <mrvladislavovich@gmail.com>
2020-11-01 00:46:24 +0400

    LFS:  stage-3-build_basic_system/patch.sh updated

--- stage-1-lfs-stable-x86_64/stage-3-build_basic_system/patch.sh
+++ stage-1-lfs-stable-x86_64/stage-3-build_basic_system/patch.sh
@@ -6,11 +6,6 @@ PRGNAME="patch"
 # Программа для изменения или создания файлов путем применения файлов *.patch,
 # обычно создаваемых программой diff
 
-# http://www.linuxfromscratch.org/lfs/view/stable/chapter06/patch.html
-
-# Home page: https://savannah.gnu.org/projects/patch/
-# Download:  http://ftp.gnu.org/gnu/patch/patch-2.7.6.tar.xz
-
 ROOT="/"
 source "${ROOT}check_environment.sh"                  || exit 1
 source "${ROOT}unpack_source_archive.sh" "${PRGNAME}" || exit 1
@@ -22,10 +17,11 @@ mkdir -pv "${TMP_DIR}"
 ./configure \
     --prefix=/usr || exit 1
 
-make || exit 1
-make check
-make install
+make || make -j1 || exit 1
+# make check
 make install DESTDIR="${TMP_DIR}"
+
+/bin/cp -vR "${TMP_DIR}"/* /
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Package: ${PRGNAME} (apply a diff file to an original file or files)
@@ -38,5 +34,5 @@ cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 #
 EOF
 
-source "${ROOT}/write_to_var_log_packages.sh" \
+source "${ROOT}write_to_var_log_packages.sh" \
     "${TMP_DIR}" "${PRGNAME}-${VERSION}"
