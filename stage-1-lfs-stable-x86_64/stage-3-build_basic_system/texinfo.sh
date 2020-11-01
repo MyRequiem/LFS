61b71231f8d73dc42b1633a82112db2e9a027d7a
Vladimir <mrvladislavovich@gmail.com>
2020-11-01 15:13:54 +0400

    LFS:  stage-3-build_basic_system/texinfo.sh updated

--- stage-1-lfs-stable-x86_64/stage-3-build_basic_system/texinfo.sh
+++ stage-1-lfs-stable-x86_64/stage-3-build_basic_system/texinfo.sh
@@ -5,11 +5,6 @@ PRGNAME="texinfo"
 ### Texinfo (GNU software documentation system)
 # Программы для чтения, записи и конвертации страниц info
 
-# http://www.linuxfromscratch.org/lfs/view/stable/chapter06/texinfo.html
-
-# Home page: http://www.gnu.org/software/texinfo/
-# Download:  http://ftp.gnu.org/gnu/texinfo/texinfo-6.7.tar.xz
-
 ROOT="/"
 source "${ROOT}check_environment.sh"                  || exit 1
 source "${ROOT}unpack_source_archive.sh" "${PRGNAME}" || exit 1
@@ -19,28 +14,29 @@ rm -rf "${TMP_DIR}"
 mkdir -pv "${TMP_DIR}"
 
 # скрипт 'configure' будет жаловаться, что это нераспознанный параметр, но
-# сценарий настройки для XSParagraph распознает его и использует для отключения
-# установки статического XSParagraph.a в /usr/lib/Texinfo
+# сценарий настройки для сборки библиотеки XSParagraph.so распознает его и
+# использует для отключения установки статического XSParagraph.a в
+# /usr/lib/texinfo
 #    --disable-static
 ./configure       \
     --prefix=/usr \
     --disable-static || exit 1
 
-make || exit 1
-make check
-make install
+make || make -j1 || exit 1
+# make check
 make install DESTDIR="${TMP_DIR}"
 
-# установим компоненты, относящиеся к установке TeX. Переменная TEXMF содержит
-# местоположение корня дерева пакета TeX, который будет установлен позже
-make TEXMF=/usr/share/texmf install-tex
+# установим компоненты, используемые пакетом tetex (texlive), который будет
+# установлен в BLFS
 make TEXMF="${TMP_DIR}/usr/share/texmf" install-tex
+
+/bin/cp -vR "${TMP_DIR}"/* /
 
 # система документации Info использует простые текстовые файлы в
 # /usr/share/info/, а список этих файлов хранится в файле /usr/share/info/dir
 # который мы обновим
 cd /usr/share/info || exit 1
-rm -vf dir
+rm -fv dir
 for FILE in *; do
     install-info "${FILE}" dir 2>/dev/null
 done
@@ -49,9 +45,9 @@ cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Package: ${PRGNAME} (GNU software documentation system)
 #
 # 'Texinfo' is a documentation system that uses a single source file to produce
-# both on-line information and printed output.  Using Texinfo, you can create a
+# both on-line information and printed output. Using Texinfo, you can create a
 # printed document with the normal features of a book, including chapters,
-# sections, cross references, and indices.  From the same Texinfo source file,
+# sections, cross references, and indices. From the same Texinfo source file,
 # you can create a menu-driven, on-line Info file with nodes, menus, cross
 # references, and indices. This package is needed to read the documentation
 # files in /usr/info
@@ -61,5 +57,5 @@ cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 #
 EOF
 
-source "${ROOT}/write_to_var_log_packages.sh" \
+source "${ROOT}write_to_var_log_packages.sh" \
     "${TMP_DIR}" "${PRGNAME}-${VERSION}"
