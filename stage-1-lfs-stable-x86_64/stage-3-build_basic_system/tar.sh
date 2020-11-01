4171371824802828953a7689c7799d86bca5d1ff
Vladimir <mrvladislavovich@gmail.com>
2020-11-01 13:41:57 +0400

    LFS:  stage-3-build_basic_system/tar.sh updated

--- stage-1-lfs-stable-x86_64/stage-3-build_basic_system/tar.sh
+++ stage-1-lfs-stable-x86_64/stage-3-build_basic_system/tar.sh
@@ -3,19 +3,14 @@
 PRGNAME="tar"
 
 ### Tar (archiving utility)
-# Программа архивации файлов и каталогов
-
-# http://www.linuxfromscratch.org/lfs/view/stable/chapter06/tar.html
-
-# Home page: http://www.gnu.org/software/tar/
-# Download:  http://ftp.gnu.org/gnu/tar/tar-1.32.tar.xz
+# Пакет предоставляет возможность создания/распаковки tar-архивов, а так же
+# извлечения, обновления, вывода списка файлов в архивах.
 
 ROOT="/"
 source "${ROOT}check_environment.sh"                  || exit 1
 source "${ROOT}unpack_source_archive.sh" "${PRGNAME}" || exit 1
 
 TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
-DOC_DIR="/usr/share/doc/${PRGNAME}-${VERSION}"
 rm -rf "${TMP_DIR}"
 mkdir -pv "${TMP_DIR}"
 
@@ -29,14 +24,15 @@ FORCE_UNSAFE_CONFIGURE=1 \
     --prefix=/usr        \
     --bindir=/bin || exit 1
 
-make || exit 1
-make check
-make install
+make || make -j1 || exit 1
+# make check
 make install DESTDIR="${TMP_DIR}"
 
 # установим документацию
-make -C doc install-html docdir="${DOC_DIR}"
-make -C doc install-html docdir="${DOC_DIR}" DESTDIR="${TMP_DIR}"
+make -C doc install-html docdir="/usr/share/doc/${PRGNAME}-${VERSION}" \
+    DESTDIR="${TMP_DIR}"
+
+/bin/cp -vR "${TMP_DIR}"/* /
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Package: ${PRGNAME} (archiving utility)
@@ -51,5 +47,5 @@ cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 #
 EOF
 
-source "${ROOT}/write_to_var_log_packages.sh" \
+source "${ROOT}write_to_var_log_packages.sh" \
     "${TMP_DIR}" "${PRGNAME}-${VERSION}"
