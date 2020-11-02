06a98df50e6f5bbfb122ad52710bf9a088179181
Vladimir <mrvladislavovich@gmail.com>
2020-11-02 13:10:08 +0400

    LFS:  stage-3-build_basic_system/procps-ng.sh updated

--- stage-1-lfs-stable-x86_64/stage-3-build_basic_system/procps-ng.sh
+++ stage-1-lfs-stable-x86_64/stage-3-build_basic_system/procps-ng.sh
@@ -5,11 +5,6 @@ PRGNAME="procps-ng"
 ### Procps-ng (utilities for displaying process information)
 # Программы для мониторинга процессов
 
-# http://www.linuxfromscratch.org/lfs/view/stable/chapter06/procps-ng.html
-
-# Home page: https://sourceforge.net/projects/procps-ng
-# Download:  https://sourceforge.net/projects/procps-ng/files/Production/procps-ng-3.3.15.tar.xz
-
 ROOT="/"
 source "${ROOT}check_environment.sh"                  || exit 1
 source "${ROOT}unpack_source_archive.sh" "${PRGNAME}" || exit 1
@@ -28,29 +23,21 @@ mkdir -pv "${TMP_DIR}/lib"
     --disable-kill    \
     --docdir="/usr/share/doc/${PRGNAME}-${VERSION}" || exit 1
 
-make || exit 1
-# набор тестов нуждается в некоторых пользовательских модификациях для LFS.
-# Удалим тест, который дает сбой, когда сценарии не используют tty-устройство и
-# исправим два других теста
-sed -i -r 's|(pmap_initname)\\\$|\1|' testsuite/pmap.test/pmap.exp   || exit 1
-sed -i '/set tty/d'                   testsuite/pkill.test/pkill.exp || exit 1
-rm testsuite/pgrep.test/pgrep.exp
-make check
-
-make install
+make || make -j1 || exit 1
+# make check
 make install DESTDIR="${TMP_DIR}"
 
 # переместим библиотеку libprocps.so из /usr/lib в /lib
-mv -v /usr/lib/libprocps.so.* /lib
 mv -v "${TMP_DIR}/usr/lib"/libprocps.so.* "${TMP_DIR}/lib"
 
 # установим ссылку в /usr/lib
-# libprocps.so -> ../../lib/libprocps.so.7.1.0
-ln -sfv "../../lib/$(readlink /usr/lib/libprocps.so)" /usr/lib/libprocps.so
+# libprocps.so -> ../../lib/libprocps.so.x.x.x
 (
     cd "${TMP_DIR}/usr/lib" || exit 1
     ln -sfv "../../lib/$(readlink libprocps.so)" libprocps.so
 )
+
+/bin/cp -vR "${TMP_DIR}"/* /
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Package: ${PRGNAME} (utilities for displaying process information)
@@ -63,5 +50,5 @@ cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 #
 EOF
 
-source "${ROOT}/write_to_var_log_packages.sh" \
+source "${ROOT}write_to_var_log_packages.sh" \
     "${TMP_DIR}" "${PRGNAME}-${VERSION}"
