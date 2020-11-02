95d8fd8b9697caffadbd0c258a5905784a24e3de
Vladimir <mrvladislavovich@gmail.com>
2020-11-03 01:46:56 +0400

    LFS:  stage-4-system_configuration/{README,queue} added

--- stage-1-lfs-stable-x86_64/stage-4-system_configuration/mount-virtual-kernel-file-systems.sh
+++ stage-1-lfs-stable-x86_64/stage-4-system_configuration/mount-virtual-kernel-file-systems.sh
@@ -1,1 +1,1 @@
-/home/myrequiem/projects/git/LFS/stage-1-lfs-stable-x86_64/stage-2-build_temp_system/stage-2/mount-virtual-kernel-file-systems.sh
+../stage-2-build_temp_system/stage-2/mount-virtual-kernel-file-systems.sh
