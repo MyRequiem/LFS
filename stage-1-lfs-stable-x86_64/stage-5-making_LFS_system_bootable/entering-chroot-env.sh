3a75c152477312f2ff4d088c8dc6ec1893959177
Vladimir <mrvladislavovich@gmail.com>
2020-11-03 22:22:15 +0400

    LFS:  stage-5-making_LFS_system_bootable/ service files updated
        - 08_end.txt removed
        - rename 000_entering_chroot.sh -> entering-chroot-env.sh
        - added:
            README
            mount-virtual-kernel-file-systems.sh
            queue

---
+++ stage-1-lfs-stable-x86_64/stage-5-making_LFS_system_bootable/entering-chroot-env.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# вход в среду chroot
+
+LFS="/mnt/lfs"
+
+if [[ "$(whoami)" != "root" ]]; then
+    echo "Only superuser (root) can run this script"
+    exit 1
+fi
+
+if ! mount | /bin/grep -q "${LFS}/proc"; then
+    echo "You need to mount virtual file systems. Run script:"
+    echo "  # ./mount-virtual-kernel-file-systems.sh --mount"
+    exit 1
+fi
+
+# $PATH и $PS1 при входе в среду chroot теперь читаются из /etc/profile
+chroot "${LFS}" /usr/bin/env -i \
+    HOME="/root"                \
+    TERM="${TERM}"              \
+    /bin/bash --login
