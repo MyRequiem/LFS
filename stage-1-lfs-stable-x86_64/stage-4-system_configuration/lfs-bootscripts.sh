7c4bb94bfac9a7fb4f75a27f75639c192b185074
Vladimir <mrvladislavovich@gmail.com>
2020-11-03 09:59:36 +0400

    LFS:  stage-4-system_configuration/lfs-bootscripts.sh updated

--- stage-1-lfs-stable-x86_64/stage-4-system_configuration/lfs-bootscripts.sh
+++ stage-1-lfs-stable-x86_64/stage-4-system_configuration/lfs-bootscripts.sh
@@ -6,10 +6,6 @@ PRGNAME="lfs-bootscripts"
 # Пакет содержит набор скриптов для запуска/остановки системы LFS при
 # загрузке/выключении
 
-# http://www.linuxfromscratch.org/lfs/view/stable/chapter07/bootscripts.html
-
-# Download: http://www.linuxfromscratch.org/lfs/downloads/9.1/lfs-bootscripts-20191031.tar.xz
-
 ROOT="/"
 source "${ROOT}check_environment.sh"                  || exit 1
 source "${ROOT}unpack_source_archive.sh" "${PRGNAME}" || exit 1
@@ -18,8 +14,9 @@ TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
 rm -rf "${TMP_DIR}"
 mkdir -pv "${TMP_DIR}"
 
-make install
 make install DESTDIR="${TMP_DIR}"
+
+/bin/cp -vR "${TMP_DIR}"/* /
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Package: ${PRGNAME} (scripts to start/stop the LFS system)
@@ -27,9 +24,9 @@ cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # The LFS-Bootscripts package contains a set of scripts to start/stop the LFS
 # system at bootup/shutdown.
 #
-# Download: http://www.linuxfromscratch.org/lfs/downloads/9.1/${PRGNAME}-${VERSION}.tar.xz
+# Download: http://www.linuxfromscratch.org/lfs/downloads/stable/${PRGNAME}-${VERSION}.tar.xz
 #
 EOF
 
-source "${ROOT}/write_to_var_log_packages.sh" \
+source "${ROOT}write_to_var_log_packages.sh" \
     "${TMP_DIR}" "${PRGNAME}-${VERSION}"
