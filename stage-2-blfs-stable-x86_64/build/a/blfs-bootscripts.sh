b0f1e7d6c500da0bb6d48ba81469de6e8760cdb8
Vladimir <mrvladislavovich@gmail.com>
2020-11-05 12:52:12 +0400

    BLFS: build/a/blfs-bootscripts.sh updated

--- stage-2-blfs-stable-x86_64/build/a/blfs-bootscripts.sh
+++ stage-2-blfs-stable-x86_64/build/a/blfs-bootscripts.sh
@@ -12,15 +12,13 @@ PRGNAME="blfs-bootscripts"
 #    например:
 #    # make install-service-dhcpcd
 
-# http://www.linuxfromscratch.org/blfs/view/stable/introduction/bootscripts.html
-
 # Home page: http://anduin.linuxfromscratch.org/BLFS/blfs-bootscripts/
-# Download:  http://anduin.linuxfromscratch.org/BLFS/blfs-bootscripts/blfs-bootscripts-20191204.tar.xz
+# Download:  http://anduin.linuxfromscratch.org/BLFS/blfs-bootscripts/blfs-bootscripts-20200818.tar.xz
 
-ROOT="/root"
+ROOT="/root/src/lfs"
 source "${ROOT}/check_environment.sh"                  || exit 1
 source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
 
-# просто переместим исходники в /root
-cd "${HOME}" || exit 1
+# просто переместим исходники в /root/src/lfs
+cd "${ROOT}" || exit 1
 mv "${BUILD_DIR}/${PRGNAME}-${VERSION}" "${PRGNAME}"
