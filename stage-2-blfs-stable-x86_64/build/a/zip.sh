c72920a592a2fec90c1205b9aee36dd6d65538fc
Vladimir <mrvladislavovich@gmail.com>
2020-11-05 14:16:51 +0400

    BLFS: build/a/zip.sh added

---
+++ stage-2-blfs-stable-x86_64/build/a/zip.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+PRGNAME="zip"
+ARCH_NAME="${PRGNAME}30"
+VERSION="3.0"
+
+### Zip (compressing files into ZIP archives)
+# Утилиты для сжатия файлов в ZIP архивы
+
+# Required:    no
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh" || exit 1
+
+SOURCES="${ROOT}/src"
+BUILD_DIR="/tmp/build-${PRGNAME}-${VERSION}"
+rm -rf "${BUILD_DIR}"
+mkdir -pv "${BUILD_DIR}"
+cd "${BUILD_DIR}" || exit 1
+
+tar xvf "${SOURCES}/${ARCH_NAME}".tar.?z* || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+cd "${ARCH_NAME}" || exit 1
+
+make -f unix/Makefile generic_gcc
+# пакет не содержит набора тестов
+make prefix="${TMP_DIR}/usr" MANDIR="${TMP_DIR}/usr/share/man/man1" \
+    -f unix/Makefile install
+
+source "${ROOT}/stripping.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (compressing files into ZIP archives)
+#
+# The Zip package contains Zip utilities. These are useful for compressing
+# files into ZIP archives.
+#
+# Home page: https://sourceforge.net/projects/infozip/
+# Download:  https://downloads.sourceforge.net/infozip/${ARCH_NAME}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
