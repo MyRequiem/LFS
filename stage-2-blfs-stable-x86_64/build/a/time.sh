aac8c34cc193489a3d609631931c08ab69772f47
Vladimir <mrvladislavovich@gmail.com>
2020-11-05 18:52:03 +0400

    BLFS: build/a/time.sh added

---
+++ stage-2-blfs-stable-x86_64/build/a/time.sh
@@ -0,0 +1,52 @@
+#! /bin/bash
+
+PRGNAME="time"
+
+### Time (the GNU time command for measuring program resource use)
+# Программа, которая измеряет ресурсы процессора, такие как время и память,
+# которые используют другие программы. Версия GNU может форматировать вывод
+# произвольно с помощью строки формата printf-style. Хотя оболочка bash имеет
+# встроенную команду 'time', обеспечивающую аналогичные функции, эта утилита
+# требуется LSB (Linux Standard Base)
+
+# Download: https://ftp.gnu.org/gnu/time/time-1.9.tar.gz
+
+# Required:    no
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                  || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+./configure \
+    --prefix=/usr || exit 1
+
+make || exit 1
+# make check
+make install DESTDIR="${TMP_DIR}"
+
+source "${ROOT}/stripping.sh" || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (the GNU time command for measuring program resource use)
+#
+# The time utility is a program that measures many of the CPU resources, such
+# as time and memory, that other programs use. The GNU version can format the
+# output in arbitrary ways by using a printf-style format string to include
+# various resource measurements. Although the shell has a builtin command
+# providing similar functionalities, this utility is required by the LSB (Linux
+# Standard Base)
+#
+# Home page: http://www.gnu.org/software/${PRGNAME}/
+# Download:  https://ftp.gnu.org/gnu/${PRGNAME}/${PRGNAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
