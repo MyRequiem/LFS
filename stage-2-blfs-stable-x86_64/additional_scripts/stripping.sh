f07a08e8a8727699dec50ee00fb341704820bb65
Vladimir <mrvladislavovich@gmail.com>
2020-11-07 09:40:45 +0400

    BLFS: additional_scripts/stripping.sh updated

--- stage-2-blfs-stable-x86_64/additional_scripts/stripping.sh
+++ stage-2-blfs-stable-x86_64/additional_scripts/stripping.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
-find "${TMP_DIR}" -type f -print0 | xargs -0 file 2>/dev/null | \
-    grep -e "executable" -e "shared object" | grep ELF | cut -f 1 -d : | \
-    xargs strip --strip-unneeded 2>/dev/null
+BINARY="$(find "${TMP_DIR}" -type f -print0 | xargs -0 file 2>/dev/null | \
+    grep -e "executable" -e "shared object" | grep ELF | cut -f 1 -d :)"
+
+for BIN in ${BINARY}; do
+    strip --strip-unneeded "${BIN}"
+done
