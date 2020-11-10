aa3690ad626a2563f158f4b06efd8bd8b97e6d1a
Vladimir <mrvladislavovich@gmail.com>
2020-11-10 20:59:05 +0400

    BLFS: build/d/python-modules/python2-libxml2.sh added

---
+++ stage-2-blfs-stable-x86_64/build/d/python-modules/python2-libxml2.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+
+PRGNAME="python2-libxml2"
+ARCH_NAME="libxml2"
+
+### python2-libxml2 (Python2 bindings for libxml2)
+# Python2 bindings для libxml2
+
+# Required:    libxml2
+#              python2
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+cd python || exit 1
+python2 setup.py build || exit 1
+python2 setup.py install --optimize=1 --root="${TMP_DIR}"
+
+source "${ROOT}/stripping.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (Python2 bindings for libxml2)
+#
+# Python2 bindings for libxml2
+#
+# Home page: http://xmlsoft.org/
+# Download:  http://xmlsoft.org/sources/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
