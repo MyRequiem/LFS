68555c0c0a477b1987332a2f41e92abf92085318
Vladimir <mrvladislavovich@gmail.com>
2020-11-11 18:01:54 +0400

    BLFS: build/l/json-c.sh added

--- stage-2-blfs-stable-x86_64/build/l/json-c.sh
+++ stage-2-blfs-stable-x86_64/build/l/json-c.sh
@@ -7,29 +7,35 @@ PRGNAME="json-c"
 # конструировать объекты JSON в C, выводить их как строки в формате JSON и
 # анализировать строки в JSON-формате обратно в C-представление объектов JSON
 
-# http://www.linuxfromscratch.org/blfs/view/stable/general/json-c.html
+# Required:    cmake
+# Recommended: no
+# Optional:    no
 
-# Home page: https://github.com/json-c/json-c
-# Download:  https://s3.amazonaws.com/json-c_releases/releases/json-c-0.13.1.tar.gz
-
-# Required: no
-# Optional: no
-
-ROOT="/root"
+ROOT="/root/src/lfs"
 source "${ROOT}/check_environment.sh"                  || exit 1
 source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
 
 TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
 mkdir -pv "${TMP_DIR}"
 
-./configure       \
-    --prefix=/usr \
-    --disable-static || exit 1
+mkdir build
+cd build || exit 1
+
+# применяем более высокий уровень оптимизации компилятора
+#    -DCMAKE_BUILD_TYPE=Release
+cmake                           \
+    -DCMAKE_INSTALL_PREFIX=/usr \
+    -DCMAKE_BUILD_TYPE=Release  \
+    -DBUILD_STATIC_LIBS=OFF     \
+    .. || exit 1
 
 make || exit 1
 # make check
-make install
 make install DESTDIR="${TMP_DIR}"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Package: ${PRGNAME} (JSON library in C)
@@ -45,7 +51,3 @@ EOF
 
 source "${ROOT}/write_to_var_log_packages.sh" \
     "${TMP_DIR}" "${PRGNAME}-${VERSION}"
-
-echo -e "\n---------------\nRemoving *.la files..."
-remove-la-files.sh
-echo "---------------"
