95fe1a210857b8c1b31d31d2c0da800fe660a544
Vladimir <mrvladislavovich@gmail.com>
2020-11-30 15:41:06 +0400

    BLFS: build/d/python-modules/python-six.sh added

--- stage-2-blfs-stable-x86_64/build/d/python-modules/python-six.sh
+++ stage-2-blfs-stable-x86_64/build/d/python-modules/python-six.sh
@@ -5,19 +5,14 @@ ARCH_NAME="six"
 
 ### python-six (Python2 and Python3 compatibility utilities)
 # Python библиотека совместимости Python2 и Python3. Обеспечивает функции
-# сглаживания различий между версиями Python с целью написания кода,
-# совместимого с Python2 и Python3
+# сглаживания различий между версиями Python с целью написания Python2/3 кода
 
-# http://www.linuxfromscratch.org/blfs/view/stable/general/python-modules.html#six
+# Required:    python2
+#              python3
+# Recommended: no
+# Optional:    no
 
-# Home page: https://pypi.python.org/pypi/six/
-# Download:  https://files.pythonhosted.org/packages/source/s/six/six-1.14.0.tar.gz
-
-# Required: python2
-#           python3
-# Optional: no
-
-ROOT="/root"
+ROOT="/root/src/lfs"
 source "${ROOT}/check_environment.sh"                    || exit 1
 source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
 
@@ -30,7 +25,10 @@ python3 setup.py build || exit 1
 # пакет не имеет набора тестов, сразу устанавливаем
 python2 setup.py install --optimize=1 --root="${TMP_DIR}"
 python3 setup.py install --optimize=1 --root="${TMP_DIR}"
-cp -vR "${TMP_DIR}"/* /
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Package: ${PRGNAME} (Python2 and Python3 compatibility utilities)
@@ -41,7 +39,7 @@ cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # documentation for more information on what is provided.
 #
 # Home page: https://pypi.python.org/pypi/${ARCH_NAME}/
-# Download:  https://pypi.io/packages/source/s/${ARCH_NAME}/${ARCH_NAME}-${VERSION}.tar.gz
+# Download:  https://files.pythonhosted.org/packages/source/s/${ARCH_NAME}/${ARCH_NAME}-${VERSION}.tar.gz
 #
 EOF
 
