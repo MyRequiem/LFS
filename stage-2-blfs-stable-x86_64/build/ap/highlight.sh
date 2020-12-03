74ea10617f98a9645ce6141b6e50a6ea46857582
Vladimir <mrvladislavovich@gmail.com>
2020-12-03 22:51:19 +0400

    BLFS: build/ap/highlight.sh added

--- stage-2-blfs-stable-x86_64/build/ap/highlight.sh
+++ stage-2-blfs-stable-x86_64/build/ap/highlight.sh
@@ -6,16 +6,12 @@ PRGNAME="highlight"
 # утилита для преобразования исходного кода в форматированный текст с
 # подсветкой синтаксиса
 
-# http://www.linuxfromscratch.org/blfs/view/stable/general/highlight.html
+# Required:    boost
+#              lua
+# Recommended: no
+# Optional:    qt5 (для сборки GUI интерфейса)
 
-# Home page: http://www.andre-simon.de/index.php
-# Download:  http://www.andre-simon.de/zip/highlight-3.55.tar.bz2
-
-# Required: boost
-#           lua
-# Optional: qt5 (для сборки GUI интерфейса)
-
-ROOT="/root"
+ROOT="/root/src/lfs"
 source "${ROOT}/check_environment.sh"                  || exit 1
 source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
 source "${ROOT}/config_file_processing.sh"             || exit 1
@@ -25,32 +21,33 @@ mkdir -pv "${TMP_DIR}"
 
 # не сжимаем man-страницы
 sed -i '/GZIP/s/^/#/' makefile || exit 1
-# установим правильный путь для документации
-sed -i "s#^doc_dir = .*#doc_dir = \${PREFIX}/share/doc/${PRGNAME}-${VERSION}#" \
-    makefile || exit 1
-sed -i "s#^examples_dir = .*/#examples_dir = \${doc_dir}/extras/#" \
-    makefile || exit 1
 
-QT5=""
-command -v assistant &>/dev/null && QT5="true"
+DOCS="/usr/share/doc/${PRGNAME}-${VERSION}/"
+GUI=""
+command -v assistant &>/dev/null && GUI="true"
 
-make
-[ -n "${QT5}" ] && make gui
+make || exit 1
+
+if [ -n "${GUI}" ]; then
+    make gui doc_dir="${DOCS}" || exit 1
+fi
 
 # пакет не содержит набора тестов
+
+make install doc_dir="${DOCS}" DESTDIR="${TMP_DIR}"
+
+if [ -n "${GUI}" ]; then
+    make install-gui
+fi
 
 CONFIG="/etc/highlight/filetypes.conf"
 if [ -f "${CONFIG}" ]; then
     mv "${CONFIG}" "${CONFIG}.old"
 fi
 
-make install
-make install DESTDIR="${TMP_DIR}"
-
-if [ -n "${QT5}" ]; then
-    make install-gui
-    make install-gui DESTDIR="${TMP_DIR}"
-fi
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
 
 config_file_processing "${CONFIG}"
 
