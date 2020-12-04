306ddfefd9138f61d93b3ced9f97c5b9593751d8
Vladimir <mrvladislavovich@gmail.com>
2020-12-04 11:41:18 +0400

    BLFS: build/n/libtirpc.sh added

--- stage-2-blfs-stable-x86_64/build/n/libtirpc.sh
+++ stage-2-blfs-stable-x86_64/build/n/libtirpc.sh
@@ -1,21 +1,20 @@
 #! /bin/bash
 
 PRGNAME="libtirpc"
-LIB_VERSION="3.0.0"
 
 ### libtirpc (Transport-Independent RPC library)
 # Библиотеки поддерживающие программы, которые используют API процедур
 # удаленного вызова (RPC).
 
-# http://www.linuxfromscratch.org/blfs/view/stable/basicnet/libtirpc.html
+# Required:    no
+# Recommended: no
+# Optional:    mit-kerberos-v5 (для gssapi)
 
-# Home page: http://sourceforge.net/projects/libtirpc/
-# Download:  https://downloads.sourceforge.net/libtirpc/libtirpc-1.2.5.tar.bz2
+### NOTE:
+# при обновлении этого пакета также необходимо обновить/пересобрать любую
+# существующую версию пакета rpcbind
 
-# Required: no
-# Optional: mit-kerberos-v5 (для gssapi)
-
-ROOT="/root"
+ROOT="/root/src/lfs"
 source "${ROOT}/check_environment.sh"                  || exit 1
 source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
 source "${ROOT}/config_file_processing.sh"             || exit 1
@@ -23,12 +22,8 @@ source "${ROOT}/config_file_processing.sh"             || exit 1
 TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
 mkdir -pv "${TMP_DIR}/lib"
 
-### NOTE:
-# при обновлении этого пакета также необходимо обновить любую существующую
-# версию пакета rpcbind
-
 GSSAPI="--disable-gssapi"
-command -v kadmind &>/dev/null && GSSAPI="--enable-gssapi"
+command -v kadmin &>/dev/null && GSSAPI="--enable-gssapi"
 
 ./configure           \
     --prefix=/usr     \
@@ -37,6 +32,19 @@ command -v kadmind &>/dev/null && GSSAPI="--enable-gssapi"
     "${GSSAPI}" || exit 1
 
 make || exit 1
+# пакет не имеет набора тестов
+make install DESTDIR="${TMP_DIR}"
+
+# переместим библиотеки из /usr/lib в /lib, чтобы они были доступны до
+# монтирования /usr
+mv -v "${TMP_DIR}/usr/lib/libtirpc.so".* "${TMP_DIR}/lib"
+
+# восстановим ссылку
+#    /usr/lib/libtirpc.so -> ../../lib/libtirpc.so.x.x.x
+(
+    cd "${TMP_DIR}/usr/lib" || exit 1
+    ln -svf "../../lib/$(readlink libtirpc.so)" libtirpc.so
+)
 
 NETCONFIG="/etc/netconfig"
 if [ -f "${NETCONFIG}" ]; then
@@ -48,24 +56,12 @@ if [ -f "${BINDRESVPORT}" ]; then
     mv "${BINDRESVPORT}" "${BINDRESVPORT}.old"
 fi
 
-# пакет не имеет набора тестов
-make install
-make install DESTDIR="${TMP_DIR}"
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
 
 config_file_processing "${NETCONFIG}"
 config_file_processing "${BINDRESVPORT}"
-
-# переместим библиотеки из /usr/lib в /lib, чтобы они были доступны до
-# монтирования /usr
-mv -v /usr/lib/libtirpc.so.* /lib
-mv -v "${TMP_DIR}/usr/lib/libtirpc.so".* "${TMP_DIR}/lib"
-
-# восстановим ссылку /usr/lib/libtirpc.so -> ../../lib/libtirpc.so.3.0.0
-ln -svf "../../lib/libtirpc.so.${LIB_VERSION}" /usr/lib/libtirpc.so
-(
-    cd "${TMP_DIR}/usr/lib" || exit 1
-    ln -svf "../../lib/libtirpc.so.${LIB_VERSION}" libtirpc.so
-)
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Package: ${PRGNAME} (Transport-Independent RPC library)
