0a161f00448990d1cb65f74a419e762519e79401
Vladimir <mrvladislavovich@gmail.com>
2020-12-16 12:54:26 +0400

    BLFS: build/n/p11-kit.sh added

--- stage-2-blfs-stable-x86_64/build/n/p11-kit.sh
+++ stage-2-blfs-stable-x86_64/build/n/p11-kit.sh
@@ -6,11 +6,6 @@ PRGNAME="p11-kit"
 # Пакет p11-kit предоставляет способ загрузки и перечисления PKCS#11 (Модулей
 # криптографического интерфейса Token Standard)
 
-# http://www.linuxfromscratch.org/blfs/view/stable/postlfs/p11-kit.html
-
-# Home page: http://p11-glue.freedesktop.org/p11-kit.html
-# Download:  https://github.com/p11-glue/p11-kit/releases/download/0.23.20/p11-kit-0.23.20.tar.xz
-
 # Required:   no
 # Recommended libtasn1
 #             make-ca
@@ -18,9 +13,10 @@ PRGNAME="p11-kit"
 #             libxslt
 #             nss (runtime)
 
-ROOT="/root"
+ROOT="/root/src/lfs"
 source "${ROOT}/check_environment.sh"                  || exit 1
 source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
+source "${ROOT}/config_file_processing.sh"             || exit 1
 
 TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
 mkdir -pv "${TMP_DIR}"
@@ -36,7 +32,7 @@ cat >> trust/trust-extract-compat.in << "EOF"
 EOF
 
 GTK_DOC="--disable-doc"
-command -v gtkdoc-check &>/dev/null && GTK_DOC="--enable-doc"
+# command -v gtkdoc-check &>/dev/null && GTK_DOC="--enable-doc"
 
 ./configure           \
     --prefix=/usr     \
@@ -46,21 +42,41 @@ command -v gtkdoc-check &>/dev/null && GTK_DOC="--enable-doc"
 
 make || exit 1
 # make check
-make install
 make install DESTDIR="${TMP_DIR}"
 
-ln -svf /usr/libexec/p11-kit/trust-extract-compat \
-    /usr/bin/update-ca-certificates
+[[ "x${GTK_DOC}" == "x--disable-doc" ]] && \
+    rm -rf "${TMP_DIR}/usr/share/gtk-doc"
+
+# ссылка
+#    /usr/bin/update-ca-certificates -> ../libexec/p11-kit/trust-extract-compat
 (
     cd "${TMP_DIR}/usr/bin" || exit 1
     ln -sfv ../libexec/p11-kit/trust-extract-compat update-ca-certificates
 )
 
-ln -svf ./pkcs11/p11-kit-trust.so /usr/lib/libnssckbi.so
+# чтобы сделать систему прозрачной для центров сертификации при использовании
+# приложений поддерживающих NSS, модуль /usr/lib/pkcs11/p11-kit-trust.so можно
+# использовать как замену для /usr/lib/libnssckbi.so
+#
+# создадим ссылку /usr/lib/libnssckbi.so -> pkcs11/p11-kit-trust.so
 (
     cd "${TMP_DIR}/usr/lib" || exit 1
     ln -svf ./pkcs11/p11-kit-trust.so libnssckbi.so
 )
+
+# конфиг /etc/pkcs11/pkcs11.conf
+CONFIG="/etc/pkcs11/pkcs11.conf"
+cp "${TMP_DIR}${CONFIG}.example" "${TMP_DIR}${CONFIG}"
+
+if [ -f "${CONFIG}" ]; then
+    mv "${CONFIG}" "${CONFIG}.old"
+fi
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+config_file_processing "${CONFIG}"
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Package: ${PRGNAME} (PKCS#11 toolkit)
