405f1415ae00619c97ae8b20e5f3f20234d88772
Vladimir <mrvladislavovich@gmail.com>
2020-12-16 21:06:20 +0400

    BLFS: build/l/nss.sh added

--- stage-2-blfs-stable-x86_64/build/l/nss.sh
+++ stage-2-blfs-stable-x86_64/build/l/nss.sh
@@ -8,23 +8,18 @@ PRGNAME="nss"
 # PKCS#7, PKCS#11, PKCS#12, сертификатов S/MIME, X.509 v3 и других стандартов
 # безопасности.
 
-# http://www.linuxfromscratch.org/blfs/view/svn/postlfs/nss.html
-
-# Home page: https://developer.mozilla.org/ru/docs/NSS
-# Download:  https://archive.mozilla.org/pub/security/nss/releases/NSS_3_55_RTM/src/nss-3.55.tar.gz
-# Patch:     http://www.linuxfromscratch.org/patches/blfs/svn/nss-3.55-standalone-1.patch
-
 # Required:    nspr
 # Recommended: sqlite
 #              p11-kit
 # Optional:    no
 
-ROOT="/root"
+ROOT="/root/src/lfs"
 source "${ROOT}/check_environment.sh"                  || exit 1
 source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
 
 TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
-mkdir -pv "${TMP_DIR}/usr"/{bin,"include/${PRGNAME}",lib/pkgconfig}
+NSS_INCLUDE_DIR="/usr/include/${PRGNAME}"
+mkdir -pv "${TMP_DIR}"{/usr/{bin,lib/pkgconfig},"${NSS_INCLUDE_DIR}"}
 
 patch -Np1 --verbose -i \
     "${SOURCES}/${PRGNAME}-${VERSION}-standalone-1.patch" || exit 1
@@ -60,24 +55,23 @@ make                                \
 
 cd ../dist || exit 1
 
-install -v -m755 Linux*/lib/*.so              /usr/lib
-install -v -m755 Linux*/lib/*.so              "${TMP_DIR}/usr/lib"
-install -v -m644 Linux*/lib/{*.chk,libcrmf.a} /usr/lib
-install -v -m644 Linux*/lib/{*.chk,libcrmf.a} "${TMP_DIR}/usr/lib"
-
-NSS_INCLUDE_DIR="/usr/include/${PRGNAME}"
-install -v -m755 -d "${NSS_INCLUDE_DIR}"
-
-cp -vRL {public,private}/"${PRGNAME}"/* "${NSS_INCLUDE_DIR}"
-cp -vRL {public,private}/"${PRGNAME}"/* "${TMP_DIR}${NSS_INCLUDE_DIR}"
-chmod -v 644 "${NSS_INCLUDE_DIR}"/*
-chmod -v 644 "${TMP_DIR}${NSS_INCLUDE_DIR}"/*
-
-install -v -m755 Linux*/bin/{certutil,nss-config,pk12util} /usr/bin
+# /usr/bin/
 install -v -m755 Linux*/bin/{certutil,nss-config,pk12util} "${TMP_DIR}/usr/bin"
 
-install -v -m644 Linux*/lib/pkgconfig/nss.pc  /usr/lib/pkgconfig
+# /usr/include/nss/
+cp -vRL {public,private}/"${PRGNAME}"/* "${TMP_DIR}${NSS_INCLUDE_DIR}"
+chmod -v 644 "${TMP_DIR}${NSS_INCLUDE_DIR}"/*
+
+# /usr/lib/
+install -v -m755 Linux*/lib/*.so              "${TMP_DIR}/usr/lib"
+install -v -m644 Linux*/lib/{*.chk,libcrmf.a} "${TMP_DIR}/usr/lib"
+
+# /usr/lib/pkgconfig/
 install -v -m644 Linux*/lib/pkgconfig/nss.pc  "${TMP_DIR}/usr/lib/pkgconfig"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
 
 VER="${VERSION//./_}"
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
