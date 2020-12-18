b50850c822abab299d447fc9e58523945277bdfd
Vladimir <mrvladislavovich@gmail.com>
2020-12-18 20:40:10 +0400

    BLFS: build/l/{libidn.sh,libidn2.sh} added

--- stage-2-blfs-stable-x86_64/build/l/libidn.sh
+++ stage-2-blfs-stable-x86_64/build/l/libidn.sh
@@ -1,45 +1,67 @@
 #! /bin/bash
 
-PRGNAME="libidn2"
+PRGNAME="libidn"
 
-### libidn2 (GNU Internationalized Domain Name library version 2)
+### libidn (GNU Internationalized Domain Name library)
 # Пакет, разработанный для интернационализированной обработки строк на основе
 # спецификации Stringprep, Punycode и IDNA, определенные в Интернете.
 # Применяется для конвертации данных из системного представления в UTF-8,
 # преобразуя Unicode строки в строки ASCII, позволяющие приложениям
 # использовать определенное доменное имя в ASCII формате.
 
-# http://www.linuxfromscratch.org/blfs/view/stable/general/libidn2.html
+# Required:    no
+# Recommended: no
+# Optional:    pth
+#              emacs
+#              gtk-doc  (для сборки API документации)
+#              openjdk
+#              valgrind
+#              mono     (https://www.mono-project.com/)
 
-# Home page: http://www.gnu.org/software/libidn/
-# Download:  https://ftp.gnu.org/gnu/libidn/libidn2-2.3.0.tar.gz
-
-# Required: libunistring
-# Optional: git
-#           gtk-doc (для создания API документации)
-
-ROOT="/root"
+ROOT="/root/src/lfs"
 source "${ROOT}/check_environment.sh"                  || exit 1
 source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
 
 TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
-mkdir -pv "${TMP_DIR}"
+DOCS="/usr/share/doc/${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}${DOCS}"
 
+THREADS="posix"
 GTK_DOC="--disable-gtk-doc"
-command -v gtkdoc-check &>/dev/null && GTK_DOC="--enable-gtk-doc"
+OPENJDK="--disable-java"
+VALGRIND="--disable-valgrind-tests"
+MONO="--disable-csharp"
 
-./configure       \
-    --prefix=/usr \
-    "${GTK_DOC}"  \
+command -v pth-config   &>/dev/null && THREADS="pth"
+command -v gtkdoc-check &>/dev/null && GTK_DOC="--enable-gtk-doc"
+command -v java         &>/dev/null && OPENJDK="--enable-java"
+command -v valgrind     &>/dev/null && VALGRIND="--enable-valgrind-tests"
+command -v mono         &>/dev/null && MONO="--enable-csharp"
+
+./configure                       \
+    --prefix=/usr                 \
+    --enable-threads="${THREADS}" \
+    "${GTK_DOC}"                  \
+    "${OPENJDK}"                  \
+    "${VALGRIND}"                 \
+    "${MONO}"                     \
     --disable-static || exit 1
 
 make || exit 1
 # make check
-make install
 make install DESTDIR="${TMP_DIR}"
 
+# документация
+find doc -name "Makefile*" -delete
+rm -rfv doc/{gdoc*,stamp-vti,*texi,man,idn.1,*.info}
+cp -Rv doc/* "${TMP_DIR}${DOCS}"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
-# Package: ${PRGNAME} (GNU Internationalized Domain Name library version 2)
+# Package: ${PRGNAME} (GNU Internationalized Domain Name library)
 #
 # libidn is a package designed for internationalized string handling based on
 # the Stringprep, Punycode and IDNA specifications defined by the Internet
@@ -51,8 +73,8 @@ cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # and converting entire domain names to and from the ASCII Compatible Encoding
 # (ACE) form.
 #
-# Home page: http://www.gnu.org/software/libidn/
-# Download:  https://ftp.gnu.org/gnu/libidn/${PRGNAME}-${VERSION}.tar.gz
+# Home page: http://www.gnu.org/software/${PRGNAME}/
+# Download:  https://ftp.gnu.org/gnu/${PRGNAME}/${PRGNAME}-${VERSION}.tar.gz
 #
 EOF
 
--- stage-2-blfs-stable-x86_64/build/l/libidn2.sh
+++ stage-2-blfs-stable-x86_64/build/l/libidn2.sh
@@ -1,28 +1,20 @@
 #! /bin/bash
 
-PRGNAME="libidn"
+PRGNAME="libidn2"
 
-### libidn (GNU Internationalized Domain Name library)
+### libidn2 (GNU Internationalized Domain Name library version 2)
 # Пакет, разработанный для интернационализированной обработки строк на основе
 # спецификации Stringprep, Punycode и IDNA, определенные в Интернете.
 # Применяется для конвертации данных из системного представления в UTF-8,
 # преобразуя Unicode строки в строки ASCII, позволяющие приложениям
 # использовать определенное доменное имя в ASCII формате.
 
-# http://www.linuxfromscratch.org/blfs/view/stable/general/libidn.html
+# Required:    libunistring
+# Recommended: no
+# Optional:    git
+#              gtk-doc (для создания API документации)
 
-# Home page: http://www.gnu.org/software/libidn/
-# Download:  https://ftp.gnu.org/gnu/libidn/libidn-1.35.tar.gz
-
-# Required: no
-# Optional: pth
-#           emacs
-#           gtk-doc (для сборки API документации)
-#           openjdk
-#           valgrind
-#           mono (https://www.mono-project.com/)
-
-ROOT="/root"
+ROOT="/root/src/lfs"
 source "${ROOT}/check_environment.sh"                  || exit 1
 source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
 
@@ -30,41 +22,32 @@ TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
 DOCS="/usr/share/doc/${PRGNAME}-${VERSION}"
 mkdir -pv "${TMP_DIR}${DOCS}"
 
-THREADS="posix"
 GTK_DOC="--disable-gtk-doc"
-OPENJDK="--disable-java"
-VALGRIND="--disable-valgrind-tests"
-MONO="--disable-csharp"
+# command -v gtkdoc-check &>/dev/null && GTK_DOC="--enable-gtk-doc"
 
-command -v pth-config   &>/dev/null && THREADS="pth"
-command -v gtkdoc-check &>/dev/null && GTK_DOC="--enable-gtk-doc"
-command -v java         &>/dev/null && OPENJDK="--enable-java"
-command -v valgrind     &>/dev/null && VALGRIND="--enable-valgrind-tests"
-command -v mono         &>/dev/null && MONO="--enable-csharp"
-
-./configure                       \
-    --prefix=/usr                 \
-    --enable-threads="${THREADS}" \
-    "${GTK_DOC}"                  \
-    "${OPENJDK}"                  \
-    "${VALGRIND}"                 \
-    "${MONO}"                     \
+./configure       \
+    --prefix=/usr \
+    "${GTK_DOC}"  \
     --disable-static || exit 1
 
 make || exit 1
 # make check
-make install
 make install DESTDIR="${TMP_DIR}"
 
 # документация
-mkdir -pv "${DOCS}"
 find doc -name "Makefile*" -delete
-rm -rfv doc/{gdoc,idn.1,stamp-vti,man,texi}
-cp -Rv doc/* "${DOCS}"
+rm -rfv doc/{gdoc*,stamp-vti,*texi,man,idn2.1,*.info,*.c}
 cp -Rv doc/* "${TMP_DIR}${DOCS}"
 
+[[ "x${GTK_DOC}" == "x--disable-gtk-doc" ]] && \
+    rm -rf "${TMP_DIR}/usr/share/gtk-doc"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
-# Package: ${PRGNAME} (GNU Internationalized Domain Name library)
+# Package: ${PRGNAME} (GNU Internationalized Domain Name library version 2)
 #
 # libidn is a package designed for internationalized string handling based on
 # the Stringprep, Punycode and IDNA specifications defined by the Internet
@@ -76,8 +59,8 @@ cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # and converting entire domain names to and from the ASCII Compatible Encoding
 # (ACE) form.
 #
-# Home page: http://www.gnu.org/software/${PRGNAME}/
-# Download:  https://ftp.gnu.org/gnu/${PRGNAME}/${PRGNAME}-${VERSION}.tar.gz
+# Home page: http://www.gnu.org/software/libidn/
+# Download:  https://ftp.gnu.org/gnu/libidn/${PRGNAME}-${VERSION}.tar.gz
 #
 EOF
 
