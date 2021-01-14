2fc927983dc218b731b359327b1f07fb231a81be
Vladimir <mrvladislavovich@gmail.com>
2021-01-14 20:20:40 +0400

    BLFS: build/ap/at.sh added

--- stage-2-blfs-stable-x86_64/build/ap/at.sh
+++ stage-2-blfs-stable-x86_64/build/ap/at.sh
@@ -7,15 +7,11 @@ PRGNAME="at"
 # сохраняя их как задание, которое будет запланировано для отложенного
 # выполнения. Пакет требуется для соответствия LSB (Linux Standards Base)
 
-# http://www.linuxfromscratch.org/blfs/view/stable/general/at.html
+# Required:    MTA (dovecot или exim или postfix или sendmail)
+# Recommended: no
+# Optional:    linux-pam
 
-# Home page: https://salsa.debian.org/debian/at
-# Download:  http://ftp.debian.org/debian/pool/main/a/at/at_3.1.23.orig.tar.gz
-
-# Required: MTA (dovecot или exim или postfix или sendmail)
-# Optional: linux-pam
-
-ROOT="/root"
+ROOT="/root/src/lfs"
 source "${ROOT}/check_environment.sh"      || exit 1
 source "${ROOT}/config_file_processing.sh" || exit 1
 
@@ -31,7 +27,8 @@ tar xvf "${SOURCES}/${PRGNAME}_${VERSION}"*.tar.?z* || exit 1
 cd "${PRGNAME}-${VERSION}" || exit 1
 
 TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
-mkdir -pv "${TMP_DIR}"
+DOCS="/usr/share/doc/${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}${DOCS}"
 
 # перед сборкой пакета должны существовать группа и пользователь atd, который
 # будет запускать демон atd
@@ -46,21 +43,23 @@ mkdir -pv "${TMP_DIR}"
             -s /bin/false         \
             -u 17 atd
 
-# создадим каталог /var/spool/cron
-mkdir -pv /var/spool/cron
-
-# исправим путь установки документации
-sed -i '/docdir/s/=.*/= @docdir@/' Makefile.in
-
-autoreconf || exit 1
-./configure                     \
-    --with-daemon_username=atd  \
-    --with-daemon_groupname=atd \
+./configure \
+    --with-daemon_username=atd        \
+    --with-daemon_groupname=atd       \
+    --with-jobdir=/var/spool/atjobs   \
+    --with-atspool=/var/spool/atspool \
     SENDMAIL=/usr/sbin/sendmail || exit 1
 
-# пакет "не любит" сборку в несколько пакетов
+# пакет "не любит" сборку в несколько потоков
 make -j1 || exit 1
 # пакет не имеет набора тестов
+make install docdir="${DOCS}" atdocdir="${DOCS}" IROOT="${TMP_DIR}"
+
+# скрипт /etc/init.d/atd для запуска демона atd при старте системы
+(
+    cd /root/src/lfs/blfs-bootscripts || exit 1
+    make install-atd DESTDIR="${TMP_DIR}"
+)
 
 AT_ALLOW="/etc/at.allow"
 if [ -f "${AT_ALLOW}" ]; then
@@ -72,19 +71,12 @@ if [ -f "${AT_DENY}" ]; then
     mv "${AT_DENY}" "${AT_DENY}.old"
 fi
 
-DOCS="/usr/share/doc/${PRGNAME}-${VERSION}"
-make install docdir="${DOCS}" atdocdir="${DOCS}"
-make install docdir="${DOCS}" atdocdir="${DOCS}" IROOT="${TMP_DIR}"
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
 
 config_file_processing "${AT_ALLOW}"
 config_file_processing "${AT_DENY}"
-
-# скрипт /etc/init.d/atd для запуска демона atd при старте системы
-(
-    cd /root/blfs-bootscripts || exit 1
-    make install-atd
-    make install-atd DESTDIR="${TMP_DIR}"
-)
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Package: ${PRGNAME} (delayed command execution)
@@ -94,7 +86,7 @@ cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # required for Linux Standards Base (LSB) conformance.
 #
 # Home page: https://salsa.debian.org/debian/${PRGNAME}
-# Download:  http://ftp.debian.org/debian/pool/main/a/${PRGNAME}/${PRGNAME}_${VERSION}.orig.tar.gz
+# Download:  http://software.calhariz.com/${PRGNAME}/${PRGNAME}_${VERSION}.orig.tar.gz
 #
 EOF
 
