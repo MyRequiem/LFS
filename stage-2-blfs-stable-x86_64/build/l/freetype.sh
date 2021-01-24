16c3d505ff7548333249c15201d6c5d1aa816557
Vladimir <mrvladislavovich@gmail.com>
2021-01-24 14:56:24 +0400

    BLFS: build/l/freetype.sh added

--- stage-2-blfs-stable-x86_64/build/l/freetype.sh
+++ stage-2-blfs-stable-x86_64/build/l/freetype.sh
@@ -6,30 +6,20 @@ PRGNAME="freetype"
 # Библиотека (портативный движок рендеринга), которая позволяет приложениям
 # рендерить шрифты TrueType, OpenType и Type 1
 
-# http://www.linuxfromscratch.org/blfs/view/stable/general/freetype2.html
-
-# Home page: http://www.freetype.org
-# Download:  https://downloads.sourceforge.net/freetype/freetype-2.10.1.tar.xz
-# Docs:      https://downloads.sourceforge.net/freetype/freetype-doc-2.10.1.tar.xz
-
 # Required:    no
 # Recommended: harfbuzz
 #              libpng
 #              which
-# Optional:    docwriter (для сборки документации) https://pypi.org/project/docwriter/
+# Optional:    brotli
+#              docwriter (для сборки документации) https://pypi.org/project/docwriter/
 
-ROOT="/root"
+ROOT="/root/src/lfs"
 source "${ROOT}/check_environment.sh"                  || exit 1
 source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
 
 TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
 DOCS="/usr/share/doc/${PRGNAME}-${VERSION}"
-mkdir -pv "${TMP_DIR}${DOCS}"
-
-# распакуем документацию в директорию docs
-tar -xvf "${SOURCES}/${PRGNAME}-doc-${VERSION}.tar.xz" \
-    --strip-components=2 \
-    -C docs
+mkdir -pv "${TMP_DIR}${DOCS}/docs-html"
 
 # включим проверку таблиц GX/AAT и OpenType
 #    # AUX_MODULES += gxvalid --> AUX_MODULES +=
@@ -38,8 +28,8 @@ sed -ri "s:.*(AUX_MODULES.*valid):\1:" modules.cfg || exit 1
 
 # включим субпиксельный рендеринг (раскомментируем определение
 #    #define FT_CONFIG_OPTION_SUBPIXEL_RENDERING)
-sed -r  "s:.*(#.*SUBPIXEL_RENDERING) .*:\1:" \
-    -i include/freetype/config/ftoption.h          || exit 1
+sed -r "s:.*(#.*SUBPIXEL_RENDERING) .*:\1:" \
+    -i include/freetype/config/ftoption.h || exit 1
 
 # устанавливать man-страницы для freetype-config
 #    --enable-freetype-config
@@ -50,21 +40,20 @@ sed -r  "s:.*(#.*SUBPIXEL_RENDERING) .*:\1:" \
 
 make || exit 1
 # пакет не содержит набора тестов
-make install
 make install DESTDIR="${TMP_DIR}"
 
-# вручную разместим утилиту freetype-config в /usr/bin, необходимую для других
-# программ, которые используют библиотеку FreeType
-cp builds/unix/freetype-config /usr/bin
-cp builds/unix/freetype-config "${TMP_DIR}/usr/bin"
-
 # установим документацию
-install -v -m755 -d "${DOCS}"
-cp -vR docs/* "${DOCS}"
+mv docs/reference docs/api-reference-html
+rm -f docs/freetype-config.1
 cp -vR docs/* "${TMP_DIR}${DOCS}"
 
-rm -fv "${DOCS}/freetype-config.1"
-rm -fv "${TMP_DIR}${DOCS}/freetype-config.1"
+tar -xvf "${SOURCES}/${PRGNAME}-doc-${VERSION}.tar.xz" \
+    --strip-components=2 \
+    -C "${TMP_DIR}${DOCS}/docs-html"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Package: ${PRGNAME} (A free, high-quality, and portable font engine)
@@ -75,7 +64,7 @@ cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # and portable while capable of producing high-quality output.
 #
 # Home page: http://www.freetype.org
-# Download:  https://download.savannah.gnu.org/releases/${PRGNAME}/${PRGNAME}-${VERSION}.tar.xz
+# Download:  https://downloads.sourceforge.net/${PRGNAME}/${PRGNAME}-${VERSION}.tar.xz
 #
 EOF
 
