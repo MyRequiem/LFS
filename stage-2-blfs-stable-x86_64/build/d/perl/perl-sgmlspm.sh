33b0254a6d0bed3e99c2e6c4b168caa7187e630b
Vladimir <mrvladislavovich@gmail.com>
2021-02-01 02:13:21 +0400

    BLFS: build/d/perl/perl-sgmlspm.sh added

---
+++ stage-2-blfs-stable-x86_64/build/d/perl/perl-sgmlspm.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+PRGNAME="perl-sgmlspm"
+ARCH_NAME="SGMLSpm"
+
+### SGMLSpm (Perl library for parsing the output SGMLS and NSGMLS)
+# Perl-библиотека, используемая для анализа SGMLS и NSGMLS файлов
+
+# Required:    no
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+chmod -v 644 MYMETA.yml
+perl Makefile.PL || exit 1
+make || exit 1
+# make test
+make install DESTDIR="${TMP_DIR}"
+
+ln -sv sgmlspl.pl "${TMP_DIR}/usr/bin/sgmlspl"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (Perl library for parsing the output SGMLS and NSGMLS)
+#
+# The SGMLSpm module is a Perl library used for parsing the output from James
+# Clark's SGMLS and NSGMLS parsers.
+#
+# Home page: https://www.cpan.org/authors/id/R/RA/RAAB/
+# Download:  https://www.cpan.org/authors/id/R/RA/RAAB/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
