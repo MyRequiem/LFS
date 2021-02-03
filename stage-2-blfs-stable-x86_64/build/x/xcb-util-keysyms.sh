33b778da55e405d9555541fac3ae38916201d95a
Vladimir <mrvladislavovich@gmail.com>
2021-02-03 18:33:16 +0400

    BLFS: build/x/xcb-util-keysyms.sh added

---
+++ stage-2-blfs-stable-x86_64/build/x/xcb-util-keysyms.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+PRGNAME="xcb-util-keysyms"
+
+### xcb-util-keysyms (keysyms library for XCB)
+# Библиотека, обрабатывающая стандартные X key constants и преобразующая их в
+# коды клавиш и наоборот.
+
+# Required:    libxcb
+# Recommended: no
+# Optional:    doxygen (для создания документации)
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                  || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
+source "${ROOT}/xorg_config.sh"                        || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+# shellcheck disable=SC2086
+./configure        \
+    ${XORG_CONFIG} || exit 1
+
+make || exit 1
+# пакет не имеет набора тестов
+make install DESTDIR="${TMP_DIR}"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (keysyms library for XCB)
+#
+# xcb-util-keysyms provides the keysyms library, which handles standard X key
+# constants and conversion to/from keycodes.
+#
+# Home page: https://xcb.freedesktop.org/
+# Download:  https://xcb.freedesktop.org/dist/${PRGNAME}-${VERSION}.tar.bz2
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
+
+echo -e "\n---------------\nRemoving *.la files..."
+remove-la-files.sh
+echo "---------------"
