c3008df57c6ed1cb80afa18b00837b2b107dcd09
Vladimir <mrvladislavovich@gmail.com>
2021-02-03 23:00:15 +0400

    BLFS: build/x/xcb-util-cursor.sh added

---
+++ stage-2-blfs-stable-x86_64/build/x/xcb-util-cursor.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+
+PRGNAME="xcb-util-cursor"
+
+### xcb-util-cursor (XCB cursor management library)
+# Библиотека управления курсорами XCB, которая является заменой для libXcursor
+
+# Required:    xcb-util
+# Recommended: no
+# Optional:    doxygen (для сборки документации)
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                  || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
+source "${ROOT}/xorg_config.sh"                        || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+# shellcheck disable=SC2086
+./configure        \
+    ${XORG_CONFIG} || exit 1
+
+make || exit 1
+# пакет не имеет набора тестов
+make install DESTDIR="${TMP_DIR}"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (XCB cursor management library)
+#
+# The xcb-util-cursor module implements the XCB cursor library, which is the
+# XCB replacement for libXcursor.
+#
+# Home page: https://xcb.freedesktop.org/
+# Download:  https://xcb.freedesktop.org/dist/${PRGNAME}-${VERSION}.tar.bz2
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
+
+echo -e "\n---------------\nRemoving *.la files..."
+remove-la-files.sh
+echo "---------------"
