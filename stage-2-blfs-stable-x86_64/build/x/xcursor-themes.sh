34268cc77c4c485b8bd1438daa28b1d49ce7edd6
Vladimir <mrvladislavovich@gmail.com>
2021-02-06 12:22:38 +0400

    BLFS: build/x/xcursor-themes.sh added

---
+++ stage-2-blfs-stable-x86_64/build/x/xcursor-themes.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+PRGNAME="xcursor-themes"
+
+### xcursor-themes (X11 cursor themes)
+# Анимированные redglass и whiteglass темы курсора для использования с
+# libXcursor
+
+# Required:    xorg-applications
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                  || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
+source "${ROOT}/xorg_config.sh"                        || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+# shellcheck disable=SC2086
+./configure        \
+    ${XORG_CONFIG} || exit 1
+
+make || exit 1
+# пакет не имеет набора тестов
+make install DESTDIR="${TMP_DIR}"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (X11 cursor themes)
+#
+# The xcursor-themes package contains the redglass and whiteglass animated
+# cursor themes for use with libXcursor
+#
+# Home page: https://www.x.org
+# Download:  https://www.x.org/pub/individual/data/${PRGNAME}-${VERSION}.tar.bz2
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
