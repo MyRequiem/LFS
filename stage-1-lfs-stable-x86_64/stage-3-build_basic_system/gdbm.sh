f13cce47954613896889e1b456ce7ef38e645bc9
Vladimir <mrvladislavovich@gmail.com>
2021-03-07 21:22:13 +0400

    LFS:  gdbm package updated

--- stage-1-lfs-stable-x86_64/stage-3-build_basic_system/gdbm.sh
+++ stage-1-lfs-stable-x86_64/stage-3-build_basic_system/gdbm.sh
@@ -17,9 +17,6 @@ TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
 rm -rf "${TMP_DIR}"
 mkdir -pv "${TMP_DIR}"
 
-# устраним проблему, обнаруженную при сборке с gcc-10
-sed -r -i '/^char.*parseopt_program_(doc|args)/d' src/parseopt.c
-
 # ключ позволяет создать библиотеку совместимости, содержащую старые функции
 # DBM (libgdbm_compat.so), так как некоторые пакеты за пределами LFS могут
 # требовать этих более старых процедур
@@ -33,7 +30,18 @@ make || make -j1 || exit 1
 # make check
 make install DESTDIR="${TMP_DIR}"
 
+rm -f "${TMP_DIR}/usr/share/info/dir"
+
 /bin/cp -vR "${TMP_DIR}"/* /
+
+# система документации Info использует простые текстовые файлы в
+# /usr/share/info/, а список этих файлов хранится в файле /usr/share/info/dir
+# который мы обновим
+cd /usr/share/info || exit 1
+rm -fv dir
+for FILE in *; do
+    install-info "${FILE}" dir 2>/dev/null
+done
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Package: ${PRGNAME} (GNU database routines)
