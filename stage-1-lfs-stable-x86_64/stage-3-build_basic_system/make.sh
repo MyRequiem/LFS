9e022d49278461844cc182fa414aa97e0c3c79f6
Vladimir <mrvladislavovich@gmail.com>
2021-03-08 17:42:23 +0400

    LFS:  stage-3-build_basic_system/make.sh updated

--- stage-1-lfs-stable-x86_64/stage-3-build_basic_system/make.sh
+++ stage-1-lfs-stable-x86_64/stage-3-build_basic_system/make.sh
@@ -20,7 +20,18 @@ make || make -j1 || exit 1
 # make check
 make install DESTDIR="${TMP_DIR}"
 
+rm -f "${TMP_DIR}/usr/share/info/dir"
+
 /bin/cp -vR "${TMP_DIR}"/* /
+
+# система документации Info использует простые текстовые файлы в
+# /usr/share/info/, а список этих файлов хранится в файле /usr/share/info/dir
+# который мы обновим
+cd /usr/share/info || exit 1
+rm -fv dir
+for FILE in *; do
+    install-info "${FILE}" dir 2>/dev/null
+done
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Package: ${PRGNAME} (GNU make utility to maintain groups of programs)
