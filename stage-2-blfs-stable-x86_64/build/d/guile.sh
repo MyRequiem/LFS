c7fcedc98082f2c6402908a00ec8a4940a3f1192
Vladimir <mrvladislavovich@gmail.com>
2021-03-23 18:47:32 +0400

    BLFS: guile package updated

--- stage-2-blfs-stable-x86_64/build/d/guile.sh
+++ stage-2-blfs-stable-x86_64/build/d/guile.sh
@@ -26,30 +26,30 @@ mkdir -pv "${TMP_DIR}${LIBS}"
     --docdir="/usr/share/doc/${PRGNAME}-${VERSION}" || exit 1
 
 make      || exit 1
-make html || exit 1
+# make html || exit 1
 
-makeinfo --plaintext -o doc/r5rs/r5rs.txt doc/r5rs/r5rs.texi || exit 1
-makeinfo --plaintext -o doc/ref/guile.txt doc/ref/guile.texi || exit 1
+# makeinfo --plaintext -o doc/r5rs/r5rs.txt doc/r5rs/r5rs.texi || exit 1
+# makeinfo --plaintext -o doc/ref/guile.txt doc/ref/guile.texi || exit 1
 
 # тесты
 # ./check-guile
 
-make install      DESTDIR="${TMP_DIR}"
-make install-html DESTDIR="${TMP_DIR}"
+make install DESTDIR="${TMP_DIR}"
+# make install-html DESTDIR="${TMP_DIR}"
 
 mv "${TMP_DIR}/usr/lib/libguile-"*-gdb.scm "${TMP_DIR}${LIBS}"
 
-DOCS="/usr/share/doc/${PRGNAME}-${VERSION}"
-mv "${TMP_DIR}${DOCS}"/{guile.html,ref}
-mv "${TMP_DIR}${DOCS}"/r5rs{.html,}
+# DOCS="/usr/share/doc/${PRGNAME}-${VERSION}"
+# mv "${TMP_DIR}${DOCS}"/{guile.html,ref}
+# mv "${TMP_DIR}${DOCS}"/r5rs{.html,}
 
-find examples -name "Makefile*" -delete
-cp -vR examples "${TMP_DIR}${DOCS}"
+# find examples -name "Makefile*" -delete
+# cp -vR examples "${TMP_DIR}${DOCS}"
 
-for DIRNAME in ref r5rs; do
-    install -v -m644  "doc/${DIRNAME}"/*.txt "${TMP_DIR}${DOCS}/${DIRNAME}"
-done
-unset DIRNAME
+# for DIRNAME in ref r5rs; do
+#     install -v -m644  "doc/${DIRNAME}"/*.txt "${TMP_DIR}${DOCS}/${DIRNAME}"
+# done
+# unset DIRNAME
 
 source "${ROOT}/stripping.sh"      || exit 1
 source "${ROOT}/update-info-db.sh" || exit 1
