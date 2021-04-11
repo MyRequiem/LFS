97320d977d4a8066fe1fba098035c72560c4f563
Vladimir <mrvladislavovich@gmail.com>
2021-04-11 14:42:02 +0400

    LFS:  stage-2-build_temp_system/stage-1/file.sh updated
        - Thanks to BimboPolitics (https://github.com/BimboPolitics)

--- stage-1-lfs-stable-x86_64/stage-2-build_temp_system/stage-1/file.sh
+++ stage-1-lfs-stable-x86_64/stage-2-build_temp_system/stage-1/file.sh
@@ -8,9 +8,27 @@ PRGNAME="file"
 source "$(pwd)/check_environment.sh"                  || exit 1
 source "$(pwd)/unpack_source_archive.sh" "${PRGNAME}" || exit 1
 
-./configure       \
-    --prefix=/usr \
-    --host="${LFS_TGT}" || exit 1
+# утилита 'file' на хосте должна быть той же версии, что и та, которую мы
+# создаем для LFS (для создания файла подписи). Создадим утилиту на хосте:
+mkdir -p build
+pushd build || exit 1
+
+../configure             \
+    --disable-bzlib      \
+    --disable-libseccomp \
+    --disable-xzlib      \
+    --disable-zlib || exit 1
 
 make || make -j1 || exit 1
+popd || exit 1
+
+# конфигурируем и собираем пакет для LFS
+./configure             \
+    --prefix=/usr       \
+    --host="${LFS_TGT}" \
+    --build="$(./config.guess)" || exit 1
+
+make FILE_COMPILE="$(pwd)/build/src/file" || \
+    make -j1 FILE_COMPILE="$(pwd)/build/src/file" || exit 1
+
 make install DESTDIR="${LFS}"
