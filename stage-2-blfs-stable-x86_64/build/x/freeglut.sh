34985586c93ebda5e193631b320be0e9c428e96c
Vladimir <mrvladislavovich@gmail.com>
2021-04-19 23:25:52 +0400

    BLFS: freeglut package added

---
+++ stage-2-blfs-stable-x86_64/build/x/freeglut.sh
@@ -0,0 +1,61 @@
+#! /bin/bash
+
+PRGNAME="freeglut"
+
+### Freeglut (alternative GLUT library)
+# Полностью открытый и 100% совместимый клон библиотеки OpenGL Utility Toolkit
+# (GLUT). GLUT - это независимый от Window System инструментарий для написания
+# OpenGL-программ, реализующий простой оконный API, что делает достаточно
+# легким обучение OpenGL-программированию.
+
+# Required:    cmake
+#              mesa
+# Recommended: glu
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                  || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+# исправим проблему сборки с GCC-10
+patch --verbose -Np1 \
+    -i "${SOURCES}/${PRGNAME}-${VERSION}-gcc10_fix-1.patch" || exit 1
+
+mkdir build
+cd build || exit 1
+
+# отключим создание дополнительных демонстрационных программ (рекомендуется)
+#    -DFREEGLUT_BUILD_DEMOS=OFF
+cmake \
+    -DCMAKE_INSTALL_PREFIX=/usr       \
+    -DCMAKE_BUILD_TYPE=Release        \
+    -DFREEGLUT_BUILD_DEMOS=OFF        \
+    -DFREEGLUT_BUILD_STATIC_LIBS=OFF  \
+    -Wno-dev .. || exit 1
+
+make || exit 1
+# пакет не имеет набора тестов
+make install DESTDIR="${TMP_DIR}"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (alternative GLUT library)
+#
+# freeglut is a completely OpenSourced alternative to the OpenGL Utility
+# Toolkit (GLUT) library. GLUT (and hence freeglut) allows the user to create
+# and manage windows containing OpenGL contexts on a wide range of platforms
+# and also read the mouse, keyboard, and joystick functions.
+#
+# Home page: http://freeglut.sourceforge.net/
+# Download:  https://downloads.sourceforge.net/${PRGNAME}/${PRGNAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
