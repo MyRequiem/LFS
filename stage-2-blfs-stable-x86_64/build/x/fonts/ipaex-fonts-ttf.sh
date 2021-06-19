5512795139aff45bd25b7682386be69f8ba9029b
Vladimir <mrvladislavovich@gmail.com>
2021-06-19 14:02:59 +0400

    BLFS: ipaex-fonts-ttf package added

---
+++ stage-2-blfs-stable-x86_64/build/x/fonts/ipaex-fonts-ttf.sh
@@ -0,0 +1,61 @@
+#! /bin/bash
+
+PRGNAME="ipaex-fonts-ttf"
+ARCH_NAME="IPAexfont"
+
+### IPAex fonts (Japanese fonts by IPA)
+# Японский шрифт фиксированной ширины
+
+# Required:    xorg-applications
+#              fontconfig
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh" || exit 1
+
+SOURCES="${ROOT}/src"
+VERSION="$(find "${SOURCES}" -type f \
+    -name "${ARCH_NAME}*.zip" 2>/dev/null | sort | head -n 1 | rev | \
+    cut -d . -f 2- | cut -d t -f 1 | rev)"
+
+BUILD_DIR="/tmp/build-${PRGNAME}-${VERSION}"
+rm -rf "${BUILD_DIR}"
+mkdir -pv "${BUILD_DIR}"
+cd "${BUILD_DIR}" || exit 1
+
+unzip "${SOURCES}/${ARCH_NAME}${VERSION}".zip || exit 1
+cd "${ARCH_NAME}${VERSION}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+TTF_FONT_DIR="/usr/share/fonts/X11/TTF/"
+mkdir -pv "${TMP_DIR}${TTF_FONT_DIR}"
+
+chmod 644 ipaex{g,m}.ttf
+cp ipaex{g,m}.ttf "${TMP_DIR}${TTF_FONT_DIR}"
+
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+# обновим индексы установленных шрифтов
+cd "${TTF_FONT_DIR}" || exit 1
+# создаем индекс файлов масштабируемых шрифтов
+mkfontscale .
+# создаем индекс файлов шрифтов в каталоге
+mkfontdir .
+# создаем файлы кэша информации о шрифтах для fontconfig
+fc-cache -f
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (Japanese fonts by IPA)
+#
+# IPAex fonts feature fixed width glyphs for Japanese characters and
+# proportional width glyphs for Western characters. They were designed as an
+# implementation of JIS X 0213:2004 standard.
+#
+# Home page: https://ipafont.ipa.go.jp
+# Download:  https://moji.or.jp/wp-content/ipafont/${ARCH_NAME}/${ARCH_NAME}${VERSION}.zip
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
