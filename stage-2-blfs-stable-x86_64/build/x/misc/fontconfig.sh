c35a0012db3723101b10de1d41e3e7937c87770e
Vladimir <mrvladislavovich@gmail.com>
2021-06-20 19:03:30 +0400

    BLFS: build/x/misc/fontconfig.sh updated

--- stage-2-blfs-stable-x86_64/build/x/misc/fontconfig.sh
+++ stage-2-blfs-stable-x86_64/build/x/misc/fontconfig.sh
@@ -20,7 +20,7 @@ source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
 source "${ROOT}/config_file_processing.sh"             || exit 1
 
 TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
-mkdir -pv "${TMP_DIR}"
+mkdir -pv "${TMP_DIR}/etc/fonts/conf.avail"
 
 # для сборки документации нужны пакеты texlive или install-tl-unx,
 # docbook-utils и perl-sgmlspm
@@ -51,14 +51,14 @@ make install DESTDIR="${TMP_DIR}"
 #
 # конфиги:
 #    /etc/fonts/*
-#    /etc/fonts/conf.d/*
+#    /etc/fonts/conf.avail/*
 #    /usr/share/fontconfig/conf.avail/*
 #
 # основной файл конфигурации:
 #    /etc/fonts/fonts.conf
 # он также читает
 #    /etc/fonts/local.conf
-# и все файлы в
+# и все ссылки на конфиги в
 #    /etc/fonts/conf.d/
 #
 # Чтобы сконфигурировать новый каталог со шрифтами, создаем/правим
@@ -88,6 +88,63 @@ if [[ "x${INSTALL_DOCS}" == "x--disable-docs" ]]; then
     install -v -m644 doc/*.3          "${TMP_DIR}${MAN}/man3"
     install -v -m644 doc/fonts-conf.5 "${TMP_DIR}${MAN}/man5"
 fi
+
+# отключаем уродливые старые растровые (bitmap) шрифты Xorg
+NO_BITMAPS_CONF="/etc/fonts/conf.avail/70-no-bitmaps.conf"
+cat << EOF > "${TMP_DIR}${NO_BITMAPS_CONF}"
+<?xml version='1.0'?>
+<!DOCTYPE fontconfig SYSTEM 'fonts.dtd'>
+<fontconfig>
+    <!-- Reject bitmap fonts -->
+    <selectfont>
+        <rejectfont>
+            <pattern>
+                <patelt name="scalable">
+                    <bool>false</bool>
+                </patelt>
+            </pattern>
+        </rejectfont>
+    </selectfont>
+</fontconfig>
+EOF
+
+# настройка CJK шрифтов (Корейский, Японский и Китайский)
+CJK_FONTS_CONF="/etc/fonts/conf.avail/70-cjk.conf"
+cat << EOF > "${TMP_DIR}${CJK_FONTS_CONF}"
+<?xml version='1.0'?>
+<!DOCTYPE fontconfig SYSTEM 'fonts.dtd'>
+<fontconfig>
+    <alias>
+        <family>serif</family>
+        <prefer>
+            <family>AR PL UMing</family>
+            <family>IPAexMincho</family>
+        </prefer>
+    </alias>
+    <alias>
+        <family>sans-serif</family>
+        <prefer>
+            <family>WenQuanYi Zen Hei</family>
+            <family>VL Gothic</family>
+            <family>IPAexGothic</family>
+        </prefer>
+    </alias>
+    <alias>
+        <family>monospace</family>
+        <prefer>
+            <family>VL Gothic</family>
+            <family>IPAexGothic</family>
+            <family>WenQuanYi Zen Hei</family>
+        </prefer>
+    </alias>
+</fontconfig>
+EOF
+
+(
+    cd "${TMP_DIR}/etc/fonts/conf.d" || exit 1
+    ln -s ../conf.avail/70-no-bitmaps.conf 70-no-bitmaps.conf
+    ln -s ../conf.avail/70-cjk.conf 70-cjk.conf
+)
 
 FONTS_CONFIG="/etc/fonts/fonts.conf"
 if [ -f "${FONTS_CONFIG}" ]; then
