2faa28a4eacbd2e9e001c6dd8edfd9f854312023
Vladimir <mrvladislavovich@gmail.com>
2021-06-22 15:21:29 +0400

    BLFS: cairomm package added

---
+++ stage-2-blfs-stable-x86_64/build/l/cairomm.sh
@@ -0,0 +1,56 @@
+#! /bin/bash
+
+PRGNAME="cairomm"
+
+### cairomm (C++ wrapper for the cairo graphics library)
+# C++ интерфейс для графической библиотеки cairo
+
+# Required:    cairo
+#              libsigc++2
+# Recommended: boost (для тестов)
+# Optional:    doxygen
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                  || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+DOCS="false"
+TESTS="false"
+EXAMPLES="false"
+
+mkdir _build &&
+cd _build || exit 1
+
+meson                               \
+    --prefix=/usr                   \
+    -Dboost-shared=true             \
+    -Dbuild-tests="${TESTS}"        \
+    -Dbuild-examples="${EXAMPLES}"  \
+    -Dbuild-documentation="${DOCS}" \
+      .. || exit 1
+
+ninja || exit 1
+# ninja test
+DESTDIR="${TMP_DIR}" ninja install
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (C++ wrapper for the cairo graphics library)
+#
+# cairomm is a C++ wrapper (C++ interface) for the cairo graphics library. It
+# offers all the power of cairo with an interface familiar to C++ developers,
+# including use of the Standard Template Library where it makes sense.
+#
+# Home page: https://www.cairographics.org/${PRGNAME}/
+# Download:  https://www.cairographics.org/releases/${PRGNAME}-${VERSION}.tar.xz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
