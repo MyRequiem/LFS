3ec0b75595337de1156203b1fd0444fcee7f375b
Vladimir <mrvladislavovich@gmail.com>
2021-06-24 14:20:31 +0400

    BLFS: imlib2 package added

---
+++ stage-2-blfs-stable-x86_64/build/l/imlib2.sh
@@ -0,0 +1,64 @@
+#! /bin/bash
+
+PRGNAME="imlib2"
+
+### imlib2 (successor to Imlib)
+# Графическая библиотека для быстрой загрузки, сохранения, рендеринга,
+# модификации изображений
+
+# Required:    xorg-libraries
+# Recommended: no
+# Optional:    libpng
+#              libjpeg-turbo
+#              libtiff
+#              giflib
+#              libid3tag (https://sourceforge.net/projects/mad/)
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                  || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+DOCS="false"
+
+./configure       \
+    --prefix=/usr \
+    --disable-static || exit 1
+
+make || exit 1
+# пакет не имеет набора тестов
+make install DESTDIR="${TMP_DIR}"
+
+if [[ "x${DOCS}" == "xtrue" ]]; then
+    DOC_DIR="/usr/share/doc/${PRGNAME}-${VERSION}"
+    install -v -m755 -d                     "${TMP_DIR}${DOC_DIR}"
+    install -v -m644 doc/{*.gif,index.html} "${TMP_DIR}${DOC_DIR}"
+fi
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (successor to Imlib)
+#
+# imlib2 is a graphics library for fast file loading, saving, rendering and
+# manipulation, which load image files from disk in one of many formats, save
+# images to disk in one of many formats, render image data onto other images,
+# render images to an X-Windows drawable, produce pixmaps and pixmap masks of
+# images, apply filters to images, rotate images, accept RGBA data for images,
+# scale images, and more.
+#
+# Home page: https://www.enlightenment.org
+# Download:  https://downloads.sourceforge.net/enlightenment/${PRGNAME}-${VERSION}.tar.bz2
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
+
+echo -e "\n---------------\nRemoving *.la files..."
+remove-la-files.sh
+echo "---------------"
