aa493c409db5897c0d1779cbd4571212f1a2f270
Vladimir <mrvladislavovich@gmail.com>
2021-06-28 15:38:50 +0400

    BLFS: gsettings-desktop-schemas package added

---
+++ stage-2-blfs-stable-x86_64/build/l/gsettings-desktop-schemas.sh
@@ -0,0 +1,54 @@
+#! /bin/bash
+
+PRGNAME="gsettings-desktop-schemas"
+
+### gsettings-desktop-schemas (GSettings schemas)
+# Набор схем GSettings для настройки различных компонентов рабочего стола
+
+# Required:    glib
+#              gobject-introspection
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                  || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+# исправим некоторые устаревшие записи в шаблонах схем
+sed -i -r 's:"(/system):"/org/gnome\1:g' schemas/*.in &&
+
+mkdir build
+cd build || exit 1
+
+meson             \
+    --prefix=/usr \
+    .. || exit 1
+
+ninja || exit 1
+# пакет не имеет набора тестов
+DESTDIR="${TMP_DIR}" ninja install
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+# создадим/обновим /usr/share/glib-2.0/schemas/gschemas.compiled
+glib-compile-schemas /usr/share/glib-2.0/schemas
+
+MAJ_VERSION="$(echo "${VERSION}" | cut -d . -f 1,2)"
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (GSettings schemas)
+#
+# gsettings-desktop-schemas contains a collection of GSettings schemas for
+# settings shared by various components of a desktop.
+#
+# Home page: https://github.com/GNOME/${PRGNAME}
+# Download:  https://download.gnome.org/sources/${PRGNAME}/${MAJ_VERSION}/${PRGNAME}-${VERSION}.tar.xz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
