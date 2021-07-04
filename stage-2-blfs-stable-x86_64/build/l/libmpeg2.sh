e5214458549ce2cad3228b2ba80a49eda83fba54
Vladimir <mrvladislavovich@gmail.com>
2021-07-04 23:13:07 +0400

    BLFS: libmpeg2 package added

---
+++ stage-2-blfs-stable-x86_64/build/l/libmpeg2.sh
@@ -0,0 +1,63 @@
+#! /bin/bash
+
+PRGNAME="libmpeg2"
+
+### libmpeg2 (mpeg-video decoding library)
+# Библиотека для декодирования видео MPEG-2 и MPEG-1 потоков
+
+# Required:    no
+# Recommended: no
+# Optional:    X Window System
+#              sdl
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                  || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+DOCS="false"
+
+# устраним проблему сборки с GCC >=10.2.0
+sed -i 's/static const/static/' "${PRGNAME}/idct_mmx.c" || exit 1
+
+./configure         \
+    --prefix=/usr   \
+    --enable-shared \
+    --disable-static || exit 1
+
+make || exit 1
+# make check
+make install DESTDIR="${TMP_DIR}"
+
+if [[ "x${DOCS}" == "xtrue"  ]]; then
+    DOC_PATH="/usr/share/doc/${PRGNAME}-${VERSION}"
+    install -v -m755 -d "${TMP_DIR}${DOC_PATH}"
+    install -v -m644 README "doc/${PRGNAME}.txt" "${TMP_DIR}${DOC_PATH}"
+fi
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (mpeg-video decoding library)
+#
+# The libmpeg2 package contains a library for decoding MPEG-2 and MPEG-1 video
+# streams. The library is able to decode all MPEG streams that conform to
+# certain restrictions: ?constrained parameters? for MPEG-1, and ?main profile?
+# for MPEG-2. This is useful for programs and applications needing to decode
+# MPEG-2 and MPEG-1 video streams.
+#
+# Home page: http://${PRGNAME}.sourceforge.net/
+# Download:  http://${PRGNAME}.sourceforge.net/files/${PRGNAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
+
+echo -e "\n---------------\nRemoving *.la files..."
+remove-la-files.sh
+echo "---------------"
