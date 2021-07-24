8816ad07d75d3a5c0444a1e225bc3127d6c4caa7
Vladimir <mrvladislavovich@gmail.com>
2021-07-24 23:12:39 +0400

    BLFS: python3-pygobject3 package added

--- stage-2-blfs-stable-x86_64/build/d/python-modules/python3-pygobject3.sh
+++ stage-2-blfs-stable-x86_64/build/d/python-modules/python3-pygobject3.sh
@@ -1,24 +1,25 @@
 #! /bin/bash
 
-PRGNAME="python2-pygobject2"
+PRGNAME="python3-pygobject3"
 ARCH_NAME="pygobject"
 
-### PyGObject (GObject bindings for Python2)
-# Python2 bindings для GObject
+### PyGObject3 (GObject bindings for Python3)
+# Python3 bindings для GObject
 
-# Required:    glib
-#              python2-pycairo
-#              python2
+# Required:    gobject-introspection
+#              python3-pycairo
+#              python3
 # Recommended: no
-# Optional:    gobject-introspection
-#              libxslt (для сборки документации)
+# Optional:    pep8     (https://pypi.org/project/pep8/)
+#              pyflakes (https://pypi.org/project/pyflakes/)
+#              pytest   (https://pypi.org/project/pytest/)
 
 ROOT="/root/src/lfs"
 source "${ROOT}/check_environment.sh" || exit 1
 
 SOURCES="${ROOT}/src"
 VERSION="$(find "${SOURCES}" -type f \
-    -name "${ARCH_NAME}-2*.tar.?z*" 2>/dev/null | sort | head -n 1 | \
+    -name "${ARCH_NAME}-3*.tar.?z*" 2>/dev/null | sort | head -n 1 | \
     rev | cut -d . -f 3- | cut -d - -f 1 | rev)"
 
 BUILD_DIR="/tmp/build-${PRGNAME}-${VERSION}"
@@ -32,20 +33,19 @@ mkdir -pv "${TMP_DIR}"
 tar xvf "${SOURCES}/${ARCH_NAME}-${VERSION}"*.tar.?z* || exit 1
 cd "${ARCH_NAME}-${VERSION}" || exit 1
 
-GTK_DOC="false"
+mkdir build
+cd build || exit 1
 
-# отключаем сборку с установленным gobject-introspection, т.к. пакет будет
-# конфликтовать с пакетом python3-pygobject3
-#    --disable-introspection
-./configure       \
+meson             \
     --prefix=/usr \
-    --disable-introspection || exit 1
+    .. || exit 1
 
-make || exit 1
-# пакет не имеет набора тестов
-make install DESTDIR="${TMP_DIR}"
+ninja || exit 1
 
-[[ "x${GTK_DOC}" == "xfalse" ]] && rm -rf "${TMP_DIR}/usr/share/gtk-doc"
+# тесты необходимо проводить в графической среде
+# ninja test
+
+DESTDIR="${TMP_DIR}" ninja install
 
 source "${ROOT}/stripping.sh"      || exit 1
 source "${ROOT}/update-info-db.sh" || exit 1
@@ -53,7 +53,7 @@ source "${ROOT}/update-info-db.sh" || exit 1
 
 MAJ_VERSION="$(echo "${VERSION}" | cut -d . -f 1,2)"
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
-# Package: ${PRGNAME} (GObject bindings for Python2)
+# Package: ${PRGNAME} (GObject bindings for Python3)
 #
 # This archive contains bindings for the GObject, to be used in Python. It is a
 # fairly complete set of bindings, it's already rather useful, and is usable to
@@ -66,7 +66,3 @@ EOF
 
 source "${ROOT}/write_to_var_log_packages.sh" \
     "${TMP_DIR}" "${PRGNAME}-${VERSION}"
-
-echo -e "\n---------------\nRemoving *.la files..."
-remove-la-files.sh
-echo "---------------"
