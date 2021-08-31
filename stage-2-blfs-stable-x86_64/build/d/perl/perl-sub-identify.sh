4fc748574f6a024e068ff5b07ac97cef26dd1a44
Vladimir <mrvladislavovich@gmail.com>
2021-08-31 15:38:07 +0400

    BLFS: perl-module-build.sh, perl-sgmlspm.sh, perl-sub-identify.sh fixed

--- stage-2-blfs-stable-x86_64/build/d/perl/perl-sub-identify.sh
+++ stage-2-blfs-stable-x86_64/build/d/perl/perl-sub-identify.sh
@@ -22,6 +22,14 @@ make             || exit 1
 # make test
 make install DESTDIR="${TMP_DIR}"
 
+# исправим пути (убираем из путей временную директорию установки пакета)
+PERL_MAJ_VER="$(perl -v | /bin/grep version | cut -d \( -f 2 | cut -d v -f 2 | \
+    cut -d . -f 1,2)"
+PERL_MAIN_VER="$(echo "${PERL_MAJ_VER}" | cut -d . -f 1)"
+PERL_LIB_PATH="/usr/lib/perl${PERL_MAIN_VER}/${PERL_MAJ_VER}"
+sed -e "s|${TMP_DIR}||" -i \
+    "${TMP_DIR}${PERL_LIB_PATH}/site_perl/auto/Sub/Identify/.packlist"
+
 source "${ROOT}/stripping.sh"      || exit 1
 source "${ROOT}/update-info-db.sh" || exit 1
 /bin/cp -vpR "${TMP_DIR}"/* /
