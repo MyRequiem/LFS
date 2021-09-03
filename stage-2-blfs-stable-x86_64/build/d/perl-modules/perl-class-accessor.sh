45da163424d8fb2f48bf7fd98c2f4ad4a86e1f78
Vladimir <mrvladislavovich@gmail.com>
2021-09-03 22:58:33 +0400

    BLFS: perl-class-accessor package added

---
+++ stage-2-blfs-stable-x86_64/build/d/perl-modules/perl-class-accessor.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+
+PRGNAME="perl-class-accessor"
+ARCH_NAME="Class-Accessor"
+
+### Class::Accessor (generates accessors/mutators for your class)
+# Perl модуль для генерации аксессоров класса
+
+# Required:    no
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+perl Makefile.PL || exit 1
+make             || exit 1
+# make test
+make install DESTDIR="${TMP_DIR}"
+
+# исправим пути (убираем из путей временную директорию установки пакета)
+PERL_MAJ_VER="$(perl -v | /bin/grep version | cut -d \( -f 2 | cut -d v -f 2 | \
+    cut -d . -f 1,2)"
+PERL_MAIN_VER="$(echo "${PERL_MAJ_VER}" | cut -d . -f 1)"
+PERL_LIB_PATH="/usr/lib/perl${PERL_MAIN_VER}/${PERL_MAJ_VER}"
+sed -e "s|${TMP_DIR}||" -i \
+    "${TMP_DIR}${PERL_LIB_PATH}/site_perl/auto/Class/Accessor/.packlist"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (generates accessors/mutators for your class)
+#
+# Perl module Class::Accessor generates accessors/mutators for your class
+#
+# Home page: https://metacpan.org/pod/Class::Accessor
+# Download:  https://www.cpan.org/authors/id/K/KA/KASEI/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
