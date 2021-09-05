d13b1a3bac6c14d6d2c0dc06a50c0ebd334ce444
Vladimir <mrvladislavovich@gmail.com>
2021-09-05 21:16:39 +0400

    BLFS: perl-clone, perl-file-find-rule and perl-text-glob packages added

---
+++ stage-2-blfs-stable-x86_64/build/d/perl-modules/perl-file-find-rule.sh
@@ -0,0 +1,50 @@
+#! /bin/bash
+
+PRGNAME="perl-file-find-rule"
+ARCH_NAME="File-Find-Rule"
+
+### File::Find::Rule (friendlier interface to File::Find)
+# Perl модуль реализующий более удобный интерфейс для File::Find
+
+# Required:    perl-number-compare
+#              perl-text-glob
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+perl Makefile.PL || exit 1
+make             || exit 1
+# make test
+make install DESTDIR="${TMP_DIR}"
+
+# исправим пути (убираем из путей временную директорию установки пакета)
+PERL_MAJ_VER="$(perl -v | /bin/grep version | cut -d \( -f 2 | cut -d v -f 2 | \
+    cut -d . -f 1,2)"
+PERL_MAIN_VER="$(echo "${PERL_MAJ_VER}" | cut -d . -f 1)"
+PERL_LIB_PATH="/usr/lib/perl${PERL_MAIN_VER}/${PERL_MAJ_VER}"
+sed -e "s|${TMP_DIR}||" -i \
+    "${TMP_DIR}${PERL_LIB_PATH}/site_perl/auto/File/Find/Rule/.packlist"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (friendlier interface to File::Find)
+#
+# File::Find::Rule is a friendlier interface to File::Find. It allows you to
+# build rules which specify the desired files and directories
+#
+# Home page: https://metacpan.org/pod/File::Find::Rule
+# Download:  https://cpan.metacpan.org/authors/id/R/RC/RCLAMP/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
---
+++ stage-2-blfs-stable-x86_64/build/d/perl-modules/perl-text-glob.sh
@@ -0,0 +1,50 @@
+#! /bin/bash
+
+PRGNAME="perl-text-glob"
+ARCH_NAME="Text-Glob"
+
+### Text::Glob (glob style matching)
+# Perl модуль реализующий сопоставление имен файлов в стиле glob, без получения
+# имен из файловой системы
+
+# Required:    no
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+perl Makefile.PL || exit 1
+make             || exit 1
+# make test
+make install DESTDIR="${TMP_DIR}"
+
+# исправим пути (убираем из путей временную директорию установки пакета)
+PERL_MAJ_VER="$(perl -v | /bin/grep version | cut -d \( -f 2 | cut -d v -f 2 | \
+    cut -d . -f 1,2)"
+PERL_MAIN_VER="$(echo "${PERL_MAJ_VER}" | cut -d . -f 1)"
+PERL_LIB_PATH="/usr/lib/perl${PERL_MAIN_VER}/${PERL_MAJ_VER}"
+sed -e "s|${TMP_DIR}||" -i \
+    "${TMP_DIR}${PERL_LIB_PATH}/site_perl/auto/Text/Glob/.packlist"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (glob style matching)
+#
+# Text::Glob implements glob(3) style matching that can be used to match
+# against text, rather than fetching names from a filesystem
+#
+# Home page: https://metacpan.org/pod/Text::Glob
+# Download:  https://cpan.metacpan.org/authors/id/R/RC/RCLAMP/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
