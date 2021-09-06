4582fce40b2231506fbb35212d9c59dd8f6a31b7
Vladimir <mrvladislavovich@gmail.com>
2021-09-06 13:47:18 +0400

    BLFS: perl-data-dump package added

---
+++ stage-2-blfs-stable-x86_64/build/d/perl-modules/perl-data-dump.sh
@@ -0,0 +1,50 @@
+#! /bin/bash
+
+PRGNAME="perl-data-dump"
+ARCH_NAME="Data-Dump"
+
+### Data::Dump (pretty printing of data structures)
+# Perl модуль Data::Dump
+
+# Required:    no
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+perl Makefile.PL || exit 1
+make             || exit 1
+# make test
+make install DESTDIR="${TMP_DIR}"
+
+# исправим пути (убираем из путей временную директорию установки пакета)
+PERL_MAJ_VER="$(perl -v | /bin/grep version | cut -d \( -f 2 | cut -d v -f 2 | \
+    cut -d . -f 1,2)"
+PERL_MAIN_VER="$(echo "${PERL_MAJ_VER}" | cut -d . -f 1)"
+PERL_LIB_PATH="/usr/lib/perl${PERL_MAIN_VER}/${PERL_MAJ_VER}"
+sed -e "s|${TMP_DIR}||" -i \
+    "${TMP_DIR}${PERL_LIB_PATH}/site_perl/auto/Data/Dump/.packlist"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (pretty printing of data structures)
+#
+# This Perl module provides a few functions that traverse their argument list
+# and return a string containing Perl code that, when evaled, produces a deep
+# copy of the original arguments
+#
+# Home page: https://metacpan.org/pod/Data::Dump
+# Download:  https://cpan.metacpan.org/authors/id/G/GA/GARU/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
