33d0e58db2b429b23dc7e1db00445d965b974fb1
Vladimir <mrvladislavovich@gmail.com>
2021-09-07 22:23:33 +0400

    BLFS: perl-try-tiny package added

---
+++ stage-2-blfs-stable-x86_64/build/d/perl-modules/perl-try-tiny.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+PRGNAME="perl-try-tiny"
+ARCH_NAME="Try-Tiny"
+
+### Try::Tiny (Try::Tiny Perl module)
+# Try::Tiny Perl модуль
+
+# Required:    no
+# Recommended: no
+# Optional:    perl-capture-tiny (для тестов)
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+perl Makefile.PL || exit 1
+make             || exit 1
+# make test
+make install DESTDIR="${TMP_DIR}"
+
+# исправим пути (убираем из путей временную директорию установки пакета)
+PERL_MAJ_VER="$(perl -v | /bin/grep version | cut -d \( -f 2 | cut -d v -f 2 | \
+    cut -d . -f 1,2)"
+PERL_MAIN_VER="$(echo "${PERL_MAJ_VER}" | cut -d . -f 1)"
+PERL_LIB_PATH="/usr/lib/perl${PERL_MAIN_VER}/${PERL_MAJ_VER}"
+sed -e "s|${TMP_DIR}||" -i \
+    "${TMP_DIR}${PERL_LIB_PATH}/site_perl/auto/Try/Tiny/.packlist"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (Try::Tiny Perl module)
+#
+# Try::Tiny provides try and catch to expect and handle exceptional conditions,
+# avoiding quirks in Perl and common mistakes
+#
+# Home page: https://metacpan.org/pod/Try::Tiny
+# Download:  https://cpan.metacpan.org/authors/id/E/ET/ETHER/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
