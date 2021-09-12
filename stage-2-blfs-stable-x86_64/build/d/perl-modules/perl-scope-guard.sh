e1250e489d7e51732d803dc86be8b638f434300f
Vladimir <mrvladislavovich@gmail.com>
2021-09-12 20:18:19 +0400

    BLFS: perl-scope-guard package added

---
+++ stage-2-blfs-stable-x86_64/build/d/perl-modules/perl-scope-guard.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+PRGNAME="perl-scope-guard"
+ARCH_NAME="Scope-Guard"
+
+### Scope::Guard (convenient way to perform cleanup)
+# Scope::Guard Perl модуль
+
+# Required:    no
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+perl Makefile.PL || exit 1
+make             || exit 1
+# make test
+make install DESTDIR="${TMP_DIR}"
+
+# исправим пути (убираем из путей временную директорию установки пакета)
+PERL_MAJ_VER="$(perl -v | /bin/grep version | cut -d \( -f 2 | cut -d v -f 2 | \
+    cut -d . -f 1,2)"
+PERL_MAIN_VER="$(echo "${PERL_MAJ_VER}" | cut -d . -f 1)"
+PERL_LIB_PATH="/usr/lib/perl${PERL_MAIN_VER}/${PERL_MAJ_VER}"
+sed -e "s|${TMP_DIR}||" -i \
+    "${TMP_DIR}${PERL_LIB_PATH}/site_perl/auto/Scope/Guard/.packlist"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (convenient way to perform cleanup)
+#
+# Scope::Guard provides a convenient way to perform cleanup or other forms of
+# resource management at the end of a scope
+#
+# Home page: https://metacpan.org/pod/Scope::Guard
+# Download:  https://cpan.metacpan.org/authors/id/C/CH/CHOCOLATE/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
