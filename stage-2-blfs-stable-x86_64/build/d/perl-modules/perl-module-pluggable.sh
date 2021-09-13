13cdc09b611e8bf2d5fda1176bd2281e2d3b7c44
Vladimir <mrvladislavovich@gmail.com>
2021-09-13 19:32:35 +0400

    BLFS: perl-module-pluggable and perl-test2-suite packages added

---
+++ stage-2-blfs-stable-x86_64/build/d/perl-modules/perl-module-pluggable.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+
+PRGNAME="perl-module-pluggable"
+ARCH_NAME="Module-Pluggable"
+
+### Module::Pluggable (provides a way of having 'plugins' for your module)
+# Module::Pluggable Perl модуль
+
+# Required:    no
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+perl Makefile.PL || exit 1
+make             || exit 1
+# make test
+make install DESTDIR="${TMP_DIR}"
+
+# исправим пути (убираем из путей временную директорию установки пакета)
+PERL_MAJ_VER="$(perl -v | /bin/grep version | cut -d \( -f 2 | cut -d v -f 2 | \
+    cut -d . -f 1,2)"
+PERL_MAIN_VER="$(echo "${PERL_MAJ_VER}" | cut -d . -f 1)"
+PERL_LIB_PATH="/usr/lib/perl${PERL_MAIN_VER}/${PERL_MAJ_VER}"
+sed -e "s|${TMP_DIR}||" -i \
+    "${TMP_DIR}${PERL_LIB_PATH}/site_perl/auto/Module/Pluggable/.packlist"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (provides a way of having 'plugins' for your module)
+#
+# Module::Pluggable provides a way of having 'plugins' for your module
+#
+# Home page: https://metacpan.org/pod/Module::Pluggable
+# Download:  https://cpan.metacpan.org/authors/id/S/SI/SIMONW/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
