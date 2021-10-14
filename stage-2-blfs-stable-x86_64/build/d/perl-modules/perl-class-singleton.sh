972a567ddbad3c077af670e3a32e6c704f2b3b02
Vladimir <mrvladislavovich@gmail.com>
2021-10-14 20:24:07 +0400

    BLFS: perl-class-singleton package added

---
+++ stage-2-blfs-stable-x86_64/build/d/perl-modules/perl-class-singleton.sh
@@ -0,0 +1,50 @@
+#! /bin/bash
+
+PRGNAME="perl-class-singleton"
+ARCH_NAME="Class-Singleton"
+
+### Class::Singleton (implementation of a "Singleton" Perl class)
+# Class::Singleton Perl модуль
+
+# Required:    no
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+perl Makefile.PL || exit 1
+make             || exit 1
+# make test
+make install DESTDIR="${TMP_DIR}"
+
+# исправим пути (убираем из путей временную директорию установки пакета)
+PERL_MAJ_VER="$(perl -v | /bin/grep version | cut -d \( -f 2 | cut -d v -f 2 | \
+    cut -d . -f 1,2)"
+PERL_MAIN_VER="$(echo "${PERL_MAJ_VER}" | cut -d . -f 1)"
+PERL_LIB_PATH="/usr/lib/perl${PERL_MAIN_VER}/${PERL_MAJ_VER}"
+sed -e "s|${TMP_DIR}||" -i \
+    "${TMP_DIR}${PERL_LIB_PATH}/site_perl/auto/Class/Singleton/.packlist"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (implementation of a "Singleton" Perl class)
+#
+# A Singleton describes an object class that can have only one instance in any
+# system, such as a print spooler. This module implements a Singelton class
+# from which other classes can be derived.
+#
+# Home page: https://metacpan.org/pod/Class::Singleton
+# Download:  https://cpan.metacpan.org/authors/id/S/SH/SHAY/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
