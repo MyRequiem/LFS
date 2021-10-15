0bd4fbd62bf7dbb5882bb9a1a717f19de66f3c9b
Vladimir <mrvladislavovich@gmail.com>
2021-10-15 13:11:35 +0400

    BLFS: perl-file-sharedir-install package added

---
+++ stage-2-blfs-stable-x86_64/build/d/perl-modules/perl-file-sharedir-install.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+PRGNAME="perl-file-sharedir-install"
+ARCH_NAME="File-ShareDir-Install"
+
+### File::ShareDir::Install (install shared files)
+# File::ShareDir::Install Perl модуль
+
+# Required:    no
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+perl Makefile.PL || exit 1
+make             || exit 1
+# make test
+make install DESTDIR="${TMP_DIR}"
+
+# исправим пути (убираем из путей временную директорию установки пакета)
+PERL_MAJ_VER="$(perl -v | /bin/grep version | cut -d \( -f 2 | cut -d v -f 2 | \
+    cut -d . -f 1,2)"
+PERL_MAIN_VER="$(echo "${PERL_MAJ_VER}" | cut -d . -f 1)"
+PERL_LIB_PATH="/usr/lib/perl${PERL_MAIN_VER}/${PERL_MAJ_VER}"
+sed -e "s|${TMP_DIR}||" -i \
+    "${TMP_DIR}${PERL_LIB_PATH}/site_perl/auto/File/ShareDir/Install/.packlist"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (install shared files)
+#
+# File::ShareDir::Install allows you to install read-only data files from a
+# distribution
+#
+# Home page: https://metacpan.org/pod/File::ShareDir::Install
+# Download:  https://cpan.metacpan.org/authors/id/E/ET/ETHER/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
