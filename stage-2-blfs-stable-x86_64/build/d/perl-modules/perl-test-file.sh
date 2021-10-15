0877d0beb287c172fb0a59fff66679651f55af5d
Vladimir <mrvladislavovich@gmail.com>
2021-10-15 20:41:38 +0400

    BLFS: perl-test-file package added

---
+++ stage-2-blfs-stable-x86_64/build/d/perl-modules/perl-test-file.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+
+PRGNAME="perl-test-file"
+ARCH_NAME="Test-File"
+
+### Test::File (test file attributes)
+# Test::File Perl модуль
+
+# Required:    no
+# Recommended: perl-test-utf8 (для тестов)
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+perl Makefile.PL || exit 1
+make             || exit 1
+# make test
+make install DESTDIR="${TMP_DIR}"
+
+# исправим пути (убираем из путей временную директорию установки пакета)
+PERL_MAJ_VER="$(perl -v | /bin/grep version | cut -d \( -f 2 | cut -d v -f 2 | \
+    cut -d . -f 1,2)"
+PERL_MAIN_VER="$(echo "${PERL_MAJ_VER}" | cut -d . -f 1)"
+PERL_LIB_PATH="/usr/lib/perl${PERL_MAIN_VER}/${PERL_MAJ_VER}"
+sed -e "s|${TMP_DIR}||" -i \
+    "${TMP_DIR}${PERL_LIB_PATH}/site_perl/auto/Test/File/.packlist"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (test file attributes)
+#
+# Test::File provides a collection of test utilities for file attributes
+#
+# Home page: https://metacpan.org/pod/Test::File
+# Download:  https://cpan.metacpan.org/authors/id/B/BD/BDFOY/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
