30c0de39716d874810b4fa6f9d0d6baefa6c3ea3
Vladimir <mrvladislavovich@gmail.com>
2021-10-18 17:24:08 +0400

    BLFS: perl-datetime-locale package added

---
+++ stage-2-blfs-stable-x86_64/build/d/perl-modules/perl-datetime-locale.sh
@@ -0,0 +1,53 @@
+#! /bin/bash
+
+PRGNAME="perl-datetime-locale"
+ARCH_NAME="DateTime-Locale"
+
+### DateTime::Locale (localization support for DateTime.pm)
+# DateTime::Locale Perl модуль
+
+# Required:    perl-dist-checkconflicts
+#              perl-file-sharedir
+#              perl-namespace-autoclean
+#              perl-params-validationcompiler
+# Recommended: perl-cpan-meta-check    (для тестов)
+#              perl-ipc-system-simple  (для тестов)
+#              perl-test-file-sharedir (для тестов)
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+perl Makefile.PL || exit 1
+make             || exit 1
+# make test
+make install DESTDIR="${TMP_DIR}"
+
+# исправим пути (убираем из путей временную директорию установки пакета)
+PERL_MAJ_VER="$(perl -v | /bin/grep version | cut -d \( -f 2 | cut -d v -f 2 | \
+    cut -d . -f 1,2)"
+PERL_MAIN_VER="$(echo "${PERL_MAJ_VER}" | cut -d . -f 1)"
+PERL_LIB_PATH="/usr/lib/perl${PERL_MAIN_VER}/${PERL_MAJ_VER}"
+sed -e "s|${TMP_DIR}||" -i \
+    "${TMP_DIR}${PERL_LIB_PATH}/site_perl/auto/DateTime/Locale/.packlist"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (localization support for DateTime.pm)
+#
+# DateTime::Locale provides localization support for DateTime
+#
+# Home page: https://metacpan.org/pod/DateTime::Locale
+# Download:  https://cpan.metacpan.org/authors/id/D/DR/DROLSKY/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
