a99f59a2907f62c7c63c8a56a9a57f2e0bbb4f37
Vladimir <mrvladislavovich@gmail.com>
2021-10-18 19:20:53 +0400

    BLFS: perl-datetime package added

---
+++ stage-2-blfs-stable-x86_64/build/d/perl-modules/perl-datetime.sh
@@ -0,0 +1,51 @@
+#! /bin/bash
+
+PRGNAME="perl-datetime"
+ARCH_NAME="DateTime"
+
+### DateTime (a date and time object for Perl)
+# DateTime Perl модуль
+
+# Required:    perl-datetime-locale
+#              perl-datetime-timezone
+# Recommended: perl-cpan-meta-check (для тестов)
+#              perl-test-fatal      (для тестов)
+#              perl-test-warnings   (для тестов)
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+perl Makefile.PL || exit 1
+make             || exit 1
+# make test
+make install DESTDIR="${TMP_DIR}"
+
+# исправим пути (убираем из путей временную директорию установки пакета)
+PERL_MAJ_VER="$(perl -v | /bin/grep version | cut -d \( -f 2 | cut -d v -f 2 | \
+    cut -d . -f 1,2)"
+PERL_MAIN_VER="$(echo "${PERL_MAJ_VER}" | cut -d . -f 1)"
+PERL_LIB_PATH="/usr/lib/perl${PERL_MAIN_VER}/${PERL_MAJ_VER}"
+sed -e "s|${TMP_DIR}||" -i \
+    "${TMP_DIR}${PERL_LIB_PATH}/site_perl/auto/DateTime/.packlist"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (a date and time object for Perl)
+#
+# DateTime is a date and time object for perl
+#
+# Home page: https://metacpan.org/pod/DateTime
+# Download:  https://cpan.metacpan.org/authors/id/D/DR/DROLSKY/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
