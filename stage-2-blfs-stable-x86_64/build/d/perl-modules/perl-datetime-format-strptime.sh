861f7f42275f4a74c1abe8b282369ddbc7a76ca2
Vladimir <mrvladislavovich@gmail.com>
2021-10-21 13:13:00 +0400

    BLFS: perl-datetime-format-strptime package added

---
+++ stage-2-blfs-stable-x86_64/build/d/perl-modules/perl-datetime-format-strptime.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+PRGNAME="perl-datetime-format-strptime"
+ARCH_NAME="DateTime-Format-Strptime"
+
+### DateTime::Format::Strptime (parse and format strp and strf time patterns)
+# DateTime::Format::Strptime Perl модуль
+
+# Required:    perl-datetime
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+perl Makefile.PL || exit 1
+make             || exit 1
+# make test
+make install DESTDIR="${TMP_DIR}"
+
+# исправим пути (убираем из путей временную директорию установки пакета)
+PERL_MAJ_VER="$(perl -v | /bin/grep version | cut -d \( -f 2 | cut -d v -f 2 | \
+    cut -d . -f 1,2)"
+PERL_MAIN_VER="$(echo "${PERL_MAJ_VER}" | cut -d . -f 1)"
+PERL_LIB_PATH="/usr/lib/perl${PERL_MAIN_VER}/${PERL_MAJ_VER}"
+PACKLIST="site_perl/auto/DateTime/Format/Strptime/.packlist"
+sed -e "s|${TMP_DIR}||" -i "${TMP_DIR}${PERL_LIB_PATH}/${PACKLIST}"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (parse and format strp and strf time patterns)
+#
+# DateTime::Format::Strptime implements most of strptime(3), i.e. it takes a
+# string and a pattern and returns a DateTime object
+#
+# Home page: https://metacpan.org/pod/DateTime::Format::Strptime
+# Download:  https://cpan.metacpan.org/authors/id/D/DR/DROLSKY/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
