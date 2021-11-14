36a27c87bef611cdcd4b6aa61332645acaf8d942
Vladimir <mrvladislavovich@gmail.com>
2021-11-14 13:11:41 +0400

    BLFS: perl-encode-jis2k package added

---
+++ stage-2-blfs-stable-x86_64/build/d/perl-modules/perl-encode-jis2k.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+
+PRGNAME="perl-encode-jis2k"
+ARCH_NAME="Encode-JIS2K"
+
+### Encode::JIS2K (JIS X 0212 (aka JIS 2000) Encodings)
+# Encode::JIS2K Perl модуль
+
+# Required:    no
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+perl Makefile.PL || exit 1
+make             || exit 1
+# make test
+make install DESTDIR="${TMP_DIR}"
+
+# исправим пути (убираем из путей временную директорию установки пакета)
+PERL_MAJ_VER="$(perl -v | /bin/grep version | cut -d \( -f 2 | cut -d v -f 2 | \
+    cut -d . -f 1,2)"
+PERL_MAIN_VER="$(echo "${PERL_MAJ_VER}" | cut -d . -f 1)"
+PERL_LIB_PATH="/usr/lib/perl${PERL_MAIN_VER}/${PERL_MAJ_VER}"
+sed -e "s|${TMP_DIR}||" -i \
+    "${TMP_DIR}${PERL_LIB_PATH}/site_perl/auto/Encode/JIS2K/.packlist"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (JIS X 0212 (aka JIS 2000) Encodings)
+#
+# The Encode::JIS2K Perl module provides JIS X 0212 (aka JIS 2000) Encodings
+#
+# Home page: https://metacpan.org/pod/Encode::JIS2K
+# Download:  https://www.cpan.org/authors/id/D/DA/DANKOGAI/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
