7bf83ed00c7a77119f3711afc510a18e5bc8bf54
Vladimir <mrvladislavovich@gmail.com>
2021-11-26 23:02:19 +0400

    BLFS: perl-html-tagset package added

---
+++ stage-2-blfs-stable-x86_64/build/d/perl-modules/perl-html-tagset.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+
+PRGNAME="perl-html-tagset"
+ARCH_NAME="HTML-Tagset"
+
+### HTML::Tagset (data tables useful in parsing HTML)
+# HTML::Tagset Perl модуль
+
+# Required:    no
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+perl Makefile.PL || exit 1
+make             || exit 1
+# make test
+make install DESTDIR="${TMP_DIR}"
+
+# исправим пути (убираем из путей временную директорию установки пакета)
+PERL_MAJ_VER="$(perl -v | /bin/grep version | cut -d \( -f 2 | cut -d v -f 2 | \
+    cut -d . -f 1,2)"
+PERL_MAIN_VER="$(echo "${PERL_MAJ_VER}" | cut -d . -f 1)"
+PERL_LIB_PATH="/usr/lib/perl${PERL_MAIN_VER}/${PERL_MAJ_VER}"
+sed -e "s|${TMP_DIR}||" -i \
+    "${TMP_DIR}${PERL_LIB_PATH}/site_perl/auto/HTML/Tagset/.packlist"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (data tables useful in parsing HTML)
+#
+# HTML::Tagset provides several data tables useful in parsing HTML
+#
+# Home page: https://metacpan.org/pod/HTML::Tagset
+# Download:  https://cpan.metacpan.org/authors/id/P/PE/PETDANCE/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
