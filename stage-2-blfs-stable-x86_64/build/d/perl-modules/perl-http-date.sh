550ed22bb4d7fc928bec213c8d863892202b5bac
Vladimir <mrvladislavovich@gmail.com>
2021-11-30 16:11:43 +0400

    BLFS: perl-http-date package added

---
+++ stage-2-blfs-stable-x86_64/build/d/perl-modules/perl-http-date.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+PRGNAME="perl-http-date"
+ARCH_NAME="HTTP-Date"
+
+### HTTP::Date (date conversion routines)
+# HTTP::Date Perl модуль
+
+# Required:    no
+# Recommended: perl-timedate (для распознавания числовых зон и зон отличных от GMT)
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+perl Makefile.PL || exit 1
+make             || exit 1
+# make test
+make install DESTDIR="${TMP_DIR}"
+
+# исправим пути (убираем из путей временную директорию установки пакета)
+PERL_MAJ_VER="$(perl -v | /bin/grep version | cut -d \( -f 2 | cut -d v -f 2 | \
+    cut -d . -f 1,2)"
+PERL_MAIN_VER="$(echo "${PERL_MAJ_VER}" | cut -d . -f 1)"
+PERL_LIB_PATH="/usr/lib/perl${PERL_MAIN_VER}/${PERL_MAJ_VER}"
+sed -e "s|${TMP_DIR}||" -i \
+    "${TMP_DIR}${PERL_LIB_PATH}/site_perl/auto/HTTP/Date/.packlist"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (date conversion routines)
+#
+# HTTP::Date provides functions to deal with the date formats used by the HTTP
+# protocol and also with some other date formats
+#
+# Home page: https://metacpan.org/pod/HTTP::Date
+# Download:  https://cpan.metacpan.org/authors/id/O/OA/OALDERS/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
