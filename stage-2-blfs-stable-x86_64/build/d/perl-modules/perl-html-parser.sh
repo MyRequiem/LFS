497b163f7382f4ad4e47b451e8de9d30b79210e3
Vladimir <mrvladislavovich@gmail.com>
2021-12-01 23:16:38 +0400

    BLFS: perl-html-parser package added

---
+++ stage-2-blfs-stable-x86_64/build/d/perl-modules/perl-html-parser.sh
@@ -0,0 +1,50 @@
+#! /bin/bash
+
+PRGNAME="perl-html-parser"
+ARCH_NAME="HTML-Parser"
+
+### HTML::Parser (HTML parser class)
+# HTML::Parser Perl модуль
+
+# Required:    perl-html-tagset
+#              perl-http-message
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+perl Makefile.PL || exit 1
+make             || exit 1
+# make test
+make install DESTDIR="${TMP_DIR}"
+
+# исправим пути (убираем из путей временную директорию установки пакета)
+PERL_MAJ_VER="$(perl -v | /bin/grep version | cut -d \( -f 2 | cut -d v -f 2 | \
+    cut -d . -f 1,2)"
+PERL_MAIN_VER="$(echo "${PERL_MAJ_VER}" | cut -d . -f 1)"
+PERL_LIB_PATH="/usr/lib/perl${PERL_MAIN_VER}/${PERL_MAJ_VER}"
+sed -e "s|${TMP_DIR}||" -i \
+    "${TMP_DIR}${PERL_LIB_PATH}/site_perl/auto/HTML/Parser/.packlist"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (HTML parser class)
+#
+# The HTML::Parser distribution is a collection of modules that parse and
+# extract information from HTML documents
+#
+# Home page: https://metacpan.org/pod/HTML::Parser
+# Download:  https://www.cpan.org/authors/id/C/CA/CAPOEIRAB/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
