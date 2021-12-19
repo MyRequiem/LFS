a195980f647d2f36e9889198677e53eaaecf48d8
Vladimir <mrvladislavovich@gmail.com>
2021-12-19 21:46:42 +0400

    BLFS: perl-lingua-translit package added

---
+++ stage-2-blfs-stable-x86_64/build/d/perl-modules/perl-lingua-translit.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+PRGNAME="perl-lingua-translit"
+ARCH_NAME="Lingua-Translit"
+
+### Lingua::Translit (transliterates text between writing systems)
+# Lingua::Translit Perl модуль
+
+# Required:    no
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+perl Makefile.PL || exit 1
+make             || exit 1
+# make test
+make install DESTDIR="${TMP_DIR}"
+
+# исправим пути (убираем из путей временную директорию установки пакета)
+PERL_MAJ_VER="$(perl -v | /bin/grep version | cut -d \( -f 2 | cut -d v -f 2 | \
+    cut -d . -f 1,2)"
+PERL_MAIN_VER="$(echo "${PERL_MAJ_VER}" | cut -d . -f 1)"
+PERL_LIB_PATH="/usr/lib/perl${PERL_MAIN_VER}/${PERL_MAJ_VER}"
+sed -e "s|${TMP_DIR}||" -i \
+    "${TMP_DIR}${PERL_LIB_PATH}/site_perl/auto/Lingua/Translit/.packlist"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (transliterates text between writing systems)
+#
+# Lingua::Translit and its translit program transliterate text between writing
+# systems
+#
+# Home page: https://metacpan.org/pod/Lingua::Translit
+# Download:  https://www.cpan.org/authors/id/A/AL/ALINKE/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
