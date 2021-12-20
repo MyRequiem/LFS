f9dab46d2eccc08e05d834da32c162c9404df3a1
Vladimir <mrvladislavovich@gmail.com>
2021-12-20 10:29:36 +0400

    BLFS: perl-log-log4perl package added

---
+++ stage-2-blfs-stable-x86_64/build/d/perl-modules/perl-log-log4perl.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+
+PRGNAME="perl-log-log4perl"
+ARCH_NAME="Log-Log4perl"
+
+### Log::Log4perl (Log4j implementation for Perl)
+# Log::Log4perl Perl модуль
+
+# Required:    no
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+perl Makefile.PL || exit 1
+make             || exit 1
+# make test
+make install DESTDIR="${TMP_DIR}"
+
+# исправим пути (убираем из путей временную директорию установки пакета)
+PERL_MAJ_VER="$(perl -v | /bin/grep version | cut -d \( -f 2 | cut -d v -f 2 | \
+    cut -d . -f 1,2)"
+PERL_MAIN_VER="$(echo "${PERL_MAJ_VER}" | cut -d . -f 1)"
+PERL_LIB_PATH="/usr/lib/perl${PERL_MAIN_VER}/${PERL_MAJ_VER}"
+sed -e "s|${TMP_DIR}||" -i \
+    "${TMP_DIR}${PERL_LIB_PATH}/site_perl/auto/Log/Log4perl/.packlist"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (Log4j implementation for Perl)
+#
+# Log::Log4perl provides a Log4j implementation for perl
+#
+# Home page: https://metacpan.org/pod/Log::Log4perl
+# Download:  https://www.cpan.org/authors/id/E/ET/ETJ/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
