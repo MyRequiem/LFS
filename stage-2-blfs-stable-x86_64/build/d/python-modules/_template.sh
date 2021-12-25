92f3b844b7d19401af0dde0d4b153de4b63da1b8
Vladimir <mrvladislavovich@gmail.com>
2021-12-25 13:52:07 +0400

    BLFS: build/d/{perl-modules,python-modules}/_template.sh added

---
+++ stage-2-blfs-stable-x86_64/build/d/python-modules/_template.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+
+PRGNAME=""
+ARCH_NAME=""
+
+###  ()
+
+# Required:    no
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+python2 setup.py build || exit 1
+python2 setup.py install --optimize=1 --root="${TMP_DIR}"
+
+python3 setup.py build || exit 1
+python3 setup.py install --optimize=1 --root="${TMP_DIR}"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} ()
+#
+#
+# Home page:
+# Download:
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
