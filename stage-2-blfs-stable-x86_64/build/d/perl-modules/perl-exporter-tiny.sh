37c69e218909de440f50340941b71c6dbeb0b031
Vladimir <mrvladislavovich@gmail.com>
2021-12-27 23:52:23 +0400

    BLFS: perl-exporter-tiny package added

---
+++ stage-2-blfs-stable-x86_64/build/d/perl-modules/perl-exporter-tiny.sh
@@ -0,0 +1,50 @@
+#! /bin/bash
+
+PRGNAME="perl-exporter-tiny"
+ARCH_NAME="Exporter-Tiny"
+
+### Exporter::Tiny (an exporter with the features of Sub::Exporter but only core dependencies)
+# Exporter::Tiny Perl модуль
+
+# Required:    no
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+# стандартная установка
+perl Makefile.PL || exit 1
+make             || exit 1
+# make test
+make install DESTDIR="${TMP_DIR}"
+
+# исправим пути (убираем из путей временную директорию установки пакета)
+PERL_MAJ_VER="$(perl -v | /bin/grep version | cut -d \( -f 2 | cut -d v -f 2 | \
+    cut -d . -f 1,2)"
+PERL_MAIN_VER="$(echo "${PERL_MAJ_VER}" | cut -d . -f 1)"
+PERL_LIB_PATH="/usr/lib/perl${PERL_MAIN_VER}/${PERL_MAJ_VER}"
+sed -e "s|${TMP_DIR}||" -i \
+    "${TMP_DIR}${PERL_LIB_PATH}/site_perl/auto/Exporter/Tiny/.packlist"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (an exporter with the features of Sub::Exporter but only core dependencies)
+#
+# Exporter::Tiny is an exporter with the features of Sub::Exporter but only
+# core dependencies
+#
+# Home page: https://metacpan.org/pod/Exporter::Tiny
+# Download:  https://cpan.metacpan.org/authors/id/T/TO/TOBYINK/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
