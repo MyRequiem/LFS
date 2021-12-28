17dd12fd13bb75b55a78ee22f55dcf709d730eca
Vladimir <mrvladislavovich@gmail.com>
2021-12-28 14:19:37 +0400

    BLFS: perl-http-cookies package added

---
+++ stage-2-blfs-stable-x86_64/build/d/perl-modules/perl-http-cookies.sh
@@ -0,0 +1,51 @@
+#! /bin/bash
+
+PRGNAME="perl-http-cookies"
+ARCH_NAME="HTTP-Cookies"
+
+### HTTP::Cookies (HTTP cookie jars)
+# HTTP::Cookies Perl модуль
+
+# Required:    perl-http-message
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+# стандартная установка
+perl Makefile.PL || exit 1
+make             || exit 1
+# make test
+make install DESTDIR="${TMP_DIR}"
+
+# исправим пути (убираем из путей временную директорию установки пакета)
+PERL_MAJ_VER="$(perl -v | /bin/grep version | cut -d \( -f 2 | cut -d v -f 2 | \
+    cut -d . -f 1,2)"
+PERL_MAIN_VER="$(echo "${PERL_MAJ_VER}" | cut -d . -f 1)"
+PERL_LIB_PATH="/usr/lib/perl${PERL_MAIN_VER}/${PERL_MAJ_VER}"
+sed -e "s|${TMP_DIR}||" -i \
+    "${TMP_DIR}${PERL_LIB_PATH}/site_perl/auto/HTTP/Cookies/.packlist"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (HTTP cookie jars)
+#
+# HTTP::Cookies provides a class for objects that represent a "cookie jar" -
+# that is, a database of all the HTTP cookies that a given LWP::UserAgent
+# (from perl-libwww-perl package) object knows about
+#
+# Home page: https://metacpan.org/pod/HTTP::Cookies
+# Download:  https://cpan.metacpan.org/authors/id/O/OA/OALDERS/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
