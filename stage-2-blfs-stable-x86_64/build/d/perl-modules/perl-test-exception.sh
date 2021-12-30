a78641f901880d9985e2a058ccbe612733029792
Vladimir <mrvladislavovich@gmail.com>
2021-12-30 21:52:47 +0400

    BLFS: perl-test-exception package added

---
+++ stage-2-blfs-stable-x86_64/build/d/perl-modules/perl-test-exception.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+PRGNAME="perl-test-exception"
+ARCH_NAME="Test-Exception"
+
+### Test::Exception (test exception-based code)
+# Test::Exception Perl модуль
+
+# Required:    perl-sub-uplevel
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+# стандартная установка
+perl Makefile.PL || exit 1
+make             || exit 1
+# make test
+make install DESTDIR="${TMP_DIR}"
+
+# исправим пути (убираем из путей временную директорию установки пакета)
+PERL_MAJ_VER="$(perl -v | /bin/grep version | cut -d \( -f 2 | cut -d v -f 2 | \
+    cut -d . -f 1,2)"
+PERL_MAIN_VER="$(echo "${PERL_MAJ_VER}" | cut -d . -f 1)"
+PERL_LIB_PATH="/usr/lib/perl${PERL_MAIN_VER}/${PERL_MAJ_VER}"
+sed -e "s|${TMP_DIR}||" -i \
+    "${TMP_DIR}${PERL_LIB_PATH}/site_perl/auto/Test/Exception/.packlist"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (test exception-based code)
+#
+# Test::Exception provides convenience methods for testing exception based code
+#
+# Home page: https://metacpan.org/pod/Test::Exception
+# Download:  https://cpan.metacpan.org/authors/id/E/EX/EXODIST/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
