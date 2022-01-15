58d5211ed63be50daf0c6472de7a1bd2787ac0e0
Vladimir <mrvladislavovich@gmail.com>
2022-01-15 12:07:05 +0400

    BLFS: perl-text-diff package added

---
+++ stage-2-blfs-stable-x86_64/build/d/perl-modules/perl-text-diff.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+PRGNAME="perl-text-diff"
+ARCH_NAME="Text-Diff"
+
+### Text::Diff (perform diffs on files and record sets)
+# Text::Diff Perl модуль
+
+# Required:    perl-algorithm-diff
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+# стандартная установка
+perl Makefile.PL || exit 1
+make             || exit 1
+# make test
+make install DESTDIR="${TMP_DIR}"
+
+# исправим пути (убираем из путей временную директорию установки пакета)
+PERL_MAJ_VER="$(perl -v | /bin/grep version | cut -d \( -f 2 | cut -d v -f 2 | \
+    cut -d . -f 1,2)"
+PERL_MAIN_VER="$(echo "${PERL_MAJ_VER}" | cut -d . -f 1)"
+PERL_LIB_PATH="/usr/lib/perl${PERL_MAIN_VER}/${PERL_MAJ_VER}"
+sed -e "s|${TMP_DIR}||" -i \
+    "${TMP_DIR}${PERL_LIB_PATH}/site_perl/auto/Text/Diff/.packlist"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (perform diffs on files and record sets)
+#
+# Text::Diff performs diffs on files and record sets
+#
+# Home page: https://metacpan.org/pod/Text::Diff
+# Download:  https://cpan.metacpan.org/authors/id/N/NE/NEILB/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
