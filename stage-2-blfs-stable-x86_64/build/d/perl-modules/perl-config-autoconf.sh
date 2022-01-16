88b49fd284fc08ffb3dede3816cfbd0dfedd3229
Vladimir <mrvladislavovich@gmail.com>
2022-01-16 19:01:55 +0400

    BLFS: perl-config-autoconf package added

---
+++ stage-2-blfs-stable-x86_64/build/d/perl-modules/perl-config-autoconf.sh
@@ -0,0 +1,51 @@
+#! /bin/bash
+
+PRGNAME="perl-config-autoconf"
+ARCH_NAME="Config-AutoConf"
+
+### Config::AutoConf (a module to implement some of autoconf macros in pure perl)
+# Config::AutoConf Perl модуль
+
+# Required:    perl-capture-tiny
+#              perl-file-slurper
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+# стандартная установка
+perl Makefile.PL || exit 1
+make             || exit 1
+# make test
+make install DESTDIR="${TMP_DIR}"
+
+# исправим пути (убираем из путей временную директорию установки пакета)
+PERL_MAJ_VER="$(perl -v | /bin/grep version | cut -d \( -f 2 | cut -d v -f 2 | \
+    cut -d . -f 1,2)"
+PERL_MAIN_VER="$(echo "${PERL_MAJ_VER}" | cut -d . -f 1)"
+PERL_LIB_PATH="/usr/lib/perl${PERL_MAIN_VER}/${PERL_MAJ_VER}"
+sed -e "s|${TMP_DIR}||" -i \
+    "${TMP_DIR}${PERL_LIB_PATH}/site_perl/auto/Config/AutoConf/.packlist"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (a module to implement some of autoconf macros in pure perl)
+#
+# The Config::AutoConf module implements some of the AutoConf macros (detecting
+# a command, detecting a library, etc.) in pure perl
+#
+# Home page: https://metacpan.org/pod/Config::AutoConf
+# Download:  https://cpan.metacpan.org/authors/id/R/RE/REHSACK/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
