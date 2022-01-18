a87adc49b3873978833330a3be47130e2270185d
Vladimir <mrvladislavovich@gmail.com>
2022-01-18 16:26:22 +0400

    BLFS: perl-alien-build package added

---
+++ stage-2-blfs-stable-x86_64/build/d/perl-modules/perl-alien-build.sh
@@ -0,0 +1,53 @@
+#! /bin/bash
+
+PRGNAME="perl-alien-build"
+ARCH_NAME="Alien-Build"
+
+### Alien::Build (build external dependencies for use in CPAN)
+# Alien::Build Perl модуль
+
+# Required:    perl-capture-tiny
+#              perl-file-which
+#              perl-ffi-checklib
+#              perl-file-chdir
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+# стандартная установка
+perl Makefile.PL || exit 1
+make             || exit 1
+# make test
+make install DESTDIR="${TMP_DIR}"
+
+# исправим пути (убираем из путей временную директорию установки пакета)
+PERL_MAJ_VER="$(perl -v | /bin/grep version | cut -d \( -f 2 | cut -d v -f 2 | \
+    cut -d . -f 1,2)"
+PERL_MAIN_VER="$(echo "${PERL_MAJ_VER}" | cut -d . -f 1)"
+PERL_LIB_PATH="/usr/lib/perl${PERL_MAIN_VER}/${PERL_MAJ_VER}"
+sed -e "s|${TMP_DIR}||" -i \
+    "${TMP_DIR}${PERL_LIB_PATH}/site_perl/auto/Alien/Build/.packlist"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (build external dependencies for use in CPAN)
+#
+# Alien::Build provides tools for building external (non-CPAN) dependencies for
+# CPAN
+#
+# Home page: https://metacpan.org/pod/Alien::Build
+# Download:  https://cpan.metacpan.org/authors/id/P/PL/PLICEASE/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
