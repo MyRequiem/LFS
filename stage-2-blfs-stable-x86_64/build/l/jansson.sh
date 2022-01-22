1fd70e00b944b354ccf0fc33c7604d0492bfaec8
Vladimir <mrvladislavovich@gmail.com>
2022-01-22 22:45:26 +0400

    BLFS: jansson package added

---
+++ stage-2-blfs-stable-x86_64/build/l/jansson.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+
+PRGNAME="jansson"
+
+### jansson (library for encoding, decoding and manipulating json data)
+# Библиотека, используемая для кодирования, декодирования и управления
+# JSON-данными
+
+# Required:    no
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                  || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+./configure       \
+    --prefix=/usr \
+    --disable-static || exit 1
+
+make || exit 1
+# make check
+make install DESTDIR="${TMP_DIR}"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (library for encoding, decoding and manipulating json data)
+#
+# The Jansson package contains a library used to encode, decode, and manipulate
+# JSON data
+#
+# Home page: https://github.com/akheron/${PRGNAME}
+# Download:  http://www.digip.org/${PRGNAME}/releases/${PRGNAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
+
+echo -e "\n---------------\nRemoving *.la files..."
+remove-la-files.sh
+echo "---------------"
