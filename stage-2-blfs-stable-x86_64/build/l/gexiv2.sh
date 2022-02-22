c2cf476398170504f28aca8bc749517a8e5e9536
Vladimir <mrvladislavovich@gmail.com>
2022-02-22 19:28:24 +0400

    BLFS: gexiv2 package added

---
+++ stage-2-blfs-stable-x86_64/build/l/gexiv2.sh
@@ -0,0 +1,47 @@
+#! /bin/bash
+
+PRGNAME="gexiv2"
+
+### gexiv2 (GObject wrapper around Exiv2 library)
+# GObject обертка для библиотеки Exiv2
+
+# Required:    exiv2
+# Recommended: vala
+# Optional:    gtk-doc (для создания документации)
+#              python2 (для создания Python модуля)
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                  || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+mkdir build
+cd build || exit 1
+
+meson             \
+    --prefix=/usr \
+    .. || exit 1
+
+ninja || exit 1
+# ninja test
+DESTDIR="${TMP_DIR}" ninja install
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+MAJ_VERSION="$(echo "${VERSION}" | cut -d . -f 1,2)"
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (GObject wrapper around Exiv2 library)
+#
+# gexiv2 is a GObject-based wrapper around the Exiv2 library
+#
+# Home page: https://wiki.gnome.org/${PRGNAME}
+# Download:  https://download.gnome.org/sources/${PRGNAME}/${MAJ_VERSION}/${PRGNAME}-${VERSION}.tar.xz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
