64dd4f9a4632eda0483377f66a07dff86014330c
Vladimir <mrvladislavovich@gmail.com>
2022-03-13 17:40:49 +0400

    BLFS: additional_scripts/stripping.sh updated

--- stage-2-blfs-stable-x86_64/additional_scripts/stripping.sh
+++ stage-2-blfs-stable-x86_64/additional_scripts/stripping.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-BINARY="$(find "${TMP_DIR}" -type f -print0 | xargs -0 file 2>/dev/null | \
-    /bin/grep -e "executable" -e "shared object" | /bin/grep ELF | \
-    /bin/grep -v "32-bit" | cut -f 1 -d :)"
+BINARY="$(find "${TMP_DIR}" -type f -not -path "*/usr/share/qemu/*" -print0 | \
+    xargs -0 file 2>/dev/null | /bin/grep -e "executable" -e "shared object" | \
+    /bin/grep ELF | /bin/grep -v "32-bit" | cut -f 1 -d :)"
 
 for BIN in ${BINARY}; do
     strip --strip-unneeded "${BIN}"
