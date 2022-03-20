888920ef3cc9f90eb4b66fb7c2a8a1541080fbc5
Vladimir <mrvladislavovich@gmail.com>
2022-03-20 17:42:06 +0400

    BLFS: perl-common-sense package added

---
+++ stage-2-blfs-stable-x86_64/build/d/perl-modules/perl-common-sense.sh
@@ -0,0 +1,51 @@
+#! /bin/bash
+
+PRGNAME="perl-common-sense"
+ARCH_NAME="common-sense"
+
+### common::sense (perl common defaults with lower memory usage)
+# Perl модуль
+
+# Required:    no
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+# стандартная установка
+perl Makefile.PL || exit 1
+make             || exit 1
+# make test
+make install DESTDIR="${TMP_DIR}"
+
+# исправим пути (убираем из путей временную директорию установки пакета)
+PERL_MAJ_VER="$(perl -v | /bin/grep version | cut -d \( -f 2 | cut -d v -f 2 | \
+    cut -d . -f 1,2)"
+PERL_MAIN_VER="$(echo "${PERL_MAJ_VER}" | cut -d . -f 1)"
+PERL_LIB_PATH="/usr/lib/perl${PERL_MAIN_VER}/${PERL_MAJ_VER}"
+sed -e "s|${TMP_DIR}||" -i \
+    "${TMP_DIR}${PERL_LIB_PATH}/site_perl/auto/common/sense/.packlist"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (perl common defaults with lower memory usage)
+#
+# common::sense module implements some sane defaults for Perl programs, as
+# defined by two typical, (or not so typical - use your common sense) specimens
+# of Perl coders.
+#
+# Home page: https://metacpan.org/pod/common::sense
+# Download:  https://cpan.metacpan.org/authors/id/M/ML/MLEHMANN/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
