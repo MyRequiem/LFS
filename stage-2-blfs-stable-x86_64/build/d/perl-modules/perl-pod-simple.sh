a171a051d842fde4802099c87f424ca7b4daf79d
Vladimir <mrvladislavovich@gmail.com>
2022-03-20 13:00:45 +0400

    BLFS: perl-pod-simple package added

---
+++ stage-2-blfs-stable-x86_64/build/d/perl-modules/perl-pod-simple.sh
@@ -0,0 +1,51 @@
+#! /bin/bash
+
+PRGNAME="perl-pod-simple"
+ARCH_NAME="Pod-Simple"
+
+### Pod::Simple (framework for parsing Pod)
+# Perl модуль
+
+# Required:    no
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+perl Makefile.PL || exit 1
+make             || exit 1
+# make test
+make install DESTDIR="${TMP_DIR}"
+
+# исправим пути (убираем из путей временную директорию установки пакета)
+PERL_MAJ_VER="$(perl -v | /bin/grep version | cut -d \( -f 2 | cut -d v -f 2 | \
+    cut -d . -f 1,2)"
+PERL_MAIN_VER="$(echo "${PERL_MAJ_VER}" | cut -d . -f 1)"
+PERL_LIB_PATH="/usr/lib/perl${PERL_MAIN_VER}/${PERL_MAJ_VER}"
+sed -e "s|${TMP_DIR}||" -i \
+    "${TMP_DIR}${PERL_LIB_PATH}/site_perl/auto/Pod/Simple/.packlist"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (framework for parsing Pod)
+#
+# Pod::Simple is a Perl library for parsing text in the Pod ("plain old
+# documentation") markup language that is typically used for writing
+# documentation for Perl and for Perl modules. The Pod format is explained in
+# perlpod; the most common formatter is called perldoc
+#
+# Home page: https://metacpan.org/dist/${ARCH_NAME}
+# Download:  https://cpan.metacpan.org/authors/id/K/KH/KHW/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
