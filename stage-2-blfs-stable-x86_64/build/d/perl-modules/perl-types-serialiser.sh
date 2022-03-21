b4c0498e2f6b5ee52f86adcd6ebc46bd2fffd1c2
Vladimir <mrvladislavovich@gmail.com>
2022-03-21 11:58:04 +0400

    BLFS: perl-types-serialiser package added

--- stage-2-blfs-stable-x86_64/build/d/perl-modules/perl-types-serialiser.sh
+++ stage-2-blfs-stable-x86_64/build/d/perl-modules/perl-types-serialiser.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-PRGNAME=""
-ARCH_NAME=""
+PRGNAME="perl-types-serialiser"
+ARCH_NAME="Types-Serialiser"
 
-###  ()
+### Types::Serialiser (simple data types for serialisation formats)
 # Perl модуль
 
-# Required:    no
+# Required:    perl-common-sense
 # Recommended: no
 # Optional:    no
 
@@ -23,32 +23,28 @@ make             || exit 1
 # make test
 make install DESTDIR="${TMP_DIR}"
 
-# установка с помощью модуля Build (пакет perl-module-build)
-perl Build.PL || exit 1
-./Build       || exit 1
-# ./Build test
-./Build install destdir="${TMP_DIR}"
-
-exit
-
 # исправим пути (убираем из путей временную директорию установки пакета)
 PERL_MAJ_VER="$(perl -v | /bin/grep version | cut -d \( -f 2 | cut -d v -f 2 | \
     cut -d . -f 1,2)"
 PERL_MAIN_VER="$(echo "${PERL_MAJ_VER}" | cut -d . -f 1)"
 PERL_LIB_PATH="/usr/lib/perl${PERL_MAIN_VER}/${PERL_MAJ_VER}"
 sed -e "s|${TMP_DIR}||" -i \
-    "${TMP_DIR}${PERL_LIB_PATH}/..."
+    "${TMP_DIR}${PERL_LIB_PATH}/site_perl/auto/Types/Serialiser/.packlist"
 
 source "${ROOT}/stripping.sh"      || exit 1
 source "${ROOT}/update-info-db.sh" || exit 1
 /bin/cp -vpR "${TMP_DIR}"/* /
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
-# Package: ${PRGNAME} ()
+# Package: ${PRGNAME} (simple data types for serialisation formats)
 #
+# This module provides some extra datatypes that are used by common
+# serialisation formats such as JSON or CBOR. The idea is to have a shared
+# repository of simple/small constants and containers that can be shared by
+# different implementations so they become interoperable between each other.
 #
-# Home page:
-# Download:
+# Home page: https://metacpan.org/pod/Types::Serialiser
+# Download:  https://cpan.metacpan.org/authors/id/M/ML/MLEHMANN/${ARCH_NAME}-${VERSION}.tar.gz
 #
 EOF
 
