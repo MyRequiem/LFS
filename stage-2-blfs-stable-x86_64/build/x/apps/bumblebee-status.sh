456d71dfe0817c28cd56be8e2397ed20e73f1c4f
Vladimir <mrvladislavovich@gmail.com>
2022-05-31 13:06:53 +0400

    BLFS: bumblebee-status package added

---
+++ stage-2-blfs-stable-x86_64/build/x/apps/bumblebee-status.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+PRGNAME="bumblebee-status"
+
+### bumblebee-status (status line generator for i3wm)
+# Модульный генератор строки состояния для оконного менеджера i3
+
+# Required:    python3
+#              i3
+# Recommended: python-psutil    (для модулей cpu и cpu2)
+#              python-netifaces (для модуля nic)
+#              hddtemp          (для модуля hddtemp)
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                  || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+python3 setup.py build || exit 1
+python3 setup.py install --optimize=1 --root="${TMP_DIR}"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (status line generator for i3wm)
+#
+# bumblebee-status is a modular, theme-able status line generator for the i3
+# window manager
+#
+# Home page: https://github.com/tobi-wan-kenobi/${PRGNAME}/
+# Download:  https://github.com/tobi-wan-kenobi/${PRGNAME}/archive/v${VERSION}/${PRGNAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
