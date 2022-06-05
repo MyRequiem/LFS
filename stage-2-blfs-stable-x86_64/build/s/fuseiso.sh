744300bf5447422e43b1a3e9fa63aa94bf0a1e9e
Vladimir <mrvladislavovich@gmail.com>
2022-06-05 23:48:40 +0400

    BLFS: fuseiso package added

---
+++ stage-2-blfs-stable-x86_64/build/s/fuseiso.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+PRGNAME="fuseiso"
+
+### fuseiso (FUSE module to mount ISO filesystem images)
+# Модуль FUSE для монтирования образов .iso, .nrg, .bin, .mdf и .img
+
+# Required:    no
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                  || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+./configure           \
+    --prefix=/usr     \
+    --disable-static  \
+    --sysconfdir=/etc \
+    --localstatedir=/var/lib || exit 1
+
+make || exit 1
+make install DESTDIR="${TMP_DIR}"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (FUSE module to mount ISO filesystem images)
+#
+# FuseISO is a FUSE module to mount ISO filesystem images (.iso, .nrg, .bin,
+# .mdf and .img files). It currently support plain ISO9660 Level 1 and 2, Rock
+# Ridge, Joliet, and zisofs.
+#
+# Home page: https://sourceforge.net/projects/${PRGNAME}/
+# Download:  http://downloads.sourceforge.net/${PRGNAME}/${PRGNAME}-${VERSION}.tar.bz2
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
