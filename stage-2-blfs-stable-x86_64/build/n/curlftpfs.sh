a9ecb13ad6c95b6b8158b609e1ec36ba090935ca
Vladimir <mrvladislavovich@gmail.com>
2022-06-05 23:27:13 +0400

    BLFS: curlftpfs package added

---
+++ stage-2-blfs-stable-x86_64/build/n/curlftpfs.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+PRGNAME="curlftpfs"
+
+### CurlFtpFS (Mount FTP/SFTP via fuse)
+# Файловая система для доступа к ftp-хостам, основанная на FUSE и libcurl
+
+# Required:    curl
+#              fuse
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                  || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+./configure           \
+    --prefix=/usr     \
+    --disable-static  \
+    --sysconfdir=/etc \
+    --localstatedir=/var || exit 1
+
+make || exit 1
+make install DESTDIR="${TMP_DIR}"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (Mount FTP/SFTP via fuse)
+#
+# CurlFtpFS is a filesystem for acessing ftp hosts based on FUSE and libcurl.
+# It automatically reconnects if the server times out.
+#
+# Home page: http://${PRGNAME}.sourceforge.net/
+# Download:  http://downloads.sourceforge.net/${PRGNAME}/${PRGNAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
