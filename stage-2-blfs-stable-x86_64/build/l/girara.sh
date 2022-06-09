8c10ef16835ac7de2b45fb63691faf1c982109f6
Vladimir <mrvladislavovich@gmail.com>
2022-06-09 09:27:00 +0400

    BLFS: girara package added

---
+++ stage-2-blfs-stable-x86_64/build/l/girara.sh
@@ -0,0 +1,64 @@
+#! /bin/bash
+
+PRGNAME="girara"
+
+### girara (GTK+ based GUI for text-oriented applications)
+# Библиотека, реализующая графический пользовательский интерфейс на основе GTK+
+# для приложений, работающих с текстом (zathura, jumanji, etc). Ориентирована
+# на простоту и минимализм.
+
+# Required:    glib
+#              gtk+3
+# Recommended: no
+# Optional:    libnotify
+#              json-c
+#              doxygen
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                  || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+LANG=C
+LC_ALL=C
+export LANG LC_ALL
+
+mkdir build
+cd build || exit 1
+
+DOCS="disabled"
+NOTIFY="disabled"
+TESTS="disabled"
+# [ -x /usr/lib/libnotify.so ] && NOTIFY="enabled"
+
+meson                    \
+    --prefix=/usr        \
+    -Ddocs="${DOCS}"     \
+    -Dtests="${TESTS}"   \
+    -Dnotify="${NOTIFY}" \
+    .. || exit 1
+
+ninja || exit 1
+DESTDIR="${TMP_DIR}" ninja install
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (GTK+ based GUI for text-oriented applications)
+#
+# girara is a library that implements a user interface that focuses on
+# simplicity and minimalism. girara was designed to replace and enhance the
+# user interface that is used by zathura and jumanji and other features that
+# those applications share.
+#
+# Home page: https://github.com/pwmt/${PRGNAME}
+# Download:  https://github.com/pwmt/${PRGNAME}/archive/${VERSION}/${PRGNAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
