5876f1f34edbb5588cffaf71e3ca371da3d1f985
Vladimir <mrvladislavovich@gmail.com>
2022-06-30 22:00:15 +0400

    BLFS: slop and maim packages added

---
+++ stage-2-blfs-stable-x86_64/build/ap/maim.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+PRGNAME="maim"
+
+### maim (make image)
+# Утилита для создания скриншотов
+
+# Required:    imlib2
+#              slop
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                  || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+cmake \
+    -DCMAKE_INSTALL_PREFIX="/usr" ./
+
+make || exit 1
+make install DESTDIR="${TMP_DIR}"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (make image)
+#
+# maim (make image) is an utility that takes a screenshot of your desktop, and
+# encodes a png, jpg, bmp or webp image of it. By default it outputs the
+# encoded image data directly to standard output. It's meant to overcome
+# shortcomings of scrot and performs better in several ways.
+#
+# Home page: https://github.com/naelstrof/${PRGNAME}
+# Download:  https://github.com/naelstrof/${PRGNAME}/archive/v${VERSION}/${PRGNAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
---
+++ stage-2-blfs-stable-x86_64/build/ap/slop.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+
+PRGNAME="slop"
+
+### slop (selection query)
+# Утилита, запрашивающая выбор прямоугольной области экрана мышью
+
+# Required:    glm
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                  || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+cmake ./ || exit 1
+make     || exit 1
+make install DESTDIR="${TMP_DIR}"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+MAJ_VERSION="$(echo "${VERSION}" | cut -d . -f 1,2)"
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (selection query)
+#
+# slop (Select Operation) is an application that queries for a selection from
+# the user and prints the region to stdout. It grabs the mouse and turns it
+# into a crosshair, lets the user click and drag to make a selection (or click
+# on a window) while drawing a pretty box around it, then finally prints the
+# selection's dimensions to stdout.
+#
+# Home page: https://github.com/naelstrof/${PRGNAME}
+# Download:  https://github.com/naelstrof/${PRGNAME}/archive/v${MAJ_VERSION}/${PRGNAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
