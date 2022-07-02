16806e7f784e79e948670bbfc2b4d5d3dcc02c37
Vladimir <mrvladislavovich@gmail.com>
2022-07-02 17:54:13 +0400

    BLFS: python-pluggy package added

---
+++ stage-2-blfs-stable-x86_64/build/d/python-modules/python-pluggy.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+
+PRGNAME="python-pluggy"
+ARCH_NAME="pluggy"
+
+### pluggy (mechanisms for python)
+# Менеджер плагинов, используемый pytest, но лишенный специфичных для pytest
+# деталей.
+
+# Required:    python-setuptools-scm
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+python2 setup.py build || exit 1
+python2 setup.py install --optimize=1 --root="${TMP_DIR}"
+
+python3 setup.py build || exit 1
+python3 setup.py install --optimize=1 --root="${TMP_DIR}"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (mechanisms for python)
+#
+# This is the plugin manager as used by pytest but stripped of pytest specific
+# details.
+#
+# Home page: https://pypi.org/project/${ARCH_NAME}/
+# Download:  https://files.pythonhosted.org/packages/a1/16/db2d7de3474b6e37cbb9c008965ee63835bba517e22cdb8c35b5116b5ce1/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
