1fbfc0812d0e96e6f3c28653bcd0442b845cea7d
Vladimir <mrvladislavovich@gmail.com>
2022-07-02 16:53:15 +0400

    BLFS: python-attrs package added

---
+++ stage-2-blfs-stable-x86_64/build/d/python-modules/python-attrs.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+
+PRGNAME="python-attrs"
+ARCH_NAME="attrs"
+
+### attrs (attributes without boilerplate)
+# Декораторы классов Python, упрощающие рутинную работу по реализации наиболее
+# распространенных объектных протоколов, связанных с атрибутами.
+
+# Required:    no
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+python2 setup.py build || exit 1
+python2 setup.py install --optimize=1 --root="${TMP_DIR}"
+
+python3 setup.py build || exit 1
+python3 setup.py install --optimize=1 --root="${TMP_DIR}"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (attributes without boilerplate)
+#
+# attrs is an MIT-licensed Python package with class decorators that ease the
+# chores of implementing the most common attribute-related object protocols.
+#
+# Home page: https://pypi.org/project/${ARCH_NAME}/
+# Download:  https://files.pythonhosted.org/packages/d7/77/ebb15fc26d0f815839ecd897b919ed6d85c050feeb83e100e020df9153d2/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
