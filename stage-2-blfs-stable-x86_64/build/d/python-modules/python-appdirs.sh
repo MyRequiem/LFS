9bc7ac1983c8474337f184d2b3dbc1a2ed4e89ce
Vladimir <mrvladislavovich@gmail.com>
2022-07-04 11:53:22 +0400

    BLFS: python-appdirs package added

---
+++ stage-2-blfs-stable-x86_64/build/d/python-modules/python-appdirs.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+
+PRGNAME="python-appdirs"
+ARCH_NAME="appdirs"
+
+### appdirs (determining appropriate platform-specific dirs)
+# Python модуль для определения подходящих каталогов для конкретных платформ,
+# например каталога пользовательских данных.
+
+# Required:    no
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+python2 setup.py build || exit 1
+python2 setup.py install --optimize=1 --root="${TMP_DIR}"
+
+python3 setup.py build || exit 1
+python3 setup.py install --optimize=1 --root="${TMP_DIR}"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (determining appropriate platform-specific dirs)
+#
+# A small Python module for determining appropriate platform-specific dirs,
+# e.g. a "user data dir"
+#
+# Home page: https://pypi.org/project/${ARCH_NAME}/
+# Download:  https://files.pythonhosted.org/packages/d7/d8/05696357e0311f5b5c316d7b95f46c669dd9c15aaeecbb48c7d0aeb88c40/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
