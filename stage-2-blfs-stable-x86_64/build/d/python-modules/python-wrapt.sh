ec3de56259da683bf126794d6d138e8fd301063e
Vladimir <mrvladislavovich@gmail.com>
2022-07-04 10:18:44 +0400

    BLFS: python-wrapt package added

---
+++ stage-2-blfs-stable-x86_64/build/d/python-modules/python-wrapt.sh
@@ -0,0 +1,46 @@
+#! /bin/bash
+
+PRGNAME="python-wrapt"
+ARCH_NAME="wrapt"
+
+### wrapt (A Python module for decorators)
+# Модуль Python для декораторов, оберток и исправлений monkey. Предоставляет
+# прозрачный объектный прокси для Python, который может использоваться в
+# качестве основы для построения обёрток и декораторов функций.
+
+# Required:    no
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+python2 setup.py build || exit 1
+python2 setup.py install --optimize=1 --root="${TMP_DIR}"
+
+python3 setup.py build || exit 1
+python3 setup.py install --optimize=1 --root="${TMP_DIR}"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (A Python module for decorators)
+#
+# A Python module for decorators, wrappers and monkey patching. The aim of the
+# wrapt module is to provide a transparent object proxy for Python, which can
+# be used as the basis for the construction of function wrappers and decorator
+# functions.
+#
+# Home page: https://pypi.org/project/${ARCH_NAME}/
+# Download:  https://files.pythonhosted.org/packages/11/eb/e06e77394d6cf09977d92bff310cb0392930c08a338f99af6066a5a98f92/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
