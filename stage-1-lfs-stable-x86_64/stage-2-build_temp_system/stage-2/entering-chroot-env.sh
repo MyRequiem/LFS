15df5742f1bf802d941a91c1e9c2a31b20a13570
Vladimir <mrvladislavovich@gmail.com>
2023-03-10 20:10:44 +0400

    LFS:  stage-2-build_temp_system/stage-2/entering-chroot-env.sh updated

--- stage-1-lfs-stable-x86_64/stage-2-build_temp_system/stage-2/entering-chroot-env.sh
+++ stage-1-lfs-stable-x86_64/stage-2-build_temp_system/stage-2/entering-chroot-env.sh
@@ -23,7 +23,7 @@ RESETCOLOR="\[\033[0;0m\]"
 chroot "${LFS}" /usr/bin/env -i                                           \
     HOME="/root"                                                          \
     TERM="${TERM}"                                                        \
-    PATH=/bin:/usr/bin:/sbin:/usr/sbin                                    \
+    PATH=/usr/bin:/usr/sbin                                               \
     PS1="\u ${MAGENTA}[LFS chroot]${RESETCOLOR}:${RED}\w\$${RESETCOLOR} " \
     /bin/bash --login +h
 
