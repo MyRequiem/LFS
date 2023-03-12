570321c2ee857e0df06fb8dd0d83762c1ec0ad13
Vladimir <mrvladislavovich@gmail.com>
2023-03-12 21:50:17 +0400

    LFS:  stage-3-build_basic_system/shadow.sh updated

--- stage-1-lfs-stable-x86_64/stage-3-build_basic_system/shadow.sh
+++ stage-1-lfs-stable-x86_64/stage-3-build_basic_system/shadow.sh
@@ -8,11 +8,11 @@ PRGNAME="shadow"
 ROOT="/"
 source "${ROOT}check_environment.sh"                  || exit 1
 source "${ROOT}unpack_source_archive.sh" "${PRGNAME}" || exit 1
-source "${ROOT}config_file_processing.sh"             || exit 1
 
 TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
 rm -rf "${TMP_DIR}"
-mkdir -pv "${TMP_DIR}"
+ETC_DEFAULT="/etc/default"
+mkdir -pv "${TMP_DIR}${ETC_DEFAULT}"
 
 # отключим установку программы 'groups' и ее man-страниц, так как пакет
 # Coreutils предоставляет лучшую версию этой утилиты. Также отменим установку
@@ -29,7 +29,10 @@ find man -name Makefile.in -exec sed -i 's/passwd\.5 / /'   {} \;
 # ящиков, которые Shadow использует по умолчанию, на /var/mail, используемое в
 # LFS
 sed -e 's:#ENCRYPT_METHOD.*:ENCRYPT_METHOD SHA512:'        \
+    -e 's@#\(SHA_CRYPT_..._ROUNDS 5000\)@\100@'            \
     -e 's:MAIL_DIR.*:MAIL_DIR /var/mail:'                  \
+    -e 's:/var/spool/mail:/var/mail:'                      \
+    -e '/PATH=/{s@/sbin:@@;s@/bin:@@}'                     \
     -e 's:FAIL_DELAY.*:FAIL_DELAY 0:'                      \
     -e 's:FAILLOG_ENAB.*:FAILLOG_ENAB no:'                 \
     -e 's:LASTLOG_ENAB.*:LASTLOG_ENAB no:'                 \
@@ -51,9 +54,6 @@ sed -e 's:#ENCRYPT_METHOD.*:ENCRYPT_METHOD SHA512:'        \
 # а так же внести изменения в etc/login.defs
 # sed -i 's:DICTPATH.*:DICTPATH\t/lib/cracklib/pw_dict:' etc/login.defs
 
-# сделаем первый номер группы, сгенерированный useradd был 1000, а не 1001
-sed -i 's/1000/999/' etc/useradd
-
 # /usr/bin/passwd должен существовать перед сборкой, потому что его
 # расположение жестко закодировано в некоторых утилитах пакета
 PASSWD="/usr/bin/passwd"
@@ -63,26 +63,30 @@ PASSWD="/usr/bin/passwd"
 #    --with-group-name-max-length=32
 ./configure           \
     --sysconfdir=/etc \
+    --disable-static  \
     --with-group-name-max-length=32 || exit 1
 
 make || make -j1 || exit 1
 # пакет не имеет тестового набора
-make install DESTDIR="${TMP_DIR}"
+make exec_prefix=/usr install DESTDIR="${TMP_DIR}"
+make -C man install-man DESTDIR="${TMP_DIR}"
 
-# конфиг /etc/default/useradd содержит параметр CREATE_MAIL_SPOOL=yes, который
-# заставляет утилиту useradd создать файл почтового ящика для вновь созданного
-# пользователя. Отключим создание почтовых ящиков:
-USERADD="/etc/default/useradd"
-sed -i 's/yes/no/' "${TMP_DIR}${USERADD}"
-
-# бэкапим конфиг /etc/default/useradd если он уже существует
-if [ -f "${USERADD}" ]; then
-    mv "${USERADD}" "${USERADD}.old"
-fi
-
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
 /bin/cp -vR "${TMP_DIR}"/* /
 
-config_file_processing "${USERADD}"
+###
+# Конфигурация
+###
+
+# создадим файл /etc/default/useradd
+mkdir -p "${ETC_DEFAULT}"
+useradd -D --gid 999
+# /etc/default/useradd содержит параметр CREATE_MAIL_SPOOL=yes, который
+# заставляет утилиту useradd создать файл почтового ящика для вновь созданного
+# пользователя. Отключим создание почтовых ящиков:
+sed -i '/MAIL/s/yes/no/' "${ETC_DEFAULT}/useradd" || exit 1
+cp "${ETC_DEFAULT}/useradd" "${TMP_DIR}${ETC_DEFAULT}"
 
 # на утилиту 'passwd' установим suid бит, чтобы любой пользователь мог ее
 # запустить с правами владельца (root). Необходимо, чтобы пользователь сам мог
@@ -93,6 +97,7 @@ chmod 4711 "/usr/bin/passwd"
 pwconv
 # включим теневые групповые пароли
 grpconv
+
 # установим пароль для суперпользователя
 passwd root
 
