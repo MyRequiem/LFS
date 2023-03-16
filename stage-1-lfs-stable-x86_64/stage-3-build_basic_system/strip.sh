c74e99d05e9b85538d4ba3b9a8f7db4d1edda7d3
Vladimir <mrvladislavovich@gmail.com>
2023-03-16 13:04:33 +0400

    LFS:  stage-3-build_basic_system/strip.sh updated

--- stage-1-lfs-stable-x86_64/stage-3-build_basic_system/strip.sh
+++ stage-1-lfs-stable-x86_64/stage-3-build_basic_system/strip.sh
@@ -1,11 +1,20 @@
 #! /bin/bash
 
-DIRS="\
-/usr/bin
-/usr/lib
-/usr/sbin
-"
+ROOT="/"
+source "${ROOT}check_environment.sh" || exit 1
 
-for BINDIR in ${DIRS}; do
-    strip --strip-unneeded "${BINDIR}"/*
+BINARY="$(find /usr/bin /usr/lib /usr/libexec /usr/sbin \
+    -type f -print0 | xargs -0 file 2>/dev/null | \
+    /bin/grep -e "executable" -e "shared object" | /bin/grep "ELF" | \
+    /bin/grep -e "with debug_info" -e "not stripped" | \
+    /bin/grep -v "32-bit" | cut -f 1 -d :)"
+
+for BIN in ${BINARY}; do
+    DESTDIR="$(dirname "${BIN}")"
+    BIN_NAME="$(basename "${BIN}")"
+
+    cp "${BIN}" /tmp/
+    strip --strip-unneeded "/tmp/${BIN_NAME}"
+    install -vm755 "/tmp/${BIN_NAME}" "${DESTDIR}/"
+    rm -f "/tmp/${BIN_NAME}"
 done
