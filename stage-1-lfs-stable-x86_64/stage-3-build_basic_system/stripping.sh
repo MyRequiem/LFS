1aec641009885fd858bc4bb722b2a456b8c72304
Vladimir <mrvladislavovich@gmail.com>
2023-03-16 13:03:28 +0400

    LFS:  stage-3-build_basic_system/stripping.sh updated

--- stage-1-lfs-stable-x86_64/stage-3-build_basic_system/stripping.sh
+++ stage-1-lfs-stable-x86_64/stage-3-build_basic_system/stripping.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-BINARY="$(find "${TMP_DIR}" -type f -not -path "*/usr/share/qemu/*" -print0 | \
-    xargs -0 file 2>/dev/null | /bin/grep -e "executable" -e "shared object" | \
+BINARY="$(find "${TMP_DIR}" -type f -print0 | xargs -0 file 2>/dev/null | \
+    /bin/grep -e "executable" -e "shared object" | \
     /bin/grep ELF | /bin/grep -v "32-bit" | cut -f 1 -d :)"
 
 for BIN in ${BINARY}; do
