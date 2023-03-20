1eda7aba42b7886df9bb22322ba2a3649834a1df
Vladimir <mrvladislavovich@gmail.com>
2023-03-20 23:30:03 +0400

    BLFS: build/n/curl.sh updated

--- stage-2-blfs-stable-x86_64/build/n/curl.sh
+++ stage-2-blfs-stable-x86_64/build/n/curl.sh
@@ -19,6 +19,7 @@ PRGNAME="curl"
 #              nghttp2
 #              openldap
 #              samba
+#              gsasl       (https://www.gnu.org/software/gsasl/)
 #              impacket    (https://www.secureauth.com/labs/open-source-tools/impacket)
 #              libmetalink (https://launchpad.net/libmetalink/)
 #              librtmp     (http://rtmpdump.mplayerhq.hu/)
@@ -35,15 +36,11 @@ source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
 TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
 mkdir -pv "${TMP_DIR}"
 
-# адаптируем тесты для python3
-grep -rl '#!.*python$' | xargs sed -i '1s/python/&3/'
-
 C_ARES="--disable-ares"
 LIBIDN2="--without-libidn2"
 LIBPSL="--without-libpsl"
 LIBSSH2="--without-libssh2"
 NGHTTP2="--without-nghttp2"
-LIBMETALINK="--without-libmetalink"
 GNUTLS="--without-gnutls"
 OPENLDAP="--disable-ldap"
 SAMBA="--disable-smb"
@@ -54,7 +51,6 @@ LIBRTMP="--without-librtmp"
 [ -x /usr/lib/libpsl.so ]         && LIBPSL="--with-libpsl"
 [ -x /usr/lib/libssh2.so ]        && LIBSSH2="--with-libssh2"
 [ -x /usr/lib/libnghttp2.so ]     && NGHTTP2="--with-nghttp2=/usr"
-[ -x /usr/lib/libmetalink.so ]    && LIBMETALINK="--with-libmetalink=/usr"
 command -v gnutls-cli &>/dev/null && GNUTLS="--with-gnutls=/usr"
 command -v ldapadd    &>/dev/null && OPENLDAP="--enable-ldap"
 command -v samba      &>/dev/null && SAMBA="--enable-smb"
@@ -63,13 +59,13 @@ command -v rtmpdump   &>/dev/null && LIBRTMP="--with-librtmp=/usr"
 ./configure                    \
     --prefix=/usr              \
     --disable-static           \
+    --with-openssl             \
     --enable-threaded-resolver \
     "${C_ARES}"                \
     "${LIBIDN2}"               \
     "${LIBPSL}"                \
     "${LIBSSH2}"               \
     "${NGHTTP2}"               \
-    "${LIBMETALINK}"           \
     "${GNUTLS}"                \
     "${OPENLDAP}"              \
     "${SAMBA}"                 \
@@ -93,8 +89,8 @@ cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # user authentication, ftp upload, HTTP post, SSL (https:) connections,
 # cookies, file transfer resume and more.
 #
-# Home page: https://curl.haxx.se/
-# Download:  https://curl.haxx.se/download/${PRGNAME}-${VERSION}.tar.xz
+# Home page: https://${PRGNAME}.se/
+# Download:  https://${PRGNAME}.se/download/${PRGNAME}-${VERSION}.tar.xz
 #
 EOF
 
