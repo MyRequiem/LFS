99d26dbe8cfe69e2535604c500c85c92ff2eb55f
Vladimir <mrvladislavovich@gmail.com>
2023-03-21 13:37:16 +0400

    BLFS: build/n/gnupg.sh updated

--- stage-2-blfs-stable-x86_64/build/n/gnupg.sh
+++ stage-2-blfs-stable-x86_64/build/n/gnupg.sh
@@ -15,10 +15,10 @@ PRGNAME="gnupg"
 #              libgcrypt
 #              libksba
 #              npth
-# Recommended: pinentry
+# Recommended: gnutls
+#              pinentry
 # Optional:    curl
 #              fuse
-#              gnutls
 #              imagemagick  (для создания документации)
 #              libusb
 #              MTA          (dovecot или exim или postfix или sendmail)
@@ -33,56 +33,23 @@ source "${ROOT}/check_environment.sh"                  || exit 1
 source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
 
 TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
-DOCS="/usr/share/doc/${PRGNAME}-${VERSION}"
-mkdir -pv "${TMP_DIR}${DOCS}"
+mkdir -pv "${TMP_DIR}"
 
-# по умолчанию GnuPG не устанавливает устаревший скрипт gpg-zip, но он все еще
-# необходимо для некоторых программ, поэтому включим его установку
-sed -e '/noinst_SCRIPTS = gpg-zip/c sbin_SCRIPTS += gpg-zip' \
-    -i tools/Makefile.in || exit 1
+mkdir build
+cd    build || exit 1
 
 # создаем утилитy g13
 #    --enable-g13
-./configure                  \
-    --prefix=/usr            \
-    --enable-g13             \
-    --localstatedir=/var     \
+../configure             \
+    --prefix=/usr        \
+    --localstatedir=/var \
+    --sysconfdir=/etc    \
+    --enable-g13         \
     --docdir="/usr/share/doc/${PRGNAME}-${VERSION}" || exit 1
 
 make || exit 1
-
-# документация
-# makeinfo --html --no-split -o doc/gnupg_nochunks.html doc/gnupg.texi || exit 1
-makeinfo --plaintext -o doc/gnupg.txt doc/gnupg.texi || exit 1
-
-# html-документация
-# make -C doc html || exit 1
-
-# если пакет texlive установлен, то можно создать документацию в pdf и ps
-# форматах
-PDF_DOCS=""
-# command -v texdoc &>/dev/null && PDF_DOCS="true"
-if [ -n "${PDF_DOCS}" ]; then
-    make -C doc pdf ps || exit 1
-fi
-
 # make check
-
 make install DESTDIR="${TMP_DIR}"
-
-# установка документации
-install -v -m644 doc/gnupg.txt "${TMP_DIR}${DOCS}"
-# install -v -m644 doc/gnupg_nochunks.html  "${TMP_DIR}${DOCS}/html/gnupg.html"
-# install -v -m644 doc/gnupg.html/*         "${TMP_DIR}${DOCS}/html"
-
-mv "${TMP_DIR}${DOCS}/examples/gpgconf.conf" \
-    "${TMP_DIR}${DOCS}/gpgconf.conf_example"
-rm -rf "${TMP_DIR}${DOCS}/examples"
-
-# если создавали документацию в форматах pdf и ps
-if [ -n "${PDF_DOCS}" ]; then
-    install -v -m644 doc/gnupg.{pdf,dvi,ps} "${TMP_DIR}${DOCS}"
-fi
 
 source "${ROOT}/stripping.sh"      || exit 1
 source "${ROOT}/update-info-db.sh" || exit 1
