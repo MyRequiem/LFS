de5fe0c7d50de6fd6a56b72236da8c0fd093ae36
Vladimir <mrvladislavovich@gmail.com>
2023-04-08 20:19:47 +0400

    BLFS: build/n/net-tools.sh updated

--- stage-2-blfs-stable-x86_64/build/n/net-tools.sh
+++ stage-2-blfs-stable-x86_64/build/n/net-tools.sh
@@ -10,18 +10,6 @@ PRGNAME="net-tools"
 # Recommended: no
 # Optional:    no
 
-### NOTE:
-# В ходе конфигурации отвечаем 'y' на следующие вопросы (для минимальной
-# конфигурации на остальные вопросы отвечаем 'n')
-#    Does your system support GNU gettext? (I18N) [n] y
-#    UNIX protocol family (HAVE_AFUNIX) [y] y
-#    INET (TCP/IP) protocol family (HAVE_AFINET) [y] y
-#    INET6 (IPv6) protocol family (HAVE_AFINET6) [y] y
-#    Ethernet (generic) support (HAVE_HWETHER) [y] y
-#    PPP (serial line) support (HAVE_HWPPP) [y] y
-#    IPIP Tunnel support (HAVE_HWTUNNEL) [y] y
-#    IP Masquerading support (HAVE_FW_MASQUERADE) [y] y
-
 ROOT="/root/src/lfs"
 source "${ROOT}/check_environment.sh"                  || exit 1
 source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
@@ -30,19 +18,20 @@ VERSION="$(echo "${VERSION}" | cut -d _ -f 2)"
 TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
 mkdir -pv "${TMP_DIR}"
 
-# применим патч, который отключает создание утилит 'ifconfig' и 'hostname',
-# которые уже установлены в системе с пакетом inetutils (LFS)
-patch --verbose -Np1 -i \
-    "${SOURCES}/${PRGNAME}-CVS_${VERSION}-remove_dups-1.patch" || exit 1
+# сборка и установка пакета должна производится в один поток, все утилиты
+# устанавливаем в /usr/bin/
+export BINDIR='/usr/bin' SBINDIR='/usr/bin' && \
+yes "" |  make -j1 || exit 1
+make -j1 install DESTDIR="${TMP_DIR}"
+unset BINDIR SBINDIR
 
-# исправим ошибку сборки с linux headers v4.8
-sed -i '/#include <netinet\/ip.h>/d' iptunnel.c || exit 1
-
-make config || exit 1
-make || exit 1
-# пакет не имеет набора тестов
-# устанавливаем
-make update DESTDIR="${TMP_DIR}"
+# утилиты ifconfig и hostname, уже были установлены в системе с пакетом
+# inetutils (LFS), поэтому удалим их. Так же удалим утилиты, которые не
+# подходят для нашей системы и лишние man-страницы
+rm -f  "${TMP_DIR}/usr/bin"/{nis,yp}domainname
+rm -f  "${TMP_DIR}/usr/bin"/{hostname,dnsdomainname,domainname,ifconfig}
+rm -f  "${TMP_DIR}/usr/share/man/man8/ifconfig.8"
+rm -rf "${TMP_DIR}/usr/share/man/man1"
 
 source "${ROOT}/stripping.sh"      || exit 1
 source "${ROOT}/update-info-db.sh" || exit 1
@@ -56,7 +45,7 @@ cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # much networking without this package and the network-scripts.
 #
 # Home page: https://sourceforge.net/projects/${PRGNAME}/
-# Download:  http://anduin.linuxfromscratch.org/BLFS/${PRGNAME}/${PRGNAME}-CVS_${VERSION}.tar.gz
+# Download:  https://downloads.sourceforge.net/project/${PRGNAME}/${PRGNAME}-${VERSION}.tar.xz
 #
 EOF
 
