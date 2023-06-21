07fc8a5e8e70e796901c35587ee57084b6318581
Vladimir <mrvladislavovich@gmail.com>
2023-06-21 18:54:47 +0400

    BLFS: d/python-modules/python-pycryptodome package removed, d/python-modules/python3-pycryptodome package added

---
+++ stage-2-blfs-stable-x86_64/build/d/python-modules/python3-pycryptodome.sh
@@ -0,0 +1,84 @@
+#! /bin/bash
+
+PRGNAME="python3-pycryptodome"
+ARCH_NAME="pycryptodome"
+
+### PyCryptodome (Python Cryptography Toolkit)
+# Набор безопасных хэш-функций (таких как SHA256 и RIPEMD160) и различные
+# алгоритмы шифрования (AES, DES, RSA, ElGamal и др.). Является форком и
+# заменой для PyCrypto
+
+# Required:    no
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+##
+# создаем в директории dist дерева исходников пакет
+###
+# команда создает архив для этого пакета
+#    wheel
+# инструктирует pip поместить созданный пакет в указанный каталог dist
+#    --wheel-dir=./dist
+# не устанавливать зависимости для пакета
+#    --no-deps
+# предотвращаем получение файлов из онлайн-репозитория пакетов (PyPI). Если
+# пакеты установлены в правильном порядке, pip вообще не нужно будет извлекать
+# какие-либо файлы
+#    --no-build-isolation
+pip3 wheel               \
+    --wheel-dir=./dist   \
+    --no-deps            \
+    --no-build-isolation \
+    ./ || exit 1
+
+### устанавливаем созданный пакет в "${TMP_DIR}"
+# отключает кеш, чтобы предотвратить предупреждение при установке от
+# пользователя root
+#    --no-cache-dir
+# предотвращает ошибочный запуск команды установки от имени обычного
+# пользователя без полномочий root
+#    --no-user
+PYTHON_MAJ_VER="$(python3 -V | cut -d ' ' -f 2 | cut -d . -f 1,2)"
+TARGET="${TMP_DIR}/usr/lib/python${PYTHON_MAJ_VER}/site-packages"
+pip3 install             \
+    --target="${TARGET}" \
+    --find-links=./dist  \
+    --no-cache-dir       \
+    --no-user            \
+    --no-index "${ARCH_NAME}"
+
+# если есть директория ${TMP_DIR}/usr/lib/pythonX.X/site-packages/bin/
+# перемещаем ее в ${TMP_DIR}/usr/bin/ и удаляем все скомпилированные байт-коды
+[ -d "${TARGET}/bin" ] && mv "${TARGET}/bin" "${TMP_DIR}/usr/"
+rm -rfv "${TMP_DIR}/usr/bin/__pycache__"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (Python Cryptography Toolkit)
+#
+# PyCryptodome is a collection of both secure hash functions (such as SHA256
+# and RIPEMD160), and various encryption algorithms (AES, DES, RSA, ElGamal,
+# etc.). PyCryptodome is a fork of PyCrypto. It's a self-contained Python
+# package of low-level cryptographic primitives. It supports Python 2.6 or
+# newer, all Python 3 versions and PyPy. PyCryptodome is not a wrapper to a
+# separate C library like OpenSSL. To the largest possible extent, algorithms
+# are implemented in pure Python. Only the pieces that are extremely critical
+# to performance (e.g. block ciphers) are implemented as C extensions.
+#
+# Home page: https://pypi.org/project/${ARCH_NAME}/
+# Download:  https://github.com/Legrandin/${ARCH_NAME}/archive/v${VERSION}/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
