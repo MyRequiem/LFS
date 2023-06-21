79039883a12bc7b3802ab8a4bb00ab8d9fd9cab9
Vladimir <mrvladislavovich@gmail.com>
2023-06-21 09:45:42 +0400

    BLFS: d/python-modules/python-py3c package removed, d/py3c package added

--- stage-2-blfs-stable-x86_64/build/d/py3c.sh
+++ stage-2-blfs-stable-x86_64/build/d/py3c.sh
@@ -1,26 +1,28 @@
 #! /bin/bash
 
-PRGNAME="python-py3c"
-ARCH_NAME="py3c"
+PRGNAME="py3c"
 
-### python-py3c (Python 2/3 compatibility layer for C extensions)
-# Помогает портировать расширения C на Python 3
+### Py3c (Python 2/3 compatibility layer for C extensions)
+# Помогает портировать C-расширения на Python 3
 
 # Required:    no
 # Recommended: no
 # Optional:    no
 
 ROOT="/root/src/lfs"
-source "${ROOT}/check_environment.sh"                    || exit 1
-source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+source "${ROOT}/check_environment.sh"                  || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
 
 TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
 mkdir -pv "${TMP_DIR}"
 
 # пакет содержит только C-заголовочные файлы и устанавливает их в
-# /usr/include/py3c и поэтому не требует настройки и компиляции
+# /usr/include/py3c/ и поэтому не требует конфигурации и компиляции
 
+# тесты
 # make test-python3
+# make test-python3-cpp
+
 make prefix=/usr install DESTDIR="${TMP_DIR}"
 
 /bin/cp -vpR "${TMP_DIR}"/* /
@@ -31,8 +33,8 @@ cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Py3c helps you port C extensions to Python 3. It provides a detailed guide,
 # and a set of macros to make porting easy and reduce boilerplate
 #
-# Home page: https://pypi.org/project/${ARCH_NAME}/
-# Download:  https://github.com/encukou/${ARCH_NAME}/archive/v${VERSION}/${ARCH_NAME}-${VERSION}.tar.gz
+# Home page: https://pypi.org/project/${PRGNAME}/
+# Download:  https://github.com/encukou/${PRGNAME}/archive/v${VERSION}/${PRGNAME}-${VERSION}.tar.gz
 #
 EOF
 
