60484b1aec5032953af474d01fe59cd457c6c78d
Vladimir <mrvladislavovich@gmail.com>
2023-12-08 20:33:49 +0400

    BLFS: python3-pylint package updated

---
+++ stage-2-blfs-stable-x86_64/build/d/python-modules/python3-pylint.sh
@@ -0,0 +1,104 @@
+#! /bin/bash
+
+PRGNAME="python3-pylint"
+ARCH_NAME="pylint"
+
+### pylint (python code checker)
+# Инструмент статического анализа кода для Python 3
+
+# Required:    python3-platformdirs
+#              python3-astroid
+#              python3-isort
+#              python3-mccabe
+#              python3-tomlkit
+#              python3-dill
+#              python3-tomli
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh" || exit 1
+
+SOURCES="${ROOT}/src"
+VERSION="$(find "${SOURCES}" -type f \
+    -name "${ARCH_NAME}-2*.tar.?z*" 2>/dev/null | sort | head -n 1 | \
+    rev | cut -d . -f 3- | cut -d - -f 1 | rev)"
+
+BUILD_DIR="/tmp/build-${PRGNAME}-${VERSION}"
+rm -rf "${BUILD_DIR}"
+mkdir -pv "${BUILD_DIR}"
+cd "${BUILD_DIR}" || exit 1
+
+tar xvf "${SOURCES}/${ARCH_NAME}-${VERSION}"*.tar.?z* || exit 1
+cd "${ARCH_NAME}-${VERSION}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+###
+# сборка средствами модуля wheel
+# создаем пакет в формате .whl в директории dist дерева исходников
+###
+# команда создает архив для этого пакета
+#    wheel
+# инструктирует pip поместить созданный пакет в указанный каталог dist
+#    --wheel-dir=./dist
+# не устанавливать зависимости для пакета
+#    --no-deps
+# предотвращаем получение файлов из онлайн-репозитория пакетов (PyPI). Если
+# пакеты установлены в правильном порядке, pip вообще не нужно будет извлекать
+# какие-либо файлы
+#    --no-build-isolation
+pip3 wheel               \
+    --wheel-dir=./dist   \
+    --no-deps            \
+    --no-build-isolation \
+    ./ || exit 1
+
+### устанавливаем созданный пакет в "${TMP_DIR}"
+# отключает кеш, чтобы предотвратить предупреждение при установке от
+# пользователя root
+#    --no-cache-dir
+# предотвращает ошибочный запуск команды установки от имени обычного
+# пользователя без полномочий root
+#    --no-user
+pip3 install            \
+    --root="${TMP_DIR}" \
+    --find-links=./dist \
+    --no-cache-dir      \
+    --no-user           \
+    --no-index "${ARCH_NAME}" || exit 1
+
+# если есть директория ${TMP_DIR}/usr/lib/pythonX.X/site-packages/bin/
+# перемещаем ее в ${TMP_DIR}/usr/
+PYTHON_MAJ_VER="$(python3 -V | cut -d ' ' -f 2 | cut -d . -f 1,2)"
+TMP_SITE_PACKAGES="${TMP_DIR}/usr/lib/python${PYTHON_MAJ_VER}/site-packages"
+[ -d "${TMP_SITE_PACKAGES}/bin" ] && \
+    mv "${TMP_SITE_PACKAGES}/bin" "${TMP_DIR}/usr/"
+
+# удаляем все скомпилированные байт-коды из ${TMP_DIR}/usr/bin/, если таковые
+# имеются
+PYCACHE="${TMP_DIR}/usr/bin/__pycache__"
+[ -d "${PYCACHE}" ] && rm -rf "${PYCACHE}"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (python code checker)
+#
+# Pylint is a python tool that checks if a module satisfies a coding standard.
+# Pylint can be seen as another PyChecker since nearly all tests you can do
+# with PyChecker can also be done with Pylint. But Pylint offers some more
+# features. The big advantage with Pylint is that it is highly configurable,
+# customizable, and you can easily write a small plugin to add a personal
+# feature.
+#
+# Home page: https://pypi.org/project/${ARCH_NAME}/
+# Download:  https://files.pythonhosted.org/packages/source/p/${ARCH_NAME}/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
