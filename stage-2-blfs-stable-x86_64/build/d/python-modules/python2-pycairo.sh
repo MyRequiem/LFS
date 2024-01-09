d78875b81d0c2e218cc4d16c6720526323b8ec97
Vladimir <mrvladislavovich@gmail.com>
2024-01-09 19:22:46 +0400

    BLFS: python2-pycairo and python3-pycairo packages updated

--- stage-2-blfs-stable-x86_64/build/d/python-modules/python2-pycairo.sh
+++ stage-2-blfs-stable-x86_64/build/d/python-modules/python2-pycairo.sh
@@ -1,22 +1,23 @@
 #! /bin/bash
 
-PRGNAME="python3-pycairo"
+PRGNAME="python2-pycairo"
 ARCH_NAME="pycairo"
 
-### PyCairo (PyCairo Python3 Module)
-# Python3 bindings для Cairo
+### PyCairo (PyCairo Python2 Module)
+# Python2 bindings для Cairo
 
 # Required:    cairo
-#              python3
+#              python2
 # Recommended: no
-# Optional:    hypothesis https://hypothesis.readthedocs.io/en/latest/ (для тестов)
+# Optional:    --- для тестов ---
+#              python3-hypothesis (https://hypothesis.readthedocs.io/en/latest/)
 
 ROOT="/root/src/lfs"
 source "${ROOT}/check_environment.sh" || exit 1
 
 SOURCES="${ROOT}/src"
 VERSION="$(find "${SOURCES}" -type f \
-    -name "${ARCH_NAME}-1.2*.tar.?z*" 2>/dev/null | sort | head -n 1 | \
+    -name "${ARCH_NAME}-1.1*.tar.?z*" 2>/dev/null | sort | head -n 1 | \
     rev | cut -d . -f 3- | cut -d - -f 1 | rev)"
 
 BUILD_DIR="/tmp/build-${PRGNAME}-${VERSION}"
@@ -30,17 +31,18 @@ mkdir -pv "${TMP_DIR}"
 tar xvf "${SOURCES}/${ARCH_NAME}-${VERSION}"*.tar.?z* || exit 1
 cd "${ARCH_NAME}-${VERSION}" || exit 1
 
-python3 setup.py build || exit 1
-# пакет не имеет набора тестов
-python3 setup.py install --optimize=1 --root="${TMP_DIR}"
+python2 setup.py build                                      || exit 1
+python2 setup.py install --optimize=1   --root="${TMP_DIR}" || exit 1
+python2 setup.py install_pycairo_header --root="${TMP_DIR}" || exit 1
+python2 setup.py install_pkgconfig      --root="${TMP_DIR}" || exit 1
 
-source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/stripping.sh" || exit 1
 /bin/cp -vpR "${TMP_DIR}"/* /
 
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
-# Package: ${PRGNAME} (PyCairo Python3 Module)
+# Package: ${PRGNAME} (PyCairo Python2 Module)
 #
-# PyCairo provides Python3 bindings to Cairo
+# PyCairo provides Python2 bindings to Cairo
 #
 # Home page: https://${ARCH_NAME}.readthedocs.io/
 # Download:  https://github.com/pygobject/${ARCH_NAME}/releases/download/v${VERSION}/${ARCH_NAME}-${VERSION}.tar.gz
