9ee77bfb89c52d8abbbf4095fd7360fd4fa13f91
Vladimir <mrvladislavovich@gmail.com>
2024-01-16 18:19:48 +0400

    BLFS: gtk-vnc package updated

--- stage-2-blfs-stable-x86_64/build/s/gtk-vnc.sh
+++ stage-2-blfs-stable-x86_64/build/s/gtk-vnc.sh
@@ -12,7 +12,6 @@ PRGNAME="gtk-vnc"
 #              gtk+3
 #              libgcrypt
 # Recommended: gobject-introspection
-#              python2
 #              vala
 # Optional:    cyrus-sasl
 #              pulseaudio
@@ -27,8 +26,9 @@ mkdir -pv "${TMP_DIR}"
 mkdir build
 cd build || exit 1
 
-meson             \
-    --prefix=/usr \
+meson                   \
+    --prefix=/usr       \
+    --buildtype=release \
     .. || exit 1
 
 ninja || exit 1
