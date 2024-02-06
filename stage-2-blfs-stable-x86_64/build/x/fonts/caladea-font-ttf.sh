9b8728ea405c76540f6e45d080510daef582d504
Vladimir <mrvladislavovich@gmail.com>
2024-02-06 16:39:26 +0400

    BLFS: caladea-font-ttf package updated

--- stage-2-blfs-stable-x86_64/build/x/fonts/caladea-font-ttf.sh
+++ stage-2-blfs-stable-x86_64/build/x/fonts/caladea-font-ttf.sh
@@ -16,15 +16,15 @@ source "${ROOT}/check_environment.sh"                    || exit 1
 source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
 
 TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
-TTF_FONT_DIR="/usr/share/fonts/X11/TTF/"
-mkdir -pv "${TMP_DIR}${TTF_FONT_DIR}"
+INSTALL_DIR="/usr/share/fonts/${PRGNAME}/"
+mkdir -pv "${TMP_DIR}${INSTALL_DIR}"
 
-cp ./*.ttf "${TMP_DIR}${TTF_FONT_DIR}"
+cp ./*.ttf "${TMP_DIR}${INSTALL_DIR}"
 
 /bin/cp -vpR "${TMP_DIR}"/* /
 
 # обновим индексы установленных шрифтов
-cd "${TTF_FONT_DIR}" || exit 1
+cd "${INSTALL_DIR}" || exit 1
 # создаем индекс файлов масштабируемых шрифтов
 mkfontscale .
 # создаем индекс файлов шрифтов в каталоге
@@ -40,7 +40,7 @@ cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # have to edit a document which somebody started in Microsoft Office using
 # Cambria and then return it to them.
 #
-# Home page: https://bugs.chromium.org/p/chromium/issues/detail?id=168879
+# Home page: https://fonts.google.com/specimen/Caladea
 # Download:  http://gsdview.appspot.com/chromeos-localmirror/distfiles/${ARCH_NAME}-${VERSION}.tar.gz
 #
 EOF
