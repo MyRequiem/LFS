7db29a2ad05514b9625c2c382b2b25529d7b1c34
Vladimir <mrvladislavovich@gmail.com>
2024-02-07 20:27:24 +0400

    BLFS: gelasio-font-ttf package updated

--- stage-2-blfs-stable-x86_64/build/x/fonts/gelasio-font-ttf.sh
+++ stage-2-blfs-stable-x86_64/build/x/fonts/gelasio-font-ttf.sh
@@ -1,12 +1,10 @@
 #! /bin/bash
 
 PRGNAME="gelasio-font-ttf"
-ARCH_NAME="$(echo ${PRGNAME} | cut -d - -f 1)"
-VERSION="13042020"
 
 ### Gelasio (Gelasio TTF font)
-# Шрифт совместимый со шрифтом MS Georgia и fontconfig будет его использовать
-# если запрошивается MS Georgia, но он не устанавлен.
+# Шрифт от Google совместимый со шрифтом MS Georgia и fontconfig будет его
+# использовать если запрошивается MS Georgia, но он не устанавлен.
 
 # Required:    xorg-applications
 #              fontconfig
@@ -14,26 +12,19 @@ VERSION="13042020"
 # Optional:    no
 
 ROOT="/root/src/lfs"
-source "${ROOT}/check_environment.sh" || exit 1
-
-BUILD_DIR="/tmp/build-${PRGNAME}-${VERSION}"
-rm -rf "${BUILD_DIR}"
-mkdir -pv "${BUILD_DIR}"
-cd "${BUILD_DIR}" || exit 1
-
-SOURCES="${ROOT}/src"
-unzip -d "${ARCH_NAME}-${VERSION}" "${SOURCES}/${ARCH_NAME}.zip" || exit 1
+source "${ROOT}/check_environment.sh"                  || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
 
 TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
-TTF_FONT_DIR="/usr/share/fonts/X11/TTF/"
-mkdir -pv "${TMP_DIR}${TTF_FONT_DIR}"
+INSTALL_DIR="/usr/share/fonts/${PRGNAME}/"
+mkdir -pv "${TMP_DIR}${INSTALL_DIR}"
 
-cp "${ARCH_NAME}-${VERSION}"/*.ttf "${TMP_DIR}${TTF_FONT_DIR}"
+cp ./*.ttf "${TMP_DIR}${INSTALL_DIR}"
 
 /bin/cp -vpR "${TMP_DIR}"/* /
 
 # обновим индексы установленных шрифтов
-cd "${TTF_FONT_DIR}" || exit 1
+cd "${INSTALL_DIR}" || exit 1
 # создаем индекс файлов масштабируемых шрифтов
 mkfontscale .
 # создаем индекс файлов шрифтов в каталоге
@@ -41,14 +32,16 @@ mkfontdir .
 # создаем файлы кэша информации о шрифтах для fontconfig
 fc-cache -f
 
+cp fonts.dir fonts.scale "${TMP_DIR}${INSTALL_DIR}"
+
 cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Package: ${PRGNAME} (Gelasio TTF font)
 #
-# Gelasio is metrically compatible with MS Georgia and fontconfig will use it
-# if ever Georgia is requested but not installed.
+# Gelasio is a Google font that is metrically comaptible with MS Georgia and
+# fontconfig will use it if ever Georgia is requested but not installed.
 #
-# Home page: https://fontlibrary.org/en/font/${ARCH_NAME}
-# Download:  https://fontlibrary.org/assets/downloads/${ARCH_NAME}/4d610887ff4d445cbc639aae7828d139/${ARCH_NAME}.zip
+# Home page: https://fonts.google.com/specimen/Gelasio
+# Download:  https://github.com/MyRequiem/LFS/raw/master/stage-2-blfs-stable-x86_64/src/${PRGNAME}/${PRGNAME}-${VERSION}.tar.xz
 #
 EOF
 
