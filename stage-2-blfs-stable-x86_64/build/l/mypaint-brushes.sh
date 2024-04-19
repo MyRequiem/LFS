1461c1a3d5f28670cbb96a01fea032a2eff9be6e
Vladimir <mrvladislavovich@gmail.com>
2024-04-20 02:31:41 +0400

    BLFS: mypaint-brushes package updated

--- stage-2-blfs-stable-x86_64/build/l/mypaint-brushes.sh
+++ stage-2-blfs-stable-x86_64/build/l/mypaint-brushes.sh
@@ -10,35 +10,12 @@ PRGNAME="mypaint-brushes"
 # Optional:    no
 
 ROOT="/root/src/lfs"
-source "${ROOT}/check_environment.sh" || exit 1
-
-SOURCES="${ROOT}/src"
-VERSION="$(find "${SOURCES}" -type f \
-    -name "${PRGNAME}-*.tar.?z*" 2>/dev/null | sort | head -n 1 | \
-    rev | cut -d . -f 3- | cut -d - -f 1 | rev | cut -d v -f 2)"
-
-BUILD_DIR="/tmp/build-${PRGNAME}-${VERSION}"
-rm -rf "${BUILD_DIR}"
-mkdir -pv "${BUILD_DIR}"
-cd "${BUILD_DIR}" || exit 1
-
-tar xvf "${SOURCES}/${PRGNAME}-v${VERSION}"*.tar.?z* || exit 1
-cd "${PRGNAME}-${VERSION}" || exit 1
-
-chown -R root:root .
-find -L . \
-    \( -perm 777 -o -perm 775 -o -perm 750 -o -perm 711 -o -perm 555 \
-    -o -perm 511 \) -exec chmod 755 {} \; -o \
-    \( -perm 666 -o -perm 664 -o -perm 640 -o -perm 600 -o -perm 444 \
-    -o -perm 440 -o -perm 400 \) -exec chmod 644 {} \;
+source "${ROOT}/check_environment.sh"                  || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
 
 TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
 mkdir -pv "${TMP_DIR}"
 
-patch --verbose -Np1 -i \
-    "${SOURCES}/${PRGNAME}-${VERSION}-automake_1.16-1.patch" || exit 1
-
-./autogen.sh &&
 ./configure \
     --prefix=/usr || exit 1
 
@@ -57,7 +34,7 @@ cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # libmypaint.
 #
 # Home page: https://github.com/Jehan/${PRGNAME}
-# Download:  https://github.com/Jehan/${PRGNAME}/archive/v${VERSION}/${PRGNAME}-v${VERSION}.tar.gz
+# Download:  https://github.com/mypaint/${PRGNAME}/releases/download/v${VERSION}/${PRGNAME}-${VERSION}.tar.xz
 #
 EOF
 
