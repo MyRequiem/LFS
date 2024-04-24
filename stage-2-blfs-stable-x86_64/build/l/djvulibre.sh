25d1c7d3d2fa9b06506f43b9cf5c94078110db88
Vladimir <mrvladislavovich@gmail.com>
2024-04-25 00:55:17 +0400

    BLFS: djvulibre home page link fixed

--- stage-2-blfs-stable-x86_64/build/l/djvulibre.sh
+++ stage-2-blfs-stable-x86_64/build/l/djvulibre.sh
@@ -40,7 +40,7 @@ cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # DjVu was originally developed at AT&T Labs-Research by Leon Bottou, Yann
 # LeCun, Patrick Haffner, and many others.
 #
-# Home page: http://djvu.sourceforge.net/
+# Home page: https://djvu.sourceforge.net/
 # Download:  http://downloads.sourceforge.net/djvu/${PRGNAME}-${VERSION}.tar.gz
 #
 EOF
