16a8d4d8d3d1b568e5b23f9ef536b2887b00ad0a
Vladimir <mrvladislavovich@gmail.com>
2024-04-26 15:27:44 +0400

    BLFS: libsoup package updated

--- stage-2-blfs-stable-x86_64/build/l/libsoup.sh
+++ stage-2-blfs-stable-x86_64/build/l/libsoup.sh
@@ -12,48 +12,61 @@ PRGNAME="libsoup"
 #              libxml2
 #              sqlite
 # Recommended: gobject-introspection
-#              sysprof
 #              vala
 # Optional:    apache-httpd     (для тестов)
 #              brotli
 #              curl             (для тестов)
+#              sysprof
 #              mit-kerberos-v5  (для тестов)
 #              gtk-doc
 #              php              (собранный с поддержкой xmlrpc-epi для тестов)
 #              samba            (для тестов)
 
 ROOT="/root/src/lfs"
-source "${ROOT}/check_environment.sh"                  || exit 1
-source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
+source "${ROOT}/check_environment.sh" || exit 1
+
+SOURCES="${ROOT}/src"
+VERSION="$(find ${SOURCES} -type f \
+    -name "${PRGNAME}-2*.tar.?z*" 2>/dev/null | sort | \
+    head -n 1 | rev | cut -d . -f 3- | cut -d - -f 1 | rev)"
+
+BUILD_DIR="/tmp/build-${PRGNAME}-${VERSION}"
+rm -rf "${BUILD_DIR}"
+mkdir -pv "${BUILD_DIR}"
+cd "${BUILD_DIR}" || exit 1
+tar xvf "${SOURCES}/${PRGNAME}-${VERSION}".tar.?z* || exit 1
+cd "${PRGNAME}-${VERSION}" || exit 1
+
+chown -R root:root .
+find -L . \
+    \( -perm 777 -o -perm 775 -o -perm 750 -o -perm 711 -o -perm 555 \
+    -o -perm 511 \) -exec chmod 755 {} \; -o \
+    \( -perm 666 -o -perm 664 -o -perm 640 -o -perm 600 -o -perm 444 \
+    -o -perm 440 -o -perm 400 \) -exec chmod 644 {} \;
 
 TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
 mkdir -pv "${TMP_DIR}"
 
-GSSAPI="disabled"
 VALA_API="disabled"
-INTROSPECTION="disabled"
-GTK_DOC="false"
+GSSAPI="disabled"
 TESTS="false"
+GTK_DOC="false"
 
-command -v krb5-config   &>/dev/null && GSSAPI="enabled"
-command -v vala          &>/dev/null && VALA_API="enabled"
-command -v g-ir-compiler &>/dev/null && INTROSPECTION="enabled"
-# command -v gtkdoc-check  &>/dev/null && GTK_DOC="true"
-
-# исправим проблему в комплекте тестов, вызванную glib-networking
-patch --verbose -Np1 -i \
-    "${SOURCES}/${PRGNAME}-${VERSION}-testsuite_fix-1.patch" || exit 1
+command -v vala         &>/dev/null && VALA_API="enabled"
+command -v krb5-config  &>/dev/null && GSSAPI="enabled"
+# command -v gtkdoc-check &>/dev/null && GTK_DOC="true"
 
 mkdir build
 cd build || exit 1
 
-meson                                  \
-    --prefix=/usr                      \
-    -Dgssapi="${GSSAPI}"               \
-    -Dtests="${TESTS}"                 \
-    -Dvapi="${VALA_API}"               \
-    -Dgtk_doc="${GTK_DOC}"             \
-    -Dintrospection="${INTROSPECTION}" \
+meson                      \
+    --prefix=/usr          \
+    --buildtype=release    \
+    -Dvapi="${VALA_API}"   \
+    -Dgssapi="${GSSAPI}"   \
+    -Dsysprof=disabled     \
+    -Dtests="${TESTS}"     \
+    -Dgtk_doc="${GTK_DOC}" \
     .. || exit 1
 
 ninja || exit 1
