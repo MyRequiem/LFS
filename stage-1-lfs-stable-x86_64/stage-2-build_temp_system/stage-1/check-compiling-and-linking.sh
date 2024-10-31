5c3fbb50bcf2f3cc8b3faf936456a3ad8fcaaf4a
Vladimir <mrvladislavovich@gmail.com>
2024-10-31 15:14:07 +0400

    LFS:  stage-2-build_temp_system/stage-1/{check-compiling-and-linking,diffutils,file,findutils,gawk,grep}.sh updated

--- stage-1-lfs-stable-x86_64/stage-2-build_temp_system/stage-1/check-compiling-and-linking.sh
+++ stage-1-lfs-stable-x86_64/stage-2-build_temp_system/stage-1/check-compiling-and-linking.sh
@@ -2,8 +2,7 @@
 
 # После установки binutils, gcc и glibc необходимо убедиться, что основные
 # функции (компиляция и компоновка) новой цепочки инструментов сборки работают
-# должным образом. Для проверки работоспособности создадим простейший C-файл и
-# скомпилируем его собранным нами gcc
+# должным образом
 
 source "$(pwd)/check_environment.sh" || exit 1
 
@@ -11,24 +10,9 @@ echo ""
 echo "--------"
 echo "Step: 1"
 echo "--------"
-echo "# creating simple C-file"
-echo "echo 'int main(){}' > dummy.c"
-echo 'int main(){}' > dummy.c
-echo "ls -l dummy.c"
-ls -l dummy.c
-echo ""
-echo -n "Press any key... "
-read -r JUNK
-echo "${JUNK}" > /dev/null
-echo ""
-
-echo "--------"
-echo "Step: 2"
-echo "--------"
-echo "# compiling source file dummy.c using ${LFS}/tools/bin/${LFS_TGT}-gcc"
-echo "# (as a result of compilation, an object file a.out is generated)"
-echo "${LFS_TGT}-gcc dummy.c"
-"${LFS_TGT}"-gcc dummy.c
+echo "# creating a.out"
+echo "echo 'int main(){}' | $LFS_TGT-gcc -xc -"
+echo 'int main(){}' | "${LFS_TGT}-gcc" -xc -
 echo "ls -l a.out"
 ls -l a.out
 echo ""
@@ -38,12 +22,12 @@ echo "${JUNK}" > /dev/null
 echo ""
 
 echo "--------"
-echo "Step: 3"
+echo "Step: 2"
 echo "--------"
 # посмотрим имя динамического компоновщика
 echo "# show dynamic linker name"
-echo "readelf -l a.out | grep '/ld-linux'"
-readelf -l a.out | grep '/ld-linux'
+echo "readelf -l a.out | grep ld-linux"
+readelf -l a.out | grep ld-linux
 echo ""
 echo "# The output should be something like this:"
 echo "#     [Requesting program interpreter: /lib64/ld-linux-x86-64.so.2]"
@@ -51,4 +35,4 @@ echo ""
 # если вывод не такой, как указано выше, или вывод не был получен вообще,
 # значит что-то не так.
 
-rm -f dummy.c a.out
+rm -f a.out
