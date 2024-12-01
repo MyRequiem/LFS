9777eb48bbda7ec143eff59939bc266c4195a8c2
Vladimir <mrvladislavovich@gmail.com>
2024-12-01 20:39:59 +0400

    BLFS: python3-docutils and python3-packaging packages updated

---
+++ stage-2-blfs-stable-x86_64/build/d/python-modules/python3-docutils.sh
@@ -0,0 +1,67 @@
+#! /bin/bash
+
+PRGNAME="python3-docutils"
+ARCH_NAME="docutils"
+
+### docutils (Python Documentation Utilities)
+# Модульная система для преобразования документации в другие форматы, такие как
+# HTML, XML и LaTeX. Для ввода Docutils поддерживает reStructuredText, простой
+# для чтения текст в формате "что видишь, то и получаешь"
+
+# Required:    no
+# Recommended: no
+# Optional:    no
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+pip3 wheel               \
+    -w dist              \
+    --no-build-isolation \
+    --no-deps            \
+    --no-cache-dir       \
+    "${PWD}" || exit 1
+
+pip3 install            \
+    --root="${TMP_DIR}" \
+    --no-index          \
+    --find-links=dist   \
+    --no-cache-dir      \
+    --no-user           \
+    "${ARCH_NAME}" || exit 1
+
+# если есть директория ${TMP_DIR}/usr/lib/pythonX.X/site-packages/bin/
+# перемещаем ее в ${TMP_DIR}/usr/
+PYTHON_MAJ_VER="$(python3 -V | cut -d ' ' -f 2 | cut -d . -f 1,2)"
+TMP_SITE_PACKAGES="${TMP_DIR}/usr/lib/python${PYTHON_MAJ_VER}/site-packages"
+[ -d "${TMP_SITE_PACKAGES}/bin" ] && \
+    mv "${TMP_SITE_PACKAGES}/bin" "${TMP_DIR}/usr/"
+
+# удаляем все скомпилированные байт-коды из ${TMP_DIR}/usr/bin/, если таковые
+# имеются
+PYCACHE="${TMP_DIR}/usr/bin/__pycache__"
+[ -d "${PYCACHE}" ] && rm -rf "${PYCACHE}"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (Python Documentation Utilities)
+#
+# Docutils is a modular system for processing documentation into useful
+# formats, such as HTML, XML, and LaTeX. For input Docutils supports
+# reStructuredText, an easy-to-read, what-you-see-is-what-you-get plaintext
+# markup syntax.
+#
+# Home page: https://pypi.python.org/pypi/${ARCH_NAME}/
+# Download:  https://files.pythonhosted.org/packages/source/d/${ARCH_NAME}/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
---
+++ stage-2-blfs-stable-x86_64/build/d/python-modules/python3-packaging.sh
@@ -0,0 +1,66 @@
+#! /bin/bash
+
+PRGNAME="python3-packaging"
+ARCH_NAME="packaging"
+
+### Packaging (core utilities for Python packages)
+# утилиты, реализующие совместимость спецификаций пакетов Python
+
+# Required:    no
+# Recommended: no
+# Optional:    --- для тестов ---
+#              python3-pytest
+#              python3-pretend  (https://pypi.org/project/pretend/)
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+pip3 wheel               \
+    -w dist              \
+    --no-build-isolation \
+    --no-deps            \
+    --no-cache-dir       \
+    "${PWD}" || exit 1
+
+pip3 install            \
+    --root="${TMP_DIR}" \
+    --no-index          \
+    --find-links=dist   \
+    --no-cache-dir      \
+    --no-user           \
+    "${ARCH_NAME}" || exit 1
+
+# если есть директория ${TMP_DIR}/usr/lib/pythonX.X/site-packages/bin/
+# перемещаем ее в ${TMP_DIR}/usr/
+PYTHON_MAJ_VER="$(python3 -V | cut -d ' ' -f 2 | cut -d . -f 1,2)"
+TMP_SITE_PACKAGES="${TMP_DIR}/usr/lib/python${PYTHON_MAJ_VER}/site-packages"
+[ -d "${TMP_SITE_PACKAGES}/bin" ] && \
+    mv "${TMP_SITE_PACKAGES}/bin" "${TMP_DIR}/usr/"
+
+# удаляем все скомпилированные байт-коды из ${TMP_DIR}/usr/bin/, если таковые
+# имеются
+PYCACHE="${TMP_DIR}/usr/bin/__pycache__"
+[ -d "${PYCACHE}" ] && rm -rf "${PYCACHE}"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (core utilities for Python packages)
+#
+# The Packaging library provides utilities that implement the interoperability
+# specifications which have clearly one correct behaviour or benefit greatly
+# from having a single shared implementation.
+#
+# Home page: https://pypi.org/project/${ARCH_NAME}/
+# Download:  https://files.pythonhosted.org/packages/source/p/${ARCH_NAME}/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
