90727cf7461a02deab018621ea03fd23d547a33f
Vladimir <mrvladislavovich@gmail.com>
2024-12-03 21:33:41 +0400

    BLFS: lshw and grc packages updated

--- stage-2-blfs-stable-x86_64/build/s/lshw.sh
+++ stage-2-blfs-stable-x86_64/build/s/lshw.sh
@@ -18,22 +18,13 @@ source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
 TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
 mkdir -pv "${TMP_DIR}"
 
-GUI="no" # yes|no
 ENABLE_SQLITE=0
 [ -x /usr/lib/libsqlite3.so ] && ENABLE_SQLITE=1
 
 VERSION="${VERSION}" make ZLIB=1 SQLITE="${ENABLE_SQLITE}" || exit 1
-if [ "${GUI}" = "yes" ]; then
-    VERSION="${VERSION}" make gui ZLIB=1 SQLITE="${ENABLE_SQLITE}" || exit 1
-fi
 
 VERSION="${VERSION}" \
     make ZLIB=1 SQLITE="${ENABLE_SQLITE}" install DESTDIR="${TMP_DIR}"
-
-if [ "$GUI" = "yes" ]; then
-    VERSION="${VERSION}" make \
-        ZLIB=1 SQLITE="${ENABLE_SQLITE}" install-gui DESTDIR="${TMP_DIR}"
-fi
 
 source "${ROOT}/stripping.sh"      || exit 1
 source "${ROOT}/update-info-db.sh" || exit 1
