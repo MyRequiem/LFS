90d76a2d63af8d82ad5bf354249f44519b3e03b0
Vladimir <mrvladislavovich@gmail.com>
2024-12-07 13:52:27 +0400

    BLFS: python3-{urllib3,idna,charset-normalizer,requests} packages updated

---
+++ stage-2-blfs-stable-x86_64/build/d/python-modules/python3-charset-normalizer.sh
@@ -0,0 +1,67 @@
+#! /bin/bash
+
+PRGNAME="python3-charset-normalizer"
+ARCH_NAME="charset-normalizer"
+
+### Charset-normalizer (charset detection for python)
+# Python-библиотека, которая помогает читать текст в неизвестной кодировке
+# символов
+
+# Required:    no
+# Recommended: no
+# Optional:    --- для тестов ---
+#              python3-pytest
+#              python3            (пересобранный в BLFS с sqlite)
+#              python3-pytest-cov (https://pypi.org/project/pytest-cov/)
+
+ROOT="/root/src/lfs"
+source "${ROOT}/check_environment.sh"                    || exit 1
+source "${ROOT}/unpack_source_archive.sh" "${ARCH_NAME}" || exit 1
+
+TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
+mkdir -pv "${TMP_DIR}"
+
+pip3 wheel               \
+    -w dist              \
+    --no-build-isolation \
+    --no-deps            \
+    --no-cache-dir       \
+    "${PWD}" || exit 1
+
+pip3 install            \
+    --root="${TMP_DIR}" \
+    --no-index          \
+    --find-links=dist   \
+    --no-cache-dir      \
+    --no-user           \
+    "${ARCH_NAME}" || exit 1
+
+# если есть директория ${TMP_DIR}/usr/lib/pythonX.X/site-packages/bin/
+# перемещаем ее в ${TMP_DIR}/usr/
+PYTHON_MAJ_VER="$(python3 -V | cut -d ' ' -f 2 | cut -d . -f 1,2)"
+TMP_SITE_PACKAGES="${TMP_DIR}/usr/lib/python${PYTHON_MAJ_VER}/site-packages"
+[ -d "${TMP_SITE_PACKAGES}/bin" ] && \
+    mv "${TMP_SITE_PACKAGES}/bin" "${TMP_DIR}/usr/"
+
+# удаляем все скомпилированные байт-коды из ${TMP_DIR}/usr/bin/, если таковые
+# имеются
+PYCACHE="${TMP_DIR}/usr/bin/__pycache__"
+[ -d "${PYCACHE}" ] && rm -rf "${PYCACHE}"
+
+source "${ROOT}/stripping.sh"      || exit 1
+source "${ROOT}/update-info-db.sh" || exit 1
+/bin/cp -vpR "${TMP_DIR}"/* /
+
+cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
+# Package: ${PRGNAME} (charset detection for python)
+#
+# This library helps read text from an unknown charset encoding, providing an
+# alternative to the Universal Charset Encoding Detector, also known as Chardet
+#
+# Home page: https://pypi.org/project/${ARCH_NAME}/
+# Download:  https://files.pythonhosted.org/packages/source/c/${ARCH_NAME}/${ARCH_NAME}-${VERSION}.tar.gz
+#
+EOF
+
+source "${ROOT}/write_to_var_log_packages.sh" \
+    "${TMP_DIR}" "${PRGNAME}-${VERSION}"
