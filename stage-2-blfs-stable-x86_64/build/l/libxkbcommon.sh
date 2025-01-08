50935c355a5f8b8bd1d27aaf1920523a4df215a1
Vladimir <mrvladislavovich@gmail.com>
2025-01-08 22:49:17 +0400

    BLFS: libxkbcommon and sassc packages updated

--- stage-2-blfs-stable-x86_64/build/l/libxkbcommon.sh
+++ stage-2-blfs-stable-x86_64/build/l/libxkbcommon.sh
@@ -12,7 +12,7 @@ PRGNAME="libxkbcommon"
 # Recommended: libxcb
 #              wayland
 #              wayland-protocols
-# Optional:    doxygen              (для сборки документации)
+# Optional:    doxygen
 
 ROOT="/root/src/lfs"
 source "${ROOT}/check_environment.sh"                  || exit 1
@@ -21,33 +21,17 @@ source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
 TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
 mkdir -pv "${TMP_DIR}"
 
-ENABLE_DOCS="false"
-ENABLE_WAYLAND="false"
-
-# command -v doxygen &>/dev/null && ENABLE_DOCS="true"
-[ -d /usr/share/wayland-protocols ] && ENABLE_WAYLAND="true"
-
 mkdir build
 cd build || exit 1
 
-meson                                    \
-    --prefix=/usr                        \
-    --buildtype=release                  \
-    -Denable-docs="${ENABLE_DOCS}"       \
-    -Denable-wayland="${ENABLE_WAYLAND}" \
-    .. || exit 1
+meson setup ..          \
+    --prefix=/usr       \
+    --buildtype=release \
+    -D enable-docs=false || exit 1
 
 ninja || exit 1
 # ninja test
 DESTDIR="${TMP_DIR}" ninja install
-
-# по умолчанию документация устанавливается в /usr/share/doc/libxkbcommon,
-# исправим
-if [[ "x${ENABLE_DOCS}" == "xtrue" ]]; then
-    mv "${TMP_DIR}/usr/share/doc/${PRGNAME}"{,"-${VERSION}"}
-else
-    rm -rf "${TMP_DIR}/usr/share/doc"
-fi
 
 source "${ROOT}/stripping.sh"      || exit 1
 source "${ROOT}/update-info-db.sh" || exit 1
