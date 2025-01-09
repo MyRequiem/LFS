589125f28ee60644989fdcf3e664d7d36b600edc
Vladimir <mrvladislavovich@gmail.com>
2025-01-09 17:08:08 +0400

    BLFS: opus, libsndfile and pulseaudio packages updated

--- stage-2-blfs-stable-x86_64/build/ap/opus.sh
+++ stage-2-blfs-stable-x86_64/build/ap/opus.sh
@@ -20,17 +20,17 @@ source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
 TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
 mkdir -pv "${TMP_DIR}"
 
-API_DOCS="--disable-doc"
+mkdir build
+cd build || exit 1
 
-./configure          \
-    --prefix=/usr    \
-    --disable-static \
-    "${API_DOCS}"    \
-    --docdir="/usr/share/doc/${PRGNAME}-${VERSION}" || exit 1
+meson setup             \
+    --prefix=/usr       \
+    --buildtype=release \
+    -D docdir="/usr/share/doc/${PRGNAME}-${VERSION}" || exit 1
 
-make || exit 1
-# make check
-make install DESTDIR="${TMP_DIR}"
+ninja || exit 1
+# ninja test
+DESTDIR="${TMP_DIR}" ninja install
 
 source "${ROOT}/stripping.sh"      || exit 1
 source "${ROOT}/update-info-db.sh" || exit 1
@@ -45,13 +45,9 @@ cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Internet.
 #
 # Home page: https://${PRGNAME}-codec.org/
-# Download:  https://archive.mozilla.org/pub/${PRGNAME}/${PRGNAME}-${VERSION}.tar.gz
+# Download:  https://downloads.xiph.org/releases/${PRGNAME}/${PRGNAME}-${VERSION}.tar.gz
 #
 EOF
 
 source "${ROOT}/write_to_var_log_packages.sh" \
     "${TMP_DIR}" "${PRGNAME}-${VERSION}"
-
-echo -e "\n---------------\nRemoving *.la files..."
-remove-la-files.sh
-echo "---------------"
