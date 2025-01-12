98dc674594d532d73249b35b3634d98d365c44c5
Vladimir <mrvladislavovich@gmail.com>
2025-01-13 01:59:20 +0400

    BLFS: cairomm114, pangomm24 and gtkmm3 packages updated

--- stage-2-blfs-stable-x86_64/build/l/cairomm114.sh
+++ stage-2-blfs-stable-x86_64/build/l/cairomm114.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 PRGNAME="cairomm114"
+ARCH_NAME="cairomm"
 
 ### cairomm (C++ wrapper for the cairo graphics library)
 # C++ интерфейс для графической библиотеки cairo
@@ -11,27 +12,41 @@ PRGNAME="cairomm114"
 # Optional:    doxygen
 
 ROOT="/root/src/lfs"
-source "${ROOT}/check_environment.sh"                  || exit 1
-source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
+source "${ROOT}/check_environment.sh" || exit 1
+
+SOURCES="${ROOT}/src"
+VERSION="$(find "${SOURCES}" -type f \
+    -name "${ARCH_NAME}-1.14*.tar.?z*" 2>/dev/null | sort | head -n 1 | \
+    rev | cut -d . -f 3- | cut -d - -f 1 | rev)"
+
+BUILD_DIR="/tmp/build-${PRGNAME}-${VERSION}"
+rm -rf "${BUILD_DIR}"
+mkdir -pv "${BUILD_DIR}"
+cd "${BUILD_DIR}" || exit 1
+
+tar xvf "${SOURCES}/${ARCH_NAME}-${VERSION}"*.tar.?z* || exit 1
+cd "${ARCH_NAME}-${VERSION}" || exit 1
+
+chown -R root:root .
+find -L . \
+    \( -perm 777 -o -perm 775 -o -perm 750 -o -perm 711 -o -perm 555 \
+    -o -perm 511 \) -exec chmod 755 {} \; -o \
+    \( -perm 666 -o -perm 664 -o -perm 640 -o -perm 600 -o -perm 444 \
+    -o -perm 440 -o -perm 400 \) -exec chmod 644 {} \;
 
 TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
 mkdir -pv "${TMP_DIR}"
 
-TESTS="false"
-EXAMPLES="false"
-DOCS="false"
-
-mkdir bld &&
+mkdir bld
 cd bld || exit 1
 
-meson                               \
-    --prefix=/usr                   \
-    --buildtype=release             \
-    -Dbuild-tests="${TESTS}"        \
-    -Dboost-shared=true             \
-    -Dbuild-examples="${EXAMPLES}"  \
-    -Dbuild-documentation="${DOCS}" \
-      .. || exit 1
+meson setup ..              \
+    --prefix=/usr           \
+    --buildtype=release     \
+    -D build-tests=false    \
+    -D boost-shared=true    \
+    -D build-examples=false \
+    -D build-documentation=false     || exit 1
 
 ninja || exit 1
 # ninja test
@@ -48,8 +63,8 @@ cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # offers all the power of cairo with an interface familiar to C++ developers,
 # including use of the Standard Template Library where it makes sense.
 #
-# Home page: https://www.cairographics.org/${PRGNAME}/
-# Download:  https://www.cairographics.org/releases/${PRGNAME}-${VERSION}.tar.xz
+# Home page: https://www.cairographics.org/${ARCH_NAME}/
+# Download:  https://www.cairographics.org/releases/${ARCH_NAME}-${VERSION}.tar.xz
 #
 EOF
 
--- stage-2-blfs-stable-x86_64/build/l/gtkmm3.sh
+++ stage-2-blfs-stable-x86_64/build/l/gtkmm3.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 PRGNAME="gtkmm3"
+ARCH_NAME="gtkmm"
 
 ### GTKmm3 (C++ interface for GTK+3)
 # C++ интерфейс для популярной библиотеки графического интерфейса GTK+3.
@@ -11,38 +12,48 @@ PRGNAME="gtkmm3"
 #              gtk+3
 #              pangomm24
 # Recommended: no
-# Optional:    doxygen (для сборки документации)
+# Optional:    doxygen    (для сборки документации)
 
 ROOT="/root/src/lfs"
-source "${ROOT}/check_environment.sh"                  || exit 1
-source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
+source "${ROOT}/check_environment.sh" || exit 1
+
+SOURCES="${ROOT}/src"
+VERSION="$(find "${SOURCES}" -type f \
+    -name "${ARCH_NAME}-3*.tar.?z*" 2>/dev/null | sort | head -n 1 | \
+    rev | cut -d . -f 3- | cut -d - -f 1 | rev)"
+
+BUILD_DIR="/tmp/build-${PRGNAME}-${VERSION}"
+rm -rf "${BUILD_DIR}"
+mkdir -pv "${BUILD_DIR}"
+cd "${BUILD_DIR}" || exit 1
+
+tar xvf "${SOURCES}/${ARCH_NAME}-${VERSION}"*.tar.?z* || exit 1
+cd "${ARCH_NAME}-${VERSION}" || exit 1
+
+chown -R root:root .
+find -L . \
+    \( -perm 777 -o -perm 775 -o -perm 750 -o -perm 711 -o -perm 555 \
+    -o -perm 511 \) -exec chmod 755 {} \; -o \
+    \( -perm 666 -o -perm 664 -o -perm 640 -o -perm 600 -o -perm 444 \
+    -o -perm 440 -o -perm 400 \) -exec chmod 644 {} \;
 
 TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
 mkdir -pv "${TMP_DIR}"
 
-DEMOS="false"
-TESTS="false"
-DOCS="false"
-# command -v doxygen &>/dev/null && DOCS="true"
-
 mkdir "${PRGNAME}3-build"
 cd "${PRGNAME}3-build" || exit 1
 
-meson                               \
-    --prefix=/usr                   \
-    --buildtype=release             \
-    -Dbuild-x11-api=true            \
-    -Dbuild-demos="${DEMOS}"        \
-    -Dbuild-tests="${TESTS}"        \
-    -Dbuild-documentation="${DOCS}" \
+meson setup                      \
+    --prefix=/usr                \
+    --buildtype=release          \
+    -D build-x11-api=true        \
+    -D build-tests=false         \
+    -D build-documentation=false \
     .. || exit 1
 
 ninja || exit 1
-
-# тесты нужно запускать в графической среде, а так же изменить переменную TESTS
-# выше на "true"
+# тесты нужно запускать в графической среде
 # ninja test
-
 DESTDIR="${TMP_DIR}" ninja install
 
 DOC_DIR="${TMP_DIR}/usr/share/doc/${PRGNAME}"
@@ -62,8 +73,8 @@ cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Highlights include typesafe callbacks, and a comprehensive set of widgets
 # that are easily extensible via inheritance.
 #
-# Home page: https://www.${PRGNAME}.org/
-# Download:  https://download.gnome.org/sources/${PRGNAME}/${MAJ_VERSION}/${PRGNAME}-${VERSION}.tar.xz
+# Home page: https://www.${ARCH_NAME}.org/
+# Download:  https://download.gnome.org/sources/${ARCH_NAME}/${MAJ_VERSION}/${ARCH_NAME}-${VERSION}.tar.xz
 #
 EOF
 
