bf4d3072c8557145e2cffe854a881377f769eba2
Vladimir <mrvladislavovich@gmail.com>
2025-01-24 02:20:42 +0400

    BLFS: libnfs package removed, vde2 and qemu packages updated

--- stage-2-blfs-stable-x86_64/build/s/qemu.sh
+++ stage-2-blfs-stable-x86_64/build/s/qemu.sh
@@ -36,106 +36,102 @@ PRGNAME="qemu"
 #              vte3
 #              elogind
 #              python3-sphinx-rtd-theme
-
-capstone (https://www.capstone-engine.org/)
-ceph (https://github.com/ceph/ceph/)
-daxctl (https://pmem.io/daxctl/)
-jack (https://jackaudio.org/)
-glusterfs (https://github.com/gluster/glusterfs)
-libbpf (https://github.com/libbpf/libbpf)
-libcacard (https://gitlab.freedesktop.org/spice/libcacard)
-libcap-ng (https://people.redhat.com/sgrubb/libcap-ng/)
-libdw (https://sourceware.org/elfutils/)
-libiscsi (https://github.com/sahlberg/libiscsi)
-libnfs (https://github.com/sahlberg/libnfs)
-libpmem (https://pmem.io/pmdk/libpmem/)
-libssh (https://www.libssh.org/)
-libu2f-emu (https://github.com/Agnoctopus/libu2f-emu)
-lzfse (https://github.com/lzfse/lzfse)
-netmap (https://github.com/luigirizzo/netmap)
-numactl (https://github.com/numactl/numactl)
-rdma-core (https://github.com/linux-rdma/rdma-core)
-selinux (https://selinuxproject.org/page/Main_Page)
-snappy (https://google.github.io/snappy/)
-spice (https://gitlab.freedesktop.org/spice/spice)
-usbredir (https://gitlab.freedesktop.org/spice/usbredir)
-vde (https://github.com/virtualsquare/vde-2)
-
+#              capstone                  (https://www.capstone-engine.org/)
+#              ceph                      (https://github.com/ceph/ceph/)
+#              daxctl                    (https://pmem.io/daxctl/)
+#              jack                      (https://jackaudio.org/)
+#              glusterfs                 (https://github.com/gluster/glusterfs)
+#              libbpf                    (https://github.com/libbpf/libbpf)
+#              libcacard                 (https://gitlab.freedesktop.org/spice/libcacard)
+#              libcap-ng                 (https://people.redhat.com/sgrubb/libcap-ng/)
+#              libdw                     (https://sourceware.org/elfutils/)
+#              libiscsi                  (https://github.com/sahlberg/libiscsi)
+#              libnfs                    (https://github.com/sahlberg/libnfs)
+#              libpmem                   (https://pmem.io/pmdk/libpmem/)
+#              libssh                    (https://www.libssh.org/)
+#              libu2f-emu                (https://github.com/Agnoctopus/libu2f-emu)
+#              lzfse                     (https://github.com/lzfse/lzfse)
+#              netmap                    (https://github.com/luigirizzo/netmap)
+#              numactl                   (https://github.com/numactl/numactl)
+#              rdma-core                 (https://github.com/linux-rdma/rdma-core)
+#              selinux                   (https://selinuxproject.org/page/Main_Page)
+#              snappy                    (https://google.github.io/snappy/)
+#              spice                     (https://gitlab.freedesktop.org/spice/spice)
+#              usbredir                  (https://gitlab.freedesktop.org/spice/usbredir)
+#              vde2                      (https://github.com/virtualsquare/vde-2)
 
 ### Конфигурация ядра
 #    CONFIG_VIRTUALIZATION=y
-#    CONFIG_KVM=m
-#    CONFIG_KVM_INTEL=m  (для процессоров Intel)
+#    CONFIG_KVM=y|m
+#    CONFIG_KVM_INTEL=y|m  (для процессоров Intel)
 #    или
-#    CONFIG_KVM_AMD=m    (для процессоров AMD)
+#    CONFIG_KVM_AMD=y|m    (для процессоров AMD)
 #    CONFIG_NET=y
-#    CONFIG_BRIDGE=m
+#    CONFIG_BRIDGE=y|m
 #    CONFIG_NETDEVICES=y
-#    CONFIG_TUN=m
+#    CONFIG_NET_CORE=y
+#    CONFIG_TUN=y|m
+
+###
+# NOTE:
+#    после установки пакета нужно добавить обычного пользователя в группу kvm,
+#    т.к. правило udev позволяет использовать устройство KVM только
+#    пользователю root
+#
+#       # usermod -a -G kvm <username>
+###
 
 ROOT="/root/src/lfs"
 source "${ROOT}/check_environment.sh"                  || exit 1
 source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
 
 TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
-mkdir -pv "${TMP_DIR}"/{lib/udev/rules.d,etc/qemu}
+mkdir -pv "${TMP_DIR}"/{usr/lib/udev/rules.d,etc/qemu}
 
 # добавим группу kvm, если не существует
 ! grep -qE "^kvm:" /etc/group  && \
     groupadd -g 61 kvm
 
-# исправим проблему сборки с binutils-2.36
-sed -i "/LDFLAGS_NOPIE/d" configure pc-bios/optionrom/Makefile || exit 1
-
-JEMALLOC="--disable-jemalloc"
-[ -x /usr/lib/libjemalloc.so ] && JEMALLOC="--enable-jemalloc"
-
-TARGETS="i386-softmmu,x86_64-softmmu,i386-linux-user,x86_64-linux-user"
-TARGETS="${TARGETS},arm-softmmu,arm-linux-user,armeb-linux-user"
-
-AUDIO_DRIVERS="alsa,pa"
-
-mkdir -p build
+mkdir -vp build
 cd build || exit 1
 
-../configure                            \
-    --prefix=/usr                       \
-    --sysconfdir=/etc                   \
-    --localstatedir=/var                \
-    --enable-gtk                        \
-    --enable-system                     \
-    --enable-kvm                        \
-    --enable-virtfs                     \
-    --enable-sdl                        \
-    "${JEMALLOC}"                       \
-    --enable-nettle                     \
-    --target-list="${TARGETS}"          \
-    --audio-drv-list="${AUDIO_DRIVERS}" \
-    --disable-debug-info                \
-    --mandir=/usr/share/man             \
+###
+# NOTE:
+#    если установлен опциональный пакет libnfs, то сборка приводит к ошибке,
+#    поэтому в этом случае указываем параметр:
+#       --disable-libnfs
+###
+TARGETS="x86_64-softmmu,x86_64-linux-user"
+../configure                   \
+    --prefix=/usr              \
+    --sysconfdir=/etc          \
+    --localstatedir=/var       \
+    --target-list="${TARGETS}" \
+    --audio-drv-list=alsa,pa   \
+    --disable-debug-info       \
     --docdir="/usr/share/doc/${PRGNAME}-${VERSION}" || exit 1
 
 make || exit 1
-
-# qemu для тестов использует ninja
 # ninja test
-
 make install DESTDIR="${TMP_DIR}"
 
 rm -rf "${TMP_DIR}/var/run"
 
 # добавим правило для Udev, чтобы правильно отображались разрешения для KVM
 # устройств
-cat << EOF > "${TMP_DIR}/lib/udev/rules.d/65-kvm.rules"
+cat << EOF > "${TMP_DIR}/usr/lib/udev/rules.d/65-kvm.rules"
 KERNEL=="kvm", GROUP="kvm", MODE="0660"
 KERNEL=="vhost-net", GROUP="kvm", MODE="0660"
 EOF
 
-# необходимые разрешения для использование bridge
+# необходимые разрешения для использование bridge обычным пользователем
 chgrp kvm  "${TMP_DIR}/usr/libexec/qemu-bridge-helper"
 chmod 4750 "${TMP_DIR}/usr/libexec/qemu-bridge-helper"
-
 echo allow br0 > "${TMP_DIR}/etc/qemu/bridge.conf"
+
+# ссылка в /usr/bin
+#    qemu -> qemu-system-x86_64
+ln -sv qemu-system-"$(uname -m)" "${TMP_DIR}/usr/bin/qemu"
 
 source "${ROOT}/stripping.sh"      || exit 1
 source "${ROOT}/update-info-db.sh" || exit 1
