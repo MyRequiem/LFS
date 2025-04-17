8f964c19f98d26a932d24d87ee93cff47cb080e2
Vladimir <mrvladislavovich@gmail.com>
2025-04-17 23:10:05 +0400

    BLFS: id3lib, libao and gsimplecal packages updated

--- stage-2-blfs-stable-x86_64/build/ap/gsimplecal.sh
+++ stage-2-blfs-stable-x86_64/build/ap/gsimplecal.sh
@@ -11,7 +11,28 @@ PRGNAME="gsimplecal"
 
 ROOT="/root/src/lfs"
 source "${ROOT}/check_environment.sh"                  || exit 1
-source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
+
+SOURCES="${ROOT}/src"
+VERSION="$(find "${SOURCES}" -type f \
+    -name "${PRGNAME}-*.tar.?z*" 2>/dev/null | sort | head -n 1 | \
+    rev | cut -d . -f 3- | cut -d - -f 1 | rev)"
+
+MAJ_VERSION="$(echo "${VERSION}" | cut -d . -f 1,2)"
+
+BUILD_DIR="/tmp/build-${PRGNAME}-${VERSION}"
+rm -rf "${BUILD_DIR}"
+mkdir -pv "${BUILD_DIR}"
+cd "${BUILD_DIR}" || exit 1
+
+tar xvf "${SOURCES}/${PRGNAME}-${VERSION}"*.tar.?z* || exit 1
+cd "${PRGNAME}-${MAJ_VERSION}" || exit 1
+
+chown -R root:root .
+find -L . \
+    \( -perm 777 -o -perm 775 -o -perm 750 -o -perm 711 -o -perm 555 \
+    -o -perm 511 \) -exec chmod 755 {} \; -o \
+    \( -perm 666 -o -perm 664 -o -perm 640 -o -perm 600 -o -perm 444 \
+    -o -perm 440 -o -perm 400 \) -exec chmod 644 {} \;
 
 TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
 mkdir -pv "${TMP_DIR}"
@@ -19,7 +40,6 @@ mkdir -pv "${TMP_DIR}"
 ./autogen.sh || exit 1
 ./configure       \
     --prefix=/usr \
-    --enable-gtk2 \
     --docdir="/usr/share/doc/${PRGNAME}-${VERSION}" || exit 1
 
 make || exit 1
@@ -35,7 +55,7 @@ cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # Gsimplecal is a lightweight calendar applet written in C++ using GTK
 #
 # Home page: https://github.com/dmedvinsky/${PRGNAME}
-# Download:  https://github.com/dmedvinsky/${PRGNAME}/archive/v${VERSION}/${PRGNAME}-${VERSION}.tar.gz
+# Download:  https://github.com/dmedvinsky/${PRGNAME}/archive/v${MAJ_VERSION}/${PRGNAME}-${VERSION}.tar.gz
 #
 EOF
 
--- stage-2-blfs-stable-x86_64/build/l/libao.sh
+++ stage-2-blfs-stable-x86_64/build/l/libao.sh
@@ -4,7 +4,7 @@ PRGNAME="libao"
 
 ### Libao (Audio Output library)
 # Кроссплатформенная аудиобиблиотека, которая требуется многим программам и
-# другим библиотекам, которые используют аудио форматов ogg123, GAIM, Ogg и др.
+# другим библиотекам, которые используют аудио форматы ogg123, GAIM, Ogg и др.
 # Пакет содержит плагины для файлов WAV, OSS, ESD, NAS, aRts, ALSA и
 # PulseAudio.
 
@@ -20,6 +20,9 @@ source "${ROOT}/unpack_source_archive.sh" "${PRGNAME}" || exit 1
 
 TMP_DIR="${BUILD_DIR}/package-${PRGNAME}-${VERSION}"
 mkdir -pv "${TMP_DIR}"
+
+# исправим ошибку сборки с GCC-14
+sed -i '/limits.h/a #include <time.h>' src/plugins/pulse/ao_pulse.c || exit 1
 
 ./configure \
     --prefix=/usr || exit 1
