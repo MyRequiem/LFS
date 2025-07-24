b7a31a4291576d14386ee98f80cc8edbeab93611
Vladimir <mrvladislavovich@gmail.com>
2025-07-25 00:29:13 +0400

    LFS:  stage-1-getting_started/queue updated

--- stage-1-lfs-stable-x86_64/stage-1-getting_started/download-src.sh
+++ stage-1-lfs-stable-x86_64/stage-1-getting_started/download-src.sh
@@ -4,13 +4,13 @@ LFS="/mnt/lfs"
 SOURCES="${LFS}/sources"
 WGET_LIST="wget-list"
 
-wget                                   \
-    --input-file=./${WGET_LIST}        \
-    --no-check-certificate             \
-    --progress=bar:force               \
-    --continue                         \
-    --tries=3                          \
-    --wait=2                           \
+wget                              \
+    --input-file="./${WGET_LIST}" \
+    --no-check-certificate        \
+    --progress=bar:force          \
+    --continue                    \
+    --tries=3                     \
+    --wait=2                      \
     --directory-prefix="${SOURCES}"/
 
 chown lfs:lfs "${SOURCES}"/*
