e2875287719e350f9e3faebf4bc106a364e31be4
Vladimir <mrvladislavovich@gmail.com>
2026-03-19 12:30:20 +0400

    LFS:  Udev and Systemd are fucked. We have smoothly switched to Eudev and are enjoying life.

--- stage-1-lfs-stable-x86_64/stage-4-system_configuration/lfs-bootscripts.sh
+++ stage-1-lfs-stable-x86_64/stage-4-system_configuration/lfs-bootscripts.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
 
 PRGNAME="lfs-bootscripts"
-LFS_VERSION="13.0"
 
 ### LFS-Bootscripts (scripts to start/stop the LFS system)
-# Пакет содержит набор скриптов для запуска/остановки системы LFS при
-# загрузке/выключении
+# Пакет содержит набор скриптов для запуска/остановки системы LFS
 
 ROOT="/"
 source "${ROOT}check_environment.sh"                  || exit 1
@@ -15,12 +13,21 @@ TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
 rm -rf "${TMP_DIR}"
 mkdir -pv "${TMP_DIR}"
 
-# исправим пути установки в Makefile:
-#    /lib  ->  /usr/lib
-#    /sbin -> /usr/sbin
-sed -e 's|LIBDIR=${DESTDIR}/lib|LIBDIR=${DESTDIR}/usr/lib|' \
-    -e 's|SBIN=${DESTDIR}/sbin|SBIN=${DESTDIR}/usr/sbin|'   \
-    -i Makefile || exit 1
+# исправим пути установки:
+#    /lib  => /usr/lib
+#    /sbin => /usr/sbin
+patch --verbose -Np1 -i \
+    "${SOURCES}/${PRGNAME}-fix-install-path.diff" || exit 1
+
+# patch выше можно заменить на:
+# sed -e 's|LIBDIR=${DESTDIR}/lib|LIBDIR=${DESTDIR}/usr/lib|' \
+#     -e 's|SBIN=${DESTDIR}/sbin|SBIN=${DESTDIR}/usr/sbin|'   \
+#     -i Makefile || exit 1
+
+# у нас установлен Eudev вместо Udev, адаптируем скрипт запуска
+# /etc/rc.d/init.d/udev
+patch --verbose -Np1 -i \
+    "${SOURCES}/${PRGNAME}-from-udev-to-eudev.diff" || exit 1
 
 make install DESTDIR="${TMP_DIR}"
 
@@ -32,7 +39,7 @@ cat << EOF > "/var/log/packages/${PRGNAME}-${VERSION}"
 # The LFS-Bootscripts package contains a set of scripts to start/stop the LFS
 # system at bootup/shutdown.
 #
-# Download: https://www.linuxfromscratch.org/lfs/downloads/${LFS_VERSION}/${PRGNAME}-${VERSION}.tar.xz
+# Download: https://www.linuxfromscratch.org/lfs/downloads/development/${PRGNAME}-${VERSION}.tar.xz
 #
 EOF
 
