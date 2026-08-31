2b3a07fb3a3ab410321249c44994ff8c1ecd89f8
Vladimir <mrvladislavovich@gmail.com>
2026-08-31 15:08:31 +0400

    chore(stage-3): update gcc build script
        - Update build script in stage-3-build_basic_system/gcc.sh
        - Align compiler build steps with current system requirements

--- stage-1-lfs-stable-x86_64/stage-3-build_basic_system/gcc.sh
+++ stage-1-lfs-stable-x86_64/stage-3-build_basic_system/gcc.sh
@@ -16,20 +16,17 @@ TMP_DIR="/tmp/pkg-${PRGNAME}-${VERSION}"
 rm -rf "${TMP_DIR}"
 mkdir -pv "${TMP_DIR}"/{usr/lib/bfd-plugins,usr/share/gdb/auto-load/usr/lib}
 
-# исправим сборку с glibc >=2.43
-sed -i 's/char [*]q/const &/' libgomp/affinity-fmt.c || exit 1
-
-# изменим каталог для установки библиотек с lib64 на lib
+# Изменим каталог для установки библиотек с lib64 на lib
 sed -e '/m64=/s/lib64/lib/' -i.orig "${PRGNAME}/config/i386/t-linux64" || exit 1
 
-# документация gcc рекомендует собирать gcc в отдельном каталоге
+# Документация gcc рекомендует собирать gcc в отдельном каталоге.
 mkdir build
 cd build || exit 1
 
-# включаем поддержку только С и C++
+# Включаем поддержку только С и C++
 #    --enable-languages=c,c++
-# сообщим GCC, что нужно ссылаться на установленную в системе библиотеку Zlib,
-# а не на собственную внутреннюю копию
+# Сообщим GCC, что нужно ссылаться на установленную в системе библиотеку Zlib,
+# а не на собственную внутреннюю копию.
 #    --with-system-zlib
 ../configure                 \
     --prefix=/usr            \
@@ -38,6 +35,7 @@ cd build || exit 1
     --enable-default-pie     \
     --enable-default-ssp     \
     --enable-host-pie        \
+    --enable-targets=all     \
     --disable-multilib       \
     --disable-bootstrap      \
     --disable-fixincludes    \
@@ -47,64 +45,59 @@ make || make -j1 || exit 1
 
 ###
 # Набор тестов для GCC на данном этапе считается критическим. Нельзя пропускать
-# его ни при каких обстоятельствах
+# его ни при каких обстоятельствах.
 ###
 #
-# известно, что один набор тестов в наборе тестов GCC переполняет стек, поэтому
-# размер стека установим бесконечный
+# Известно, что один набор тестов в наборе тестов GCC переполняет стек, поэтому
+# размер стека установим бесконечный.
 # ulimit -s -H unlimited
 #
-# удалим/исправим несколько известных ошибок при тестировании
-# sed -e '/cpython/d' -i ../gcc/testsuite/gcc.dg/plugin/plugin.exp
-#
-# тесты будем запускать как непривилегированный пользователь tester, поэтому
-# изменим владельца в директории сборки
+# Тесты будем запускать как непривилегированный пользователь tester, поэтому
+# изменим владельца в директории сборки.
 # chown -Rv tester .
 # echo ""
 # echo "# Now run GCC tests"
 # echo 'su tester -c "PATH=$PATH make -k check"'
-# echo -n "Press any key for continue..."
+# echo -n "Press <Enter> for continue..."
 # read -r JUNK
 # echo "${JUNK}" > /dev/null
 # su tester -c "PATH=$PATH make -k check"
 
-# пишем результаты тестов GCC в gcc-test.log
-# ../contrib/test_summary 2>&1 | grep -A7 Summ > "${PRGNAME}-test.log"
+# Пишем результаты тестов GCC в gcc-test.log
+# ../contrib/test_summary -t 2>&1 | grep -A7 Summ > "${PRGNAME}-test.log"
 
-# вернем владельца сборочной директории обратно
+# Вернем владельца сборочной директории обратно.
 # chown -Rv root:root .
 
-# установим пакет
+# Установим пакет.
 make install DESTDIR="${TMP_DIR}"
 
 rm -rf "${TMP_DIR}/usr/share"/{doc,gtk-doc,help,licenses}
 
-# создадим символическую ссылку в /usr/lib/, требуемую FHS по "историческим"
-# причинам
+# Создадим ссылку в /usr/lib/, требуемую FHS по "историческим" причинам.
 #    cpp -> ../bin/cpp
 ln -svf ../bin/cpp "${TMP_DIR}/usr/lib"
 
-# многие программы используют имя 'cc' для вызова C-компилятора, поэтому
+# Многие программы используют имя 'cc' для вызова C-компилятора, поэтому
 # создадим символическую ссылку в /usr/bin/
 #    cc -> gcc
 ln -svf gcc "${TMP_DIR}/usr/bin/cc"
 
 ln -svf gcc.1 "${TMP_DIR}/usr/share/man/man1/cc.1"
 
-# добавим символическую ссылку в  /usr/lib/bfd-plugins/ для совместимости,
-# чтобы включить сборку программ с оптимизацией компоновки LTO (Link Time
-# Optimization)
+# Добавим ссылку в  /usr/lib/bfd-plugins/ для совместимости, чтобы включить
+# сборку программ с оптимизацией компоновки LTO (Link Time Optimization).
 #    liblto_plugin.so -> \
 #    ../../libexec/gcc/x86_64-pc-linux-gnu/${VERSION}/liblto_plugin.so
 DUMPMACHINE="$("${TMP_DIR}/usr/bin/${PRGNAME}" -dumpmachine)"
 ln -sfv "../../libexec/${PRGNAME}/${DUMPMACHINE}/${VERSION}/liblto_plugin.so" \
     "${TMP_DIR}/usr/lib/bfd-plugins/"
 
-# переместим некоторые файлы
+# Переместим некоторые файлы.
 mv -v "${TMP_DIR}/usr/lib"/*gdb.py "${TMP_DIR}/usr/share/gdb/auto-load/usr/lib/"
 
-# если мы устанавливаем пакет в первый раз, удалим директории и файлы, которые
-# были установлены GCC, построенным во временной системе
+# Если мы устанавливаем пакет в первый раз, удалим директории и файлы, которые
+# были установлены GCC, построенным во временной системе.
 if [ -d /usr/x86_64-lfs-linux-gnu ]; then
     rm -f  /usr/bin/x86_64-lfs-linux-gnu-*
     rm -rf "/usr/include/c++/${VERSION}/x86_64-lfs-linux-gnu"
